b63b273e44c887627a32235fb790f7507daa0802
Hayden Briese <haydenbriese@gmail.com>
2022-03-15 23:05:11 +1100

    Setup mono repo and add contracts

---
+++ contracts/utils/errors.ts
@@ -0,0 +1,11 @@
+export const SafeEvent = {
+  Deposit: "Deposit",
+};
+
+export const SafeError = {
+  NotSafe: "NotSafe",
+  NotPrimaryApprover: "NotPrimaryApprover",
+  TotalGroupWeightLessThan100Percent: "TotalGroupWeightLessThan100Percent",
+  TotalApprovalWeightsInsufficient: "TotalApprovalWeightsInsufficient",
+  ExecutionReverted: "ExecutionReverted",
+};
