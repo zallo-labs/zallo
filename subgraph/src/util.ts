3cd9ef35af349f098863b5cf4263cb0e6d59973c
Hayden Briese <haydenbriese@gmail.com>
2022-03-17 22:15:27 +1100

    Add subgraph

---
+++ subgraph/src/util.ts
@@ -0,0 +1,18 @@
+import { Address, ethereum } from "@graphprotocol/graph-ts";
+import { Safe } from "../generated/Safe/Safe";
+
+export function getSafe(e: ethereum.Event): Safe {
+  return Safe.bind(e.address);
+}
+
+export function getSafeObjId(safe: Address): string {
+  return safe.toHex();
+}
+
+export function getApproverId(approver: Address): string {
+  return approver.toHex();
+}
+
+export function getTxApproverId(txId: string, approverId: string): string {
+  return `${txId}-${approverId}`;
+}
