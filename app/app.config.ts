c0a3623db9ebfa0f3704cd4c2e09ff46f75807af
Hayden Briese <haydenbriese@gmail.com>
2022-03-24 21:15:14 +1100

    Fix app for yarn workspace

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
-import { ExpoConfig, ConfigContext } from "@expo/config";
+import { ExpoConfig, ConfigContext } from '@expo/config';
 
-// Cannot use import statement outside a module
-const { CONFIG } = require("lib/config");
+// No imports allow outside of a module
+const { CONFIG } = require('lib/config');
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: config.name || "",
-  slug: config.slug || "",
+  name: config.name || '',
+  slug: config.slug || '',
   extra: CONFIG,
 });
