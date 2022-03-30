0902bc570f42212c1cd3a29e7ca33f4988c244ed
Hayden Briese <haydenbriese@gmail.com>
2022-03-30 17:53:22 +1100

    Simplify users resolver

--- api/src/features/users/users.resolver.ts
+++ api/src/features/users/users.resolver.ts
@@ -1,49 +1,22 @@
-import {
-  Args,
-  Field,
-  Int,
-  ObjectType,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-} from "@nestjs/graphql";
-import { PrismaService } from "nestjs-prisma";
-import { UserWhereUniqueInput } from "~gen/user/user-where-unique.input";
-
-@ObjectType()
-export class Post {
-  @Field(() => Int)
-  id: number;
-
-  @Field()
-  createdAt: Date;
-
-  @Field()
-  updatedAt: Date;
-}
-
-@ObjectType()
-export class User {
-  @Field(() => Int)
-  id: number;
-
-  @Field()
-  email: string;
-
-  @Field(() => String, { nullable: true })
-  name: string | null;
-}
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { PrismaService } from 'nestjs-prisma';
+import { Post } from '~gen/post/post.model';
+import { FindManyUserArgs } from '~gen/user/find-many-user.args';
+import { FindUniqueUserArgs } from '~gen/user/find-unique-user.args';
+import { User } from '~gen/user/user.model';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private prisma: PrismaService) {}
 
   @Query(() => User)
-  async user(@Args("email", { type: () => String }) email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: { email },
-    });
+  async user(@Args() args: FindUniqueUserArgs): Promise<User | null> {
+    return this.prisma.user.findUnique(args);
+  }
+
+  @Query(() => [User])
+  async users(@Args() args: FindManyUserArgs): Promise<User[]> {
+    return this.prisma.user.findMany(args);
   }
 
   @ResolveField(() => [Post])
@@ -52,13 +25,6 @@ export class UsersResolver {
       where: {
         authorId: user.id,
       },
-    });
-  }
-
-  @Query(() => User)
-  async user2(@Args("where") where: UserWhereUniqueInput): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where,
     });
   }
 }
