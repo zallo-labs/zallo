4475fda4b1a3083deb4be147d89b2b06f059fe60
Hayden Briese <haydenbriese@gmail.com>
2022-04-04 19:27:24 +1000

    Add app gql types

---
+++ app/src/gql/__generated__/GetSgSafes.ts
@@ -0,0 +1,51 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetSgSafes
+// ====================================================
+
+export interface GetSgSafes_approver_groups_group_safe_groups_approvers {
+  __typename: "GroupApprover";
+  id: string;
+  weight: any;
+}
+
+export interface GetSgSafes_approver_groups_group_safe_groups {
+  __typename: "Group";
+  id: string;
+  hash: any;
+  active: boolean;
+  approvers: GetSgSafes_approver_groups_group_safe_groups_approvers[];
+}
+
+export interface GetSgSafes_approver_groups_group_safe {
+  __typename: "Safe";
+  id: string;
+  groups: GetSgSafes_approver_groups_group_safe_groups[];
+}
+
+export interface GetSgSafes_approver_groups_group {
+  __typename: "Group";
+  safe: GetSgSafes_approver_groups_group_safe;
+}
+
+export interface GetSgSafes_approver_groups {
+  __typename: "GroupApprover";
+  group: GetSgSafes_approver_groups_group;
+}
+
+export interface GetSgSafes_approver {
+  __typename: "Approver";
+  groups: GetSgSafes_approver_groups[];
+}
+
+export interface GetSgSafes {
+  approver: GetSgSafes_approver | null;
+}
+
+export interface GetSgSafesVariables {
+  approver: string;
+}
