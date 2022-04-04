efa0d9519eebe36b4410cf27b37a0d20457a3a57
Hayden Briese <haydenbriese@gmail.com>
2022-04-04 20:34:22 +1000

    Fix import

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 
 // No imports allow outside of a module
-const { CONFIG } = require('config/dist/config');
+const { CONFIG } = require('config');
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
