09137b18903092dd8db4482741490f80ab0d8a44
Hayden Briese <haydenbriese@gmail.com>
2022-04-04 19:27:24 +1000

    Add app gql types

---
+++ app/src/gql/__generated__/apiGlobalTypes.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export interface ApproverInput {
+  addr: string;
+  weight: string;
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
