3790b2dd156c003f9fa18d23e6fee0bb9e3c8270
Hayden Briese <haydenbriese@gmail.com>
2022-04-14 21:59:44 +1000

    Add SIWE auth

--- app/src/components/ListItem.tsx
+++ app/src/components/ListItem.tsx
@@ -4,18 +4,16 @@ import { Box, BoxProps } from './Box';
 export interface ListItemProps extends BoxProps {
   Left?: ReactNode;
   Main?: ReactNode;
-  InnerRight?: ReactNode;
   Right?: ReactNode;
 }
 
-export const ListItem = ({ Left, Main, InnerRight, Right, ...boxProps }: ListItemProps) => (
+export const ListItem = ({ Left, Main, Right, ...boxProps }: ListItemProps) => (
   <Box horizontal justifyContent="space-between" alignItems="center" {...boxProps}>
-    <Box horizontal justifyContent="flex-start" minWidth="40%">
+    <Box horizontal justifyContent="flex-start" alignItems="center">
       {Left && <Box mr={2}>{Left}</Box>}
       {Main}
     </Box>
 
-    {InnerRight}
     {Right}
   </Box>
 );
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,6 +1,7 @@
 export * from './typechain';
 
 export * from './util/arrays';
+export * from './util/mappedTypes';
 
 export * from './approver';
 export * from './counterfactual';
