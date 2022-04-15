8936a6515c920ea5b4298cbae8943d4917c66faf
Hayden Briese <haydenbriese@gmail.com>
2022-04-15 18:09:20 +1000

    Add better message validation error message checks

--- api/src/features/auth/auth.middleware.ts
+++ api/src/features/auth/auth.middleware.ts
@@ -1,10 +1,9 @@
-import { NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { Logger, NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { SiweMessage } from 'siwe';
 
 import CONFIG, { IS_DEV } from 'config';
-import { DateTime } from 'luxon';
-import { MaybePromise } from 'lib';
+import { VALIDATION_CHECKS } from './message.validation';
 
 interface Token {
   message: SiweMessage;
@@ -21,22 +20,6 @@ const isLocalPlayground = (req: Request) =>
 
 const isIntrospection = (req: Request) => req.body?.operationName === 'IntrospectionQuery';
 
-type IsErrorCheck = (params: {
-  msg: SiweMessage;
-  sig: string;
-  req: Request;
-}) => MaybePromise<unknown>;
-
-const checks: Record<string, IsErrorCheck> = {
-  'Session lacking nonce': ({ req }) => req.session.nonce === undefined,
-  "Message doesn't match signature": ({ msg, sig }) => !msg.validate(sig),
-  "Message nonce doesn't match session nonce": ({ msg, req }) => msg.nonce !== req.session.nonce,
-  'Message has expired': ({ msg }) =>
-    msg.expirationTime && DateTime.fromISO(msg.expirationTime) <= DateTime.now(),
-  'Message not yet valid': ({ msg }) =>
-    msg.notBefore && DateTime.fromISO(msg.notBefore) > DateTime.now(),
-};
-
 export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, _res: Response, next: NextFunction) {
     const token = req.headers.authorization;
@@ -44,12 +27,17 @@ export class AuthMiddleware implements NestMiddleware {
     if (token) {
       const { message, signature } = parseToken(token);
 
-      for (const [errorMessage, isError] of Object.entries(checks)) {
-        if (await isError({ msg: message, sig: signature, req })) {
-          console.log(`Throwing: ${errorMessage}`);
-          throw new UnauthorizedException(errorMessage);
+      for (const [fallbackErr, isError] of Object.entries(VALIDATION_CHECKS)) {
+        const r = await isError({ msg: message, sig: signature, req });
+        if (r) {
+          const err = typeof r === 'string' ? r : fallbackErr;
+          Logger.debug(`Message rejected with: ${err}`);
+          throw new UnauthorizedException(err);
         }
       }
+
+      // Use the session expiry time if provided
+      if (message.expirationTime) req.session.cookie.expires = new Date(message.expirationTime);
 
       req.userMessage = message;
     } else if (IS_DEV && isLocalPlayground(req) && !isIntrospection(req)) {
