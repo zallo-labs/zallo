533819135c027ff1ae98436d2e63633cffd9be33
Hayden Briese <haydenbriese@gmail.com>
2022-04-17 16:15:46 +1000

    Show an EIP-681 link for the QR

--- app/src/features/receive/SafeQr.tsx
+++ app/src/features/receive/SafeQr.tsx
@@ -2,10 +2,11 @@ import { useWindowDimensions } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import QRCode from 'react-native-qrcode-svg';
 
+import { CHAIN } from '@provider';
 import { useSafe } from '../safe/SafeProvider';
 
 // https://eips.ethereum.org/EIPS/eip-681
-// const getLink = (addr: string) => `ethereum:pay-${addr}@${CHAIN.id}`;
+const getLink = (addr: string) => `ethereum:pay-${addr}@${CHAIN.id}`;
 
 export const SafeQr = () => {
   const safe = useSafe();
@@ -14,7 +15,7 @@ export const SafeQr = () => {
 
   return (
     <QRCode
-      value={safe.contract.address}
+      value={getLink(safe.contract.address)}
       size={Math.min(window.width, window.height) * 0.8}
       backgroundColor={colors.background}
       ecl="M"
