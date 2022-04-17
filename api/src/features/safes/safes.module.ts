27bb6bc684e47b04fc0f4345b6a7d1ae61dd634d
Hayden Briese <haydenbriese@gmail.com>
2022-04-18 02:28:13 +1000

    Add provider to backend and refactor chain configuration

--- api/src/features/safes/safes.module.ts
+++ api/src/features/safes/safes.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProviderModule } from '../provider/provider.module';
 import { SafesResolver } from './safes.resolver';
 
 @Module({
+  imports: [ProviderModule],
   providers: [SafesResolver],
 })
 export class SafesModule {}
