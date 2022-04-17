b61028163d1e4623f20ffedf5ee36760276e49ef
Hayden Briese <haydenbriese@gmail.com>
2022-04-17 16:08:47 +1000

    Add receive screen

---
+++ app/src/features/receive/SafeQr.tsx
@@ -0,0 +1,26 @@
+import { useWindowDimensions } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import QRCode from 'react-native-qrcode-svg';
+
+import { useSafe } from '../safe/SafeProvider';
+
+// https://eips.ethereum.org/EIPS/eip-681
+// const getLink = (addr: string) => `ethereum:pay-${addr}@${CHAIN.id}`;
+
+export const SafeQr = () => {
+  const safe = useSafe();
+  const { colors } = useTheme();
+  const window = useWindowDimensions();
+
+  return (
+    <QRCode
+      value={safe.contract.address}
+      size={Math.min(window.width, window.height) * 0.8}
+      backgroundColor={colors.background}
+      ecl="M"
+      // color={colors.text}
+      enableLinearGradient
+      linearGradient={[colors.primary, colors.accent]}
+    />
+  );
+};
