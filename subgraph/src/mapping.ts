121a0991382d446816f445c88ff019b0041bf3b9
Hayden Briese <haydenbriese@gmail.com>
2022-04-26 21:33:01 +1000

    Add testnet & metanet configuration, and refactor subgraph

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,60 +1,35 @@
-import { Address, Bytes, ethereum } from '@graphprotocol/graph-ts';
 import {
   Deposit as DepositEvent,
   Execution,
   GroupAdded,
   GroupRemoved,
-  Safe__hashGroupInput_approversStruct,
-  Safe__hashTxInput_txStruct,
 } from '../generated/Safe/Safe';
 import {
-  Approver,
   Deposit,
   Group,
   GroupApprover,
   Transaction,
   TransactionApprover,
-  Safe as SafeObj,
 } from '../generated/schema';
-import { getApproverId, getSafe, getSafeObjId, getTxApproverId } from './util';
+import {
+  getApproverId,
+  getDepositId,
+  getGroupApproverId,
+  getGroupId,
+  getSafeObjId,
+  getTransactionApproverId,
+  getTransactionId,
+} from './id';
+import {
+  getOrCreateApprover,
+  getOrCreateSafeObj,
+  getSafe,
+  hashGroup,
+} from './util';
 
-/* UTIL */
-function getEventId(e: ethereum.Event): string {
-  return `${e.transaction.hash.toHex()}-${e.transactionLogIndex}`;
-}
-
-function getGroupId(safe: Address, hash: Bytes): string {
-  return `${safe.toHex()}-${hash.toHex()}`;
-}
-
-function getOrCreateSafeObj(addr: Address): SafeObj {
-  const id = getSafeObjId(addr);
-  let safeObj = SafeObj.load(id);
-  if (!safeObj) {
-    safeObj = new SafeObj(id);
-    safeObj.save();
-  }
-
-  return safeObj;
-}
-
-function getOrCreateApprover(addr: Address): Approver {
-  const id = getApproverId(addr);
-  let approver = Approver.load(id);
-  if (!approver) {
-    approver = new Approver(id);
-    approver.save();
-  }
-
-  return approver;
-}
-
-/* HANDLERS */
 export function handleGroupAdded(e: GroupAdded): void {
   const safe = getSafe(e);
-  const hash = safe.hashGroup(
-    changetype<Array<Safe__hashGroupInput_approversStruct>>(e.params.approvers),
-  );
+  const hash = hashGroup(e.params.approvers);
   const id = getGroupId(safe._address, hash);
 
   const group = new Group(id);
@@ -64,24 +39,22 @@ export function handleGroupAdded(e: GroupAdded): void {
 
   group.save();
 
-  // Create GroupApprovers
+  // Create GroupApprovers & maybe Approver
   for (let i = 0; i < e.params.approvers.length; i++) {
-    const approver = e.params.approvers[i];
+    const approverStruct = e.params.approvers[i];
+    const approverId = getOrCreateApprover(approverStruct.addr).id;
 
-    const groupApprover = new GroupApprover(
-      `${safe._address.toHex()}-${hash.toHex()}-${approver.addr.toHex()}`,
-    );
+    const groupApprover = new GroupApprover(getGroupApproverId(id, approverId));
     groupApprover.group = id;
-    groupApprover.approver = getOrCreateApprover(approver.addr).id;
-    groupApprover.weight = approver.weight;
+    groupApprover.approver = approverId;
+    groupApprover.weight = approverStruct.weight;
 
     groupApprover.save();
   }
 }
 
 export function handleGroupRemoved(e: GroupRemoved): void {
-  const id = getGroupId(e.address, e.params.groupHash);
-  const group = Group.load(id);
+  const group = Group.load(getGroupId(e.address, e.params.groupHash));
   if (group) {
     group.active = false;
     group.save();
@@ -89,7 +62,8 @@ export function handleGroupRemoved(e: GroupRemoved): void {
 }
 
 export function handleDeposit(e: DepositEvent): void {
-  const deposit = new Deposit(getEventId(e));
+  const deposit = new Deposit(getDepositId(e));
+
   deposit.safe = getSafeObjId(e.address);
   deposit.from = e.params.from;
   deposit.value = e.params.value;
@@ -103,10 +77,9 @@ export function handleExecution(e: Execution): void {
   const safe = getSafe(e);
   const p = e.params;
 
-  const txHash = safe.hashTx(changetype<Safe__hashTxInput_txStruct>(p.tx));
-  const id = txHash.toHex();
-
+  const id = getTransactionId(safe, p.tx);
   const tx = new Transaction(id);
+
   tx.safe = getSafeObjId(safe._address);
   tx.to = p.tx.to;
   tx.value = p.tx.value;
@@ -122,12 +95,13 @@ export function handleExecution(e: Execution): void {
 
   // Create TransactionApprovers
   for (let i = 0; i < p.approvers.length; i++) {
-    const approver = p.approvers[i];
-    const approverId = getApproverId(approver);
+    const approverId = getApproverId(p.approvers[i]);
 
-    const txApprover = new TransactionApprover(getTxApproverId(id, approverId));
+    const txApprover = new TransactionApprover(
+      getTransactionApproverId(id, approverId),
+    );
     txApprover.transaction = tx.id;
-    txApprover.approver = approver.toHex();
+    txApprover.approver = approverId;
 
     txApprover.save();
   }
--- subgraph/src/util.ts
+++ subgraph/src/util.ts
@@ -1,18 +1,51 @@
-import { Address, ethereum } from '@graphprotocol/graph-ts';
-import { Safe } from '../generated/Safe/Safe';
+import { Address, Bytes, crypto, ethereum } from '@graphprotocol/graph-ts';
+import { GroupAddedApproversStruct, Safe } from '../generated/Safe/Safe';
+import { Approver, Safe as SafeObj } from '../generated/schema';
+import { getApproverId, getSafeObjId } from './id';
 
 export function getSafe(e: ethereum.Event): Safe {
   return Safe.bind(e.address);
 }
 
-export function getSafeObjId(safe: Address): string {
-  return safe.toHex();
+function approverToEthValue(
+  approver: GroupAddedApproversStruct,
+): ethereum.Tuple {
+  const addr = ethereum.Value.fromAddress(approver.addr);
+  const weight = ethereum.Value.fromUnsignedBigInt(approver.weight);
+
+  return changetype<ethereum.Tuple>([addr, weight]);
 }
 
-export function getApproverId(approver: Address): string {
-  return approver.toHex();
+export function hashGroup(approvers: GroupAddedApproversStruct[]): Bytes {
+  const appTuples: ethereum.Tuple[] = [];
+  for (let i = 0; i < approvers.length; i++) {
+    appTuples.push(approverToEthValue(approvers[i]));
+  }
+
+  const encoded = ethereum.encode(ethereum.Value.fromTupleArray(appTuples));
+  if (!encoded) throw new Error('Failed to hash group');
+
+  return Bytes.fromByteArray(crypto.keccak256(encoded));
 }
 
-export function getTxApproverId(txId: string, approverId: string): string {
-  return `${txId}-${approverId}`;
+export function getOrCreateSafeObj(addr: Address): SafeObj {
+  const id = getSafeObjId(addr);
+  let safeObj = SafeObj.load(id);
+  if (!safeObj) {
+    safeObj = new SafeObj(id);
+    safeObj.save();
+  }
+
+  return safeObj;
+}
+
+export function getOrCreateApprover(addr: Address): Approver {
+  const id = getApproverId(addr);
+  let approver = Approver.load(id);
+  if (!approver) {
+    approver = new Approver(id);
+    approver.save();
+  }
+
+  return approver;
 }
