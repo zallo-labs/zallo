772404bc662d2f102d2324fab265dd0ae06d83bf
Hayden Briese <haydenbriese@gmail.com>
2022-05-01 21:46:40 +1000

    Refactor and optimize

--- contracts/contracts/Types.sol
+++ contracts/contracts/Types.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.0;
 
 // Fixed-point percentage with a precision of 28; e.g. 5.2% = 0.52 * 10 ** 28
-// A int256 can safely hold ~7e47 uint96s (min. bytes required to store the precision)
+// An int256 can safely hold ~7e47 uint96s (min. bytes required to store the precision)
 int256 constant _100_PERCENT_WEIGHT = 10**28;
 
 struct Approver {
   address addr;
-  uint256 weight;
+  uint96 weight;
 }
 
 struct Group {
-  mapping(address => uint256) approvers;
+  mapping(address => int256) approvers;
 }
 
 struct Tx {
@@ -21,7 +21,12 @@ struct Tx {
   uint256 nonce;
 }
 
+struct Signer {
+  address addr;
+  bytes signature;
+}
+
 struct SignedTx {
   Tx tx;
-  bytes[] signatures;
+  Signer[] signers;
 }
