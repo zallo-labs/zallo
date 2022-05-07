babead3571bb4ff41af1ee251b360b4a202bc200
Hayden Briese <haydenbriese@gmail.com>
2022-05-07 13:36:32 +1000

    Fix identicon address seeding

--- app/src/components/Identicon.tsx
+++ app/src/components/Identicon.tsx
@@ -1,7 +1,5 @@
-import { useMemo } from 'react';
 import { LogBox, Platform } from 'react-native';
 import Jazzicon, { IJazziconProps } from 'react-native-jazzicon';
-import { isHexString } from 'ethers/lib/utils';
 
 // react-native-jazzicon causes a warning; see https://github.com/stanislaw-glogowski/react-native-jazzicon/pull/1
 if (Platform.OS !== 'web')
@@ -12,10 +10,5 @@ export type IdenticonProps = Omit<IJazziconProps, 'seed'> & {
 };
 
 export const Identicon = ({ seed, ...props }: IdenticonProps) => {
-  const parsedSeed = useMemo(
-    () => parseFloat(isHexString(seed) ? seed.slice(2) : seed),
-    [seed],
-  );
-
-  return <Jazzicon size={40} {...props} seed={parsedSeed} />;
+  return <Jazzicon size={40} {...props} address={seed} />;
 };
