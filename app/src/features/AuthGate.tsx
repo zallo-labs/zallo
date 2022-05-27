5b5bc7c2cce4a303c5fe74b6e1a717b5218a14bb
Hayden Briese <haydenbriese@gmail.com>
2022-05-27 17:30:33 +1000

    Make AuthGate more robust for cancelled auth requests

--- app/src/features/AuthGate.tsx
+++ app/src/features/AuthGate.tsx
@@ -6,36 +6,41 @@ import { ChildrenProps } from '@util/children';
 import { Suspend } from '@components/Suspender';
 
 const tryAuthenticate = async () => {
-  const enrolledLevel = await Auth.getEnrolledLevelAsync();
-  if (enrolledLevel === Auth.SecurityLevel.NONE) return true; // What are you gonna do...?
+  try {
+    const enrolledLevel = await Auth.getEnrolledLevelAsync();
+    if (enrolledLevel === Auth.SecurityLevel.NONE) return true; // What are you gonna do...?
 
-  return (await Auth.authenticateAsync()).success;
+    return (await Auth.authenticateAsync()).success;
+  } catch (_) {
+    return false;
+  }
 };
 
 export const AuthGate = ({ children }: ChildrenProps) => {
-  const [authd, setAuthd] = useState(false);
+  // Use an object to re-render every time setAuth is called
+  const [auth, setAuth] = useState({ success: false });
 
   // Try authenticate
   useAsyncEffect(
     async (isMounted) => {
-      if (!authd) {
-        const r = await tryAuthenticate();
-        if (isMounted()) setAuthd(r);
+      if (!auth.success) {
+        const success = await tryAuthenticate();
+        if (isMounted()) setAuth({ success });
       }
     },
-    [authd, setAuthd],
+    [auth, setAuth],
   );
 
   // Unauthenticate if app had left foreground
   useEffect(() => {
     const listener = AppState.addEventListener('change', (newState) => {
-      if (newState === 'active') setAuthd(false);
+      if (newState === 'active') setAuth({ success: false });
     });
 
     return () => listener.remove();
-  }, [setAuthd]);
+  }, [setAuth]);
 
-  if (!authd) return <Suspend />;
+  if (!auth.success) return <Suspend />;
 
   return <>{children}</>;
 };
