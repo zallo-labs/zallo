e0d28050e34ef815adfbf5ad80e1a2f709567d5c
Hayden Briese <haydenbriese@gmail.com>
2022-06-14 14:52:35 +1000

    Fix dotted timeline connector having additional bottom margin

--- app/src/features/tx/timeline/TimelineConnector.tsx
+++ app/src/features/tx/timeline/TimelineConnector.tsx
@@ -1,33 +1,36 @@
 import { Box } from '@components/Box';
+import { StyleProp, ViewStyle } from 'react-native';
 import DashedLine from 'react-native-dashed-line';
 
 const minHeight = 40;
 const width = 3;
 const borderRadius = 5;
+const dashGap = 5;
+
+const dashedLineStyle: StyleProp<ViewStyle> = {
+  minHeight,
+  // The last dash also has marginBottom=dashGap, which is not what we want
+  marginBottom: -dashGap,
+};
+
+const dashStyle: StyleProp<ViewStyle> = { borderRadius };
 
 export interface TimelineConnectorProps {
   color: string;
   dotted?: boolean;
 }
 
-export const TimelineConnector = ({
-  color,
-  dotted,
-}: TimelineConnectorProps) => {
-  if (!dotted)
-    return (
-      <Box style={{ minHeight, width, borderRadius }} backgroundColor={color} />
-    );
-
-  return (
+export const TimelineConnector = ({ color, dotted }: TimelineConnectorProps) =>
+  dotted ? (
     <DashedLine
       axis="vertical"
       dashLength={10}
       dashThickness={width}
-      style={{ minHeight }}
+      style={dashedLineStyle}
       dashColor={color}
-      dashGap={5}
-      dashStyle={{ borderRadius }}
+      dashGap={dashGap}
+      dashStyle={dashStyle}
     />
+  ) : (
+    <Box style={{ minHeight, width, borderRadius }} backgroundColor={color} />
   );
-};
