206cd60fd9c46a5528867adbab4340bd0e04d0fd
Hayden Briese <haydenbriese@gmail.com>
2022-06-29 16:13:35 +1000

    Replace chevron uses with animated Chevron component

--- app/src/features/tx/timeline/TimelineChevron.tsx
+++ app/src/features/tx/timeline/TimelineChevron.tsx
@@ -1,18 +1,16 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Chevron } from '@components/Chevron';
 
 export interface TimelineChevronProps {
   color: string;
   expanded?: boolean;
 }
 
-export const TimelineChevron = ({ color, expanded }: TimelineChevronProps) => {
-  return (
-    <MaterialCommunityIcons
-      name={expanded ? 'chevron-up' : 'chevron-down'}
-      color={color}
-      size={25}
-      // Remove default margin
-      style={{ margin: -10 }}
-    />
-  );
-};
+export const TimelineChevron = ({ color, expanded }: TimelineChevronProps) => (
+  <Chevron
+    expanded={expanded}
+    color={color}
+    size={25}
+    // Remove default margin
+    style={{ margin: -10 }}
+  />
+);
