5d03ae188b18cec48f26819be5b788c9bd7e05b0
Hayden Briese <haydenbriese@gmail.com>
2022-06-29 18:27:58 +1000

    Remove statusbar override from SheetBackdrop
    This is no longer necessary after the safeview changes

--- app/src/features/tx/sheet/SheetBackdrop.tsx
+++ app/src/features/tx/sheet/SheetBackdrop.tsx
@@ -2,24 +2,14 @@ import {
   BottomSheetBackdrop,
   BottomSheetBackdropProps,
 } from '@gorhom/bottom-sheet';
-import { StatusBar } from 'expo-status-bar';
-import { useTheme } from 'react-native-paper';
 
 export interface SheetBackdropProps extends BottomSheetBackdropProps {}
 
-export const SheetBackdrop = (props: SheetBackdropProps) => {
-  const { colors } = useTheme();
-
-  return (
-    <>
-      <BottomSheetBackdrop
-        {...props}
-        // Required to fix: https://github.com/gorhom/react-native-bottom-sheet/issues/968
-        appearsOnIndex={0}
-        disappearsOnIndex={-1}
-      />
-
-      <StatusBar style="inverted" backgroundColor={colors.backdrop} />
-    </>
-  );
-};
+export const SheetBackdrop = (props: SheetBackdropProps) => (
+  <BottomSheetBackdrop
+    {...props}
+    // Required to fix: https://github.com/gorhom/react-native-bottom-sheet/issues/968
+    appearsOnIndex={0}
+    disappearsOnIndex={-1}
+  />
+);
