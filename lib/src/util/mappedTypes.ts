b64101692852d742609742aba74297a29cf4248a
Hayden Briese <haydenbriese@gmail.com>
2022-07-02 15:50:26 +1000

    Allow specifying field types when validing object is of type

--- lib/src/util/mappedTypes.ts
+++ lib/src/util/mappedTypes.ts
@@ -5,7 +5,19 @@ export type MaybeArray<T> = T | T[];
 export const maybeToArray = <T>(maybe: MaybeArray<T>): T[] =>
   Array.isArray(maybe) ? maybe : [maybe];
 
+const x = typeof '';
+type TypeOf = typeof x;
+
 export const createIsObj =
-  <T>(...fields: (keyof T)[]) =>
+  <T extends object>(...fields: (keyof T | [keyof T, TypeOf])[]) =>
   (e: unknown): e is T =>
-    typeof e === 'object' && e !== null && fields.every((f) => f in e);
+    typeof e === 'object' &&
+    e !== null &&
+    fields.every((field) => {
+      if (Array.isArray(field)) {
+        const [name, type] = field;
+        return name in e && typeof (e as T)[name] === type;
+      } else {
+        return field in e;
+      }
+    });
