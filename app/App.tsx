bd546477cfc9d72a831c4ec2911ed41bbd2b7901
Hayden Briese <haydenbriese@gmail.com>
2022-07-16 08:01:59 +1000

    Register sentry user after auth to ensure wallet is always deserialized

--- app/App.tsx
+++ app/App.tsx
@@ -29,8 +29,8 @@ export default withSentry(() => (
           <ErrorBoundary>
             <Suspense fallback={<Splash />}>
               <RecoilRoot>
-                <SentryUser />
                 <AuthGate>
+                  <SentryUser />
                   <GqlProvider>
                     <StatusBar style="inverted" />
                     <SafeProvider>
