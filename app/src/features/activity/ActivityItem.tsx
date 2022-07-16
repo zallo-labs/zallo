a7b7a829b86074325fea95e345b31136acdb8f2f
Hayden Briese <haydenbriese@gmail.com>
2022-07-16 13:17:39 +1000

    Remove unreachable code

--- app/src/features/activity/ActivityItem.tsx
+++ app/src/features/activity/ActivityItem.tsx
@@ -2,8 +2,7 @@ import { ItemProps } from '@components/list/Item';
 import { TxItem, TxItemProps } from './tx/TxItem';
 import { TransferItem } from './TransferItem';
 import { TX_STATUS_INSET } from './useTxStatusStyles';
-import { Title } from 'react-native-paper';
-import { isTx, Tx } from '~/queries/tx';
+import { Tx } from '~/queries/tx';
 import { Transfer, isTransfer } from '~/queries/tx/transfer.sub';
 
 export type Activity = Tx | Transfer;
@@ -20,8 +19,6 @@ export const ActivityItem = ({
 }: ActivityItemProps) =>
   isTransfer(activity) ? (
     <TransferItem transfer={activity} {...TX_STATUS_INSET} {...itemProps} />
-  ) : isTx(activity) ? (
-    <TxItem tx={activity} {...txProps} {...itemProps} />
   ) : (
-    <Title>Ops</Title>
+    <TxItem tx={activity} {...txProps} {...itemProps} />
   );
