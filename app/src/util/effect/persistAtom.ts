56c7e922ddc2b42921dea995c3377c6470c9196b
Hayden Briese <haydenbriese@gmail.com>
2022-07-19 09:34:05 +1000

    Add versioning and migration to persist atom

--- app/src/util/effect/persistAtom.ts
+++ app/src/util/effect/persistAtom.ts
@@ -2,6 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AtomEffect, DefaultValue } from 'recoil';
 import * as SecureStore from 'expo-secure-store';
 
+export const DEFAULT_VERSION = 0;
+
 interface Storage {
   getItem: (key: string) => Promise<string | null>;
   setItem: (key: string, value: string) => Promise<void>;
@@ -13,17 +15,8 @@ type Load<T> = (saved: string) => T;
 
 interface Wrapped {
   data: string;
+  version: number;
 }
-
-const serialize = <T>(data: T, save: Save<T>): string => {
-  const wrapped: Wrapped = { data: save(data) ?? null };
-  return JSON.stringify(wrapped);
-};
-
-const deserialize = <T>(serialized: string, load: Load<T>): T => {
-  const wrapped: Wrapped = JSON.parse(serialized);
-  return load(wrapped.data ?? null);
-};
 
 export const getSecureStore = (
   options?: SecureStore.SecureStoreOptions,
@@ -38,6 +31,8 @@ export interface PersistAtomOptions<T> {
   load?: Load<T>;
   storage?: Storage;
   saveIf?: (value: T, isDefault: boolean) => boolean;
+  version?: number;
+  migrate?: (value: unknown, version: number) => T;
 }
 
 export const persistAtom =
@@ -46,19 +41,31 @@ export const persistAtom =
     load = JSON.parse,
     storage = AsyncStorage,
     saveIf,
+    version = DEFAULT_VERSION,
+    migrate,
   }: PersistAtomOptions<T> = {}): AtomEffect<T> =>
   ({ setSelf, onSet, node: { key } }) => {
     // Loads the saved value, otherwise uses the default value
     setSelf(
       storage.getItem(key).then((saved) => {
-        return saved != null ? deserialize(saved, load) : new DefaultValue();
+        if (saved === null) return new DefaultValue();
+
+        const { data, version: dataVersion }: Wrapped = JSON.parse(saved);
+        if (dataVersion !== version)
+          return migrate
+            ? migrate(load(data), dataVersion)
+            : new DefaultValue();
+
+        return load(data ?? null);
       }),
     );
 
     // Subscribe to state changes and persist them to localForage
     onSet((newValue, _oldValue, isReset) => {
       if (!saveIf || saveIf(newValue, isReset)) {
-        storage.setItem(key, serialize(newValue, save));
+        const wrapped: Wrapped = { data: save(newValue) ?? null, version };
+
+        storage.setItem(key, JSON.stringify(wrapped));
       } else {
         storage.removeItem(key);
       }
