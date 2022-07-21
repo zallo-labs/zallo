d5f22b13a9e2948265920261c9a149bffe756e9a
Hayden Briese <haydenbriese@gmail.com>
2022-07-21 14:00:26 +1000

    Fix solidity unused variable warnings

--- contracts/contracts/ERC1155Receiver.sol
+++ contracts/contracts/ERC1155Receiver.sol
@@ -8,22 +8,22 @@ import './ERC721Receiver.sol';
 abstract contract ERC1155Reciever is IERC1155Receiver {
   /// @inheritdoc IERC1155Receiver
   function onERC1155Received(
-    address operator,
-    address from,
-    uint256 id,
-    uint256 value,
-    bytes calldata data
+    address, /* operator */
+    address, /* from */
+    uint256, /* id */
+    uint256, /* value */
+    bytes calldata /* data */
   ) external pure override returns (bytes4) {
     return IERC1155Receiver.onERC1155Received.selector;
   }
 
   /// @inheritdoc IERC1155Receiver
   function onERC1155BatchReceived(
-    address operator,
-    address from,
-    uint256[] calldata ids,
-    uint256[] calldata values,
-    bytes calldata data
+    address, /* operator */
+    address, /* from */
+    uint256[] calldata, /* ids */
+    uint256[] calldata, /* values */
+    bytes calldata /* data */
   ) external pure override returns (bytes4) {
     return IERC1155Receiver.onERC1155BatchReceived.selector;
   }
--- contracts/contracts/ERC721Receiver.sol
+++ contracts/contracts/ERC721Receiver.sol
@@ -6,10 +6,10 @@ import '@openzeppelin/contracts/interfaces/IERC721Receiver.sol';
 abstract contract ERC721Receiver is IERC721Receiver {
   /// @inheritdoc IERC721Receiver
   function onERC721Received(
-    address operator,
-    address from,
-    uint256 tokenId,
-    bytes calldata data
+    address, /* operator */
+    address, /* from */
+    uint256, /* tokenId */
+    bytes calldata /* data */
   ) external pure override returns (bytes4) {
     return IERC721Receiver.onERC721Received.selector;
   }
