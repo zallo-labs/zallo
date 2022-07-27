c66481ecf7ed85a7578502e8feb61fe34ada6c80
Hayden Briese <haydenbriese@gmail.com>
2022-07-27 14:31:37 +1000

    MD3

--- app/src/util/theme/paper.ts
+++ app/src/util/theme/paper.ts
@@ -1,89 +1,35 @@
-import { DarkTheme as PaperDarkTheme } from 'react-native-paper';
+import { MD3DarkTheme as PaperDarkTheme } from 'react-native-paper';
 import Color from 'color';
 import {
   StyledComponentsTheme,
   STYLED_COMPONENTS_THEME,
 } from './styledComponents';
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace ReactNativePaper {
-    interface ThemeColors {
-      // primary
-      onPrimary: string;
-      primaryContainer: string;
-      onPrimaryContainer: string;
-      // accent
-      onAccent: string;
-      accentContainer: string;
-      onAccentContainer: string;
+// declare global {
+//   interface Theme extends StyledComponentsTheme {
+//     iconSize: {
+//       small: number;
+//       medium: number;
+//     };
 
-      // background
-      onBackground: string;
-      // surface
-      onSurface: string;
-      opaqueSurface: string;
-      outline: string;
-
-      success: string;
-      info: string;
-      warning: string;
-      delete: string;
-      lighterText: string;
-    }
-
-    interface Theme extends StyledComponentsTheme {
-      iconSize: {
-        small: number;
-        medium: number;
-      };
-
-      iconButton: {
-        size: number;
-        containerSize: number;
-      }
-    }
-  }
-}
+//     iconButton: {
+//       size: number;
+//       containerSize: number;
+//     };
+//   }
+// }
 
 // https://github.com/callstack/react-native-paper/blob/main/src/styles/themes/v3/DarkTheme.tsx
-// https://akveo.github.io/react-native-ui-kitten/docs/design-system/eva-dark-theme
-const surface = '#2b324d';
-const onBackground = '#e9e2d9';
-
-/* Notes
- * Placeholder color is replaced with onSurfaceVariant
- */
-
-export const PAPER_THEME: ReactNativePaper.Theme = {
+export const PAPER_THEME = {
   ...PaperDarkTheme,
   ...STYLED_COMPONENTS_THEME,
   colors: {
     ...PaperDarkTheme.colors,
 
-    primary: '#f3c00b', // yellow
-    onPrimary: '#3e2e00',
-    primaryContainer: '#5a4400',
-    onPrimaryContainer: '#ffdf8b',
-
-    accent: '#ffb0c9', // maroony pink
-    onAccent: '#5e1032',
-    accentContainer: '#7b2848',
-    onAccentContainer: '#ffd8e3',
-
-    background: '#151A30', // Dark blue/grey
-    onBackground,
-    surface,
-    onSurface: onBackground,
-    opaqueSurface: new Color(surface).alpha(0.8).string(),
-    outline: '#989080',
-
     success: '#48C12A', // Green
     info: '#559EFC', // Blue
     warning: '#FFAF30', // Orange
-    error: '#FF3D71', // Red/Pink
-    delete: '#DD2C00', // Red
-    lighterText: new Color(PaperDarkTheme.colors.text).alpha(0.7).hexa(),
+    // lighterText: new Color(PaperDarkTheme.colors.onSurface).alpha(0.7).hexa(),
   },
 
   iconSize: {
@@ -93,5 +39,7 @@ export const PAPER_THEME: ReactNativePaper.Theme = {
   iconButton: {
     size: 24,
     containerSize: 40,
-  }
+  },
 };
+
+export type ThemeOverride = typeof PAPER_THEME;
