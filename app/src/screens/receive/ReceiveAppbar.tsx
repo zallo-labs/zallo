20bcea0a10c2bb3c5c4c767ea1dd94373cc5a473
Hayden Briese <haydenbriese@gmail.com>
2022-07-29 14:33:01 +1000

    Implement share button on receive screen

--- app/src/screens/receive/ReceiveAppbar.tsx
+++ app/src/screens/receive/ReceiveAppbar.tsx
@@ -1,16 +1,22 @@
 import { MenuIcon, ShareIcon } from '@util/theme/icons';
+import { useCallback } from 'react';
+import { Share } from 'react-native';
 import { Appbar } from 'react-native-paper';
 
-export interface ReceiveAppbarProps {}
+export interface ReceiveAppbarProps {
+  url: string;
+}
 
-export const ReceiveAppbar = (props: ReceiveAppbarProps) => {
+export const ReceiveAppbar = ({ url }: ReceiveAppbarProps) => {
+  const share = useCallback(() => Share.share({ url, message: url }), [url]);
+
   return (
     <Appbar.Header mode="center-aligned">
       <Appbar.Action icon={MenuIcon} onPress={() => alert('Unimplemented')} />
 
       <Appbar.Content title="Receive" />
 
-      <Appbar.Action icon={ShareIcon} onPress={() => alert('Unimplemented')} />
+      <Appbar.Action icon={ShareIcon} onPress={share} />
     </Appbar.Header>
   );
 };
