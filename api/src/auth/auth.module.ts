4f3288115accff19086b0f3d4527c07006e472c7
Hayden Briese <haydenbriese@gmail.com>
2022-08-03 10:35:51 +1000

    Remove re-import of global module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { AuthController } from './auth.controller';
 import { AuthMiddleware } from './auth.middleware';
 import { SessionMiddleware } from './session.middleware';
 
 @Module({
-  imports: [RedisModule],
   controllers: [AuthController],
 })
 export class AuthModule implements NestModule {
