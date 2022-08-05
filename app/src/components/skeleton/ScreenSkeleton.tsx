84beff31c2129cdbec4b9532c4e97f2df21eb74e
Hayden Briese <haydenbriese@gmail.com>
2022-08-05 13:12:02 +1000

    Add skeleton to receive screen

--- app/src/components/skeleton/ScreenSkeleton.tsx
+++ app/src/components/skeleton/ScreenSkeleton.tsx
@@ -1,19 +1,27 @@
 import { AppbarBack } from '@components/AppbarBack';
 import { Box } from '@components/Box';
+import { MenuIcon } from '@util/theme/icons';
 import { ReactNode } from 'react';
 import { Appbar } from 'react-native-paper';
+import { AppbarHeaderProps } from '~/components2/Appbar/useAppbarHeader';
 import { LineSkeleton } from './LineSkeleton';
 
 export interface ScreenSkeletonProps {
   children?: ReactNode;
+  mode?: AppbarHeaderProps['mode'];
+  menu?: boolean;
 }
 
-export const ScreenSkeleton = ({ children }: ScreenSkeletonProps) => (
+export const ScreenSkeleton = ({
+  children,
+  mode,
+  menu,
+}: ScreenSkeletonProps) => (
   <Box>
-    <Appbar.Header>
-      <AppbarBack />
+    <Appbar.Header mode={mode}>
+      {menu ? <Appbar.Action icon={MenuIcon} /> : <AppbarBack />}
 
-      <LineSkeleton width={150} />
+      <Appbar.Content title={<LineSkeleton width={150} />} />
     </Appbar.Header>
 
     {children}
