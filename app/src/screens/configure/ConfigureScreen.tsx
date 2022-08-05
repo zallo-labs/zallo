bc73f6a583290d580a65f41a3ad23312179e009e
Hayden Briese <haydenbriese@gmail.com>
2022-08-05 15:18:33 +1000

    Add quorum screen

--- app/src/screens/configure/ConfigureScreen.tsx
+++ app/src/screens/configure/ConfigureScreen.tsx
@@ -1,14 +1,17 @@
 import { Box } from '@components/Box';
 import { ScreenSkeleton } from '@components/skeleton/ScreenSkeleton';
 import { withSkeleton } from '@components/skeleton/withSkeleton';
+import { CheckIcon } from '@util/theme/icons';
 import { useTheme } from '@util/theme/paper';
-import { useCallback } from 'react';
+import _ from 'lodash';
+import { useMemo, useState } from 'react';
 import { FlatList } from 'react-native';
 import { Text } from 'react-native-paper';
 import { useAppbarHeader } from '~/components2/Appbar/useAppbarHeader';
+import { FAB } from '~/components2/FAB';
 import { useSetAccountName } from '~/mutations/account/useSetAccountName.api';
 import { RootNavigatorScreenProps } from '~/navigation/RootNavigator';
-import { AccountId } from '~/queries/accounts';
+import { AccountId, CombinedQuorum } from '~/queries/accounts';
 import { useAccount } from '~/queries/accounts/useAccount';
 import { QuorumCard } from '../../components2/QuorumCard';
 import { AccountNameInput } from './AccountNameInput';
@@ -27,37 +30,64 @@ export const ConfigureScreen = withSkeleton(
     const { space } = useTheme();
     const setName = useSetAccountName();
 
-    const handleNameChange = useCallback(
-      (name: string) => setName({ ...account, name }),
-      [account, setName],
+    const [quorums, setQuorums] = useState(account.quorums);
+
+    const isModified = useMemo(
+      () => !_.isEqual(account.quorums, quorums),
+      [account.quorums, quorums],
     );
 
+    const saveName = (name: string) => setName({ ...account, name });
+
+    const handleQuorumChange =
+      (quorum: CombinedQuorum) => (newQuorum?: CombinedQuorum) =>
+        setQuorums((quorums) => {
+          const withoutOld = quorums.filter((q) => q !== quorum);
+
+          return newQuorum ? [...withoutOld, newQuorum] : withoutOld;
+        });
+
     return (
-      <Box>
+      <Box flex={1}>
         <ConfigureAppbar account={account} AppbarHeader={AppbarHeader} />
 
         <FlatList
           ListHeaderComponent={
             <>
-              <AccountNameInput
-                value={account.name}
-                onSave={handleNameChange}
-              />
+              <AccountNameInput value={account.name} onSave={saveName} />
 
               <Box my={3}>
                 <Text variant="titleMedium">Quorums</Text>
               </Box>
             </>
           }
-          renderItem={({ item }) => (
-            <QuorumCard quorum={item} onPress={() => navigate('Quorum', {})} />
+          renderItem={({ item: quorum }) => (
+            <QuorumCard
+              quorum={quorum}
+              onPress={() =>
+                navigate('Quorum', {
+                  quorum,
+                  onChange: handleQuorumChange(quorum),
+                })
+              }
+            />
           )}
           ItemSeparatorComponent={() => <Box my={2} />}
           style={{ marginHorizontal: space(3) }}
-          data={account.quorums}
+          data={quorums}
           onScroll={handleScroll}
           showsVerticalScrollIndicator={false}
         />
+
+        {isModified && (
+          <FAB
+            icon={CheckIcon}
+            label="Apply"
+            onPress={() => {
+              // TODO: implement propose
+            }}
+          />
+        )}
       </Box>
     );
   },
