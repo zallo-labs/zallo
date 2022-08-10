da466e5a0e2df5ea86ad5199d087df22b27004d7
Hayden Briese <haydenbriese@gmail.com>
2022-08-10 11:52:43 +1000

    Allow FAB to be used in an appbar with a prop, removing the specialised component

--- app/src/components2/FAB.tsx
+++ app/src/components2/FAB.tsx
@@ -2,17 +2,36 @@ import { ComponentPropsWithoutRef } from 'react';
 import { StyleSheet } from 'react-native';
 import { FAB as Base } from 'react-native-paper';
 
-export type FABProps = ComponentPropsWithoutRef<typeof Base>;
+export type FABProps = ComponentPropsWithoutRef<typeof Base> & {
+  appbar?: boolean;
+};
 
-export const FAB = (props: FABProps) => (
-  <Base {...props} style={[styles.fab, props.style]} />
+export const FAB = ({ appbar, ...props }: FABProps) => (
+  <Base
+    {...props}
+    {...(appbar && {
+      size: 'small',
+      mode: 'flat',
+    })}
+    style={[
+      styles.bottom,
+      appbar ? styles.appbarMargin : styles.regularMargin,
+      props.style,
+    ]}
+  />
 );
 
 const styles = StyleSheet.create({
-  fab: {
+  bottom: {
     position: 'absolute',
     margin: 32,
     right: 0,
     bottom: 0,
+  },
+  regularMargin: {
+    margin: 32,
+  },
+  appbarMargin: {
+    margin: 16,
   },
 });
