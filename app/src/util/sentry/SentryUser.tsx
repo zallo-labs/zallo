d79572abc1e6ea5ad90ba3712c8302cc5e9b6bdf
Hayden Briese <haydenbriese@gmail.com>
2022-08-11 10:24:37 +1000

    Console log captured sentry info

--- app/src/util/sentry/SentryUser.tsx
+++ app/src/util/sentry/SentryUser.tsx
@@ -4,6 +4,7 @@ import { Native } from 'sentry-expo';
 export const SentryUser = () => {
   const device = useDevice();
   Native.setUser({ id: device.address });
+  console.debug('User', device.address);
 
   return null;
 };
--- app/src/util/sentry/sentry.ts
+++ app/src/util/sentry/sentry.ts
@@ -23,10 +23,25 @@ export const withSentry = <P>(RootComponent: React.ComponentType<P>) => {
   return Sentry.wrap(RootComponent);
 };
 
-export const {
-  Severity,
-  addBreadcrumb,
-  captureException,
-  captureEvent,
-  captureMessage,
-} = Sentry;
+export const captureException = (
+  ...params: Parameters<typeof Sentry.captureException>
+) => {
+  console.warn('Sentry.captureException', ...params);
+  return Sentry.captureException(...params);
+};
+
+export const captureEvent = (
+  ...params: Parameters<typeof Sentry.captureEvent>
+) => {
+  console.log('Sentry.captureEvent', ...params);
+  return Sentry.captureEvent(...params);
+};
+
+export const captureMessage = (
+  ...params: Parameters<typeof Sentry.captureMessage>
+) => {
+  console.log('Sentry.captureMessage', ...params);
+  return Sentry.captureMessage(...params);
+};
+
+export const { Severity, addBreadcrumb } = Sentry;
