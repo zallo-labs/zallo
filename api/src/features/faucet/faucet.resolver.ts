dbbc0b81d18077b1ec7b0432865a560c423d6d2d
Hayden Briese <haydenbriese@gmail.com>
2022-09-03 12:56:51 +1000

    Update faucet token addresses post testnet regenesis

--- api/src/features/faucet/faucet.resolver.ts
+++ api/src/features/faucet/faucet.resolver.ts
@@ -18,15 +18,15 @@ const ETH: TokenFaucet = {
 };
 
 const DAI: TokenFaucet = {
-  addr: address('0x6B175474E89094C44Da98b954EedeAC495271d0F'),
+  addr: address('0x9561634BD45CE00af2cddA94a2E21781d1DdbAdA'),
   amount: parseUnits('1', 18),
 };
 const USDC: TokenFaucet = {
-  addr: address('0x54a14D7559BAF2C8e8Fa504E019d32479739018c'),
+  addr: address('0x72c4F199CB8784425542583D345E7c00D642E345'),
   amount: parseUnits('1', 6),
 };
 const LINK: TokenFaucet = {
-  addr: address('0x4732C03B2CF6eDe46500e799DE79a15Df44929eB'),
+  addr: address('0x7EA4a22D3fc35C61b31cF1998608bDB1dF3638E9'),
   amount: parseUnits('1', 18),
 };
 
