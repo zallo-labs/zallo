8470b19890471732256afe37032ace20b07c7191
Hayden Briese <haydenbriese@gmail.com>
2022-09-09 23:46:54 +1000

    Reduce margin between cards on transaction screen

--- app/src/screens/transaction/TransactionScreen.tsx
+++ app/src/screens/transaction/TransactionScreen.tsx
@@ -30,7 +30,7 @@ export const TransactionScreen = withSkeleton(
           <Box flex={1} mx={2}>
             <Comments
               ListHeaderComponent={
-                <Container my={3} separator={<Box mt={2} />}>
+                <Container my={1} separator={<Box mt={2} />}>
                   <PotentialDisabledExecutionWarning />
                   <TransactionDetails />
                   <TransactionStatus />
