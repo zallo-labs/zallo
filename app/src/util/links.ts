46104bec9b0ae07bea272b700569fd082980201c
Hayden Briese <haydenbriese@gmail.com>
2022-11-10 14:18:16 +1100

    Update site and feedback links

--- app/src/util/links.ts
+++ app/src/util/links.ts
@@ -1,6 +1,6 @@
 export enum Link {
-  Site = 'https://allopay.io',
-  Feedback = 'mailto:feedback@allopay.io',
+  Site = 'https://zallo.io',
+  Feedback = 'mailto:feedback@zallo.io',
   Repo = 'https://github.com/zallo-labs/zallo',
   Issues = 'https://github.com/zallo-labs/zallo/issues',
   Icon = 'https://raw.githubusercontent.com/zallo-labs/zallo/dev/app/assets/icon-rounded.svg',
