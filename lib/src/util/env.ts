094fb00a1946dc2993870b112ad3126fa82477ea
Hayden Briese <haydenbriese@gmail.com>
2022-11-17 15:49:09 +1100

    Allow skipping required env checks, and use when generating api schema

--- lib/src/util/env.ts
+++ lib/src/util/env.ts
@@ -1,3 +1,5 @@
+export const SKIP_ENV_CHECK_KEY = 'SKIP_ENV_CHECK';
+
 const getKey = (strings: TemplateStringsArray, ...expr: string[]) =>
   strings.reduce((acc, str, i) => acc + str + (expr[i] || ''), '');
 
@@ -6,7 +8,11 @@ export const optionalEnv = (...params: Parameters<typeof getKey>) => process.env
 export const requiredEnv = (...params: Parameters<typeof getKey>) => {
   const key = getKey(...params);
   const value = process.env[key];
-  if (!value) throw new Error(`Missing environmental variable: ${key}`);
+
+  const skip = process.env[SKIP_ENV_CHECK_KEY]?.toLocaleLowerCase();
+  if (!value && skip !== 'true' && skip !== '1')
+    throw new Error(`Missing environmental variable: ${key}`);
+
   return value!;
 };
 
