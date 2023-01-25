5e490e02e16e72405405bece6e0d87ca050927fe
Hayden Briese <haydenbriese@gmail.com>
2023-01-25 23:08:56 +1100

    Use RNP Provider instead of ThemeProvider

--- app/src/screens/device/DeviceScreen.tsx
+++ app/src/screens/device/DeviceScreen.tsx
@@ -1,9 +1,8 @@
 import { EditIcon, ShareIcon } from '@theme/icons';
 import { makeStyles } from '@theme/makeStyles';
-import { useTheme } from '@theme/paper';
 import { useEffect, useMemo, useState } from 'react';
 import { Share } from 'react-native';
-import { Appbar, Menu, Provider, Text } from 'react-native-paper';
+import { Appbar, Menu, Text } from 'react-native-paper';
 import { AppbarBack } from '~/components/Appbar/AppbarBack';
 import { AppbarMore } from '~/components/Appbar/AppbarMore';
 import { ExpandableText } from '~/components/ExpandableText';
@@ -39,67 +38,65 @@ export const DeviceScreen = (_props: DeviceScreenProps) => {
   const url = useMemo(() => buildAddrLink({ target_address: id }), [id]);
 
   return (
-    <Provider theme={useTheme()}>
-      <Box flex={1}>
-        <Appbar.Header mode="small">
-          <AppbarBack />
-          <Appbar.Content title="" />
+    <Box flex={1}>
+      <Appbar.Header mode="small">
+        <AppbarBack />
+        <Appbar.Content title="" />
 
-          <Appbar.Action
-            icon={ShareIcon}
-            onPress={() =>
-              Share.share({
-                title: 'Device',
-                message: `${name}\n${url}`,
-                url,
-              })
-            }
-          />
+        <Appbar.Action
+          icon={ShareIcon}
+          onPress={() =>
+            Share.share({
+              title: 'Device',
+              message: `${name}\n${url}`,
+              url,
+            })
+          }
+        />
 
-          <AppbarMore>
-            {({ close }) => (
-              <Menu.Item
-                leadingIcon={EditIcon}
-                title="Rename device"
-                onPress={() => {
-                  close();
-                  setEditingName(true);
-                }}
-              />
-            )}
-          </AppbarMore>
-        </Appbar.Header>
+        <AppbarMore>
+          {({ close }) => (
+            <Menu.Item
+              leadingIcon={EditIcon}
+              title="Rename device"
+              onPress={() => {
+                close();
+                setEditingName(true);
+              }}
+            />
+          )}
+        </AppbarMore>
+      </Appbar.Header>
 
-        <Box alignItems="center" mx={4}>
-          {editingName ? (
-            <Box horizontal>
-              <TextField
-                label="Name"
-                value={nameValue}
-                onChangeText={setNameValue}
-                containerStyle={[styles.field, styles.name]}
-                onSubmitEditing={save}
-                onBlur={save}
-              />
-            </Box>
-          ) : (
-            <Text variant="displaySmall" style={[styles.text, styles.name]}>
-              {name}
+      <Box alignItems="center" mx={4}>
+        {editingName ? (
+          <Box horizontal>
+            <TextField
+              label="Name"
+              value={nameValue}
+              onChangeText={setNameValue}
+              containerStyle={[styles.field, styles.name]}
+              onSubmitEditing={save}
+              onBlur={save}
+            />
+          </Box>
+        ) : (
+          <Text variant="displaySmall" style={[styles.text, styles.name]}>
+            {name}
+          </Text>
+        )}
+
+        <QrCode value={url} />
+
+        <ExpandableText beginLen={6} endLen={4} value={id}>
+          {({ value }) => (
+            <Text variant="titleLarge" style={[styles.text, styles.address]}>
+              {value}
             </Text>
           )}
-
-          <QrCode value={url} />
-
-          <ExpandableText beginLen={6} endLen={4} value={id}>
-            {({ value }) => (
-              <Text variant="titleLarge" style={[styles.text, styles.address]}>
-                {value}
-              </Text>
-            )}
-          </ExpandableText>
-        </Box>
+        </ExpandableText>
       </Box>
-    </Provider>
+    </Box>
   );
 };
 
--- app/src/util/theme/ThemeProvider.tsx
+++ app/src/util/theme/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { ThemeProvider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { ThemeProvider as StyledComponentsProvider } from 'styled-components/native';
 import { PAPER_THEME } from './paper';
 import { STYLED_COMPONENTS_THEME } from './styledComponents';
