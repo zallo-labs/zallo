9ba2da4a1857c59ce64f05dbf30b2999e4151b35
Hayden Briese <haydenbriese@gmail.com>
2023-02-01 17:09:18 +1100

    Further restrict RLS and fix view permissions

--- api/prisma/migrations/20230126214157_security/migration.sql
+++ api/prisma/migrations/20230126214157_security/migration.sql
@@ -16,177 +16,174 @@ BEGIN
 END
 $$;
 
+-- Schema permissions
 GRANT USAGE ON SCHEMA public TO "user";
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public to "user";
 
 
 /* User functions */
 CREATE FUNCTION user_id() RETURNS text AS $$
-    SELECT current_setting('user.id', true);
+    SELECT current_setting('user.id', true)::text;
 $$ LANGUAGE SQL;
+GRANT EXECUTE ON FUNCTION user_id() TO "user";
 
 CREATE FUNCTION is_user(userId text) RETURNS boolean AS $$
     SELECT userId = (SELECT current_setting('user.id', true));
 $$ LANGUAGE SQL;
+GRANT EXECUTE ON FUNCTION is_user(text) TO "user";
 
-CREATE FUNCTION is_account_member(accountId text) RETURNS boolean AS $$
-    SELECT accountId = ANY((SELECT current_setting('user.accounts', true))::text[]);
+CREATE FUNCTION user_accounts() RETURNS text[] AS $$
+    SELECT current_setting('user.accounts', true)::text[];
 $$ LANGUAGE SQL;
+GRANT EXECUTE ON FUNCTION user_accounts TO "user";
+
+CREATE FUNCTION is_user_account(accountId text) RETURNS boolean AS $$
+    SELECT accountId = ANY(user_accounts());
+$$ LANGUAGE SQL;
+GRANT EXECUTE ON FUNCTION is_user_account(text) TO "user";
 
 
 
 /* User */
-GRANT SELECT, INSERT, UPDATE ("name", "pushToken") ON "User" TO "user";
+GRANT SELECT ("id", "name"), INSERT, UPDATE ("name", "pushToken") ON "User" TO PUBLIC;
 ALTER TABLE "User" ENABLE ROW LEVEL SECURITY;
 
--- Public can only INSERT the id
-CREATE POLICY public_insert ON "User" FOR INSERT 
+CREATE POLICY public_view ON "User" FOR SELECT
+    USING (true);
+
+CREATE POLICY public_insert ON "User" FOR INSERT
     WITH CHECK (
         "name"      IS NULL AND
         "pushToken" IS NULL
     );
 
--- User can do anything to themself
 CREATE POLICY user_all ON "User" FOR ALL
     USING (is_user("id"));
 
 
 
 /* Account */
-GRANT SELECT, INSERT, UPDATE ON "Account" TO "user";
+GRANT SELECT, INSERT, UPDATE ON "Account" TO PUBLIC;
 ALTER TABLE "Account" ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY public_insert ON "Account" FOR INSERT;
+CREATE POLICY member_all ON "Account" FOR ALL
+    USING (is_user_account("id"));
 
-CREATE POLICY member_view_or_update ON "Account" FOR UPDATE
-    USING (is_account_member("id"));
 
 
 /* Quorum */
-GRANT SELECT, INSERT, UPDATE ("key", "name", "activeStateId") ON "Quorum" TO "user";
+GRANT SELECT, INSERT, UPDATE ("key", "name", "activeStateId") ON "Quorum" TO PUBLIC;
 ALTER TABLE "Quorum" ENABLE ROW LEVEL SECURITY;
 
--- Account member can do anything
 CREATE POLICY account_member_all ON "Quorum" FOR ALL
-    USING (is_account_member("accountId"));
+    USING (is_user_account("accountId"));
 
 
 
 /* QuorumState */
-GRANT SELECT, INSERT ON "QuorumState" TO "user";
+GRANT SELECT, INSERT ON "QuorumState" TO PUBLIC;
 ALTER TABLE "QuorumState" ENABLE ROW LEVEL SECURITY;
 
--- Account member can do anything
 CREATE POLICY account_member_all ON "QuorumState" FOR ALL
-    USING (is_account_member("accountId"));
+    USING (is_user_account("accountId"));
 
 
 
 /* Approver */
-GRANT SELECT, INSERT ON "Approver" TO "user";
+GRANT SELECT, INSERT ON "Approver" TO PUBLIC;
 ALTER TABLE "Approver" ENABLE ROW LEVEL SECURITY;
 
--- Account member can do anything
 CREATE POLICY account_member_all ON "Approver" FOR ALL
-    USING (is_account_member(
+    USING (is_user_account(
         (SELECT "accountId" FROM "QuorumState" WHERE "id" = "quorumStateId")
     ));
 
 
 
 /* TokenLimit */
-GRANT SELECT, INSERT ON "TokenLimit" TO "user";
+GRANT SELECT, INSERT ON "TokenLimit" TO PUBLIC;
 ALTER TABLE "TokenLimit" ENABLE ROW LEVEL SECURITY;
 
--- Account member can do anything
 CREATE POLICY account_member_all ON "TokenLimit" FOR ALL
-    USING (is_account_member(
+    USING (is_user_account(
         (SELECT "accountId" FROM "QuorumState" WHERE "id" = "quorumStateId")
     ));
 
 
 
 /* Proposal */
-GRANT SELECT, INSERT, UPDATE, DELETE ON "Proposal" TO "user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON "Proposal" TO PUBLIC;
 ALTER TABLE "Proposal" ENABLE ROW LEVEL SECURITY;
 
--- Account members can access
 CREATE POLICY account_member_all ON "Proposal" FOR ALL
-    USING (is_account_member("accountId"));
+    USING (is_user_account("accountId"));
 
 
 
 /* Transaction */
-GRANT SELECT, INSERT ON "Transaction" TO "user";
+GRANT SELECT, INSERT ON "Transaction" TO PUBLIC;
 ALTER TABLE "Transaction" ENABLE ROW LEVEL SECURITY;
 
--- Account member can do anything
 CREATE POLICY account_member_all ON "Transaction" FOR ALL
-    USING (is_account_member(
+    USING (is_user_account(
         (SELECT "accountId" FROM "Proposal" WHERE "id" = "proposalId")
     ));
 
 
 
 /* TransactionResponse */
-GRANT SELECT ON "TransactionResponse" TO "user";
+GRANT SELECT ON "TransactionResponse" TO PUBLIC;
 
 
 
 /* Approval */
-GRANT SELECT, INSERT, UPDATE ("signature") ON "Approval" TO "user";
+GRANT SELECT, INSERT, UPDATE ("signature") ON "Approval" TO PUBLIC;
 ALTER TABLE "Approval" ENABLE ROW LEVEL SECURITY;
 
--- Account members can view
 CREATE POLICY account_member_view ON "Approval" FOR SELECT
-    USING (is_account_member(
+    USING (is_user_account(
         (SELECT "accountId" FROM "Proposal" WHERE "id" = "proposalId")
     ));
 
--- User can INSERT | UPDATE
 CREATE POLICY user_update ON "Approval" FOR UPDATE
     USING (is_user("userId"));
 
 
 
 /* Contact */
-GRANT SELECT, INSERT, UPDATE, DELETE ON "Contact" TO "user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON "Contact" TO PUBLIC;
 ALTER TABLE "Contact" ENABLE ROW LEVEL SECURITY;
 
--- User can do anything
 CREATE POLICY user_all ON "Contact" FOR ALL
     USING (is_user("userId"));
 
 
 
 /* ContractMethod */
-GRANT SELECT, INSERT, UPDATE ON "ContractMethod" TO "user";
+GRANT SELECT, INSERT, UPDATE ON "ContractMethod" TO PUBLIC;
 
 
 
 /* Comment */
-GRANT SELECT, INSERT, UPDATE ("content") ON "Comment" TO "user";
+GRANT SELECT, INSERT, UPDATE ("content") ON "Comment" TO PUBLIC;
 ALTER TABLE "Comment" ENABLE ROW LEVEL SECURITY;
 
--- Account members can view
 CREATE POLICY account_members_view ON "Comment" FOR SELECT
-    USING (is_account_member("accountId"));
+    USING (is_user_account("accountId"));
 
--- Author can do anything
 CREATE POLICY user_all ON "Comment" FOR ALL
     USING (is_user("authorId"));
 
 
 
 /* Reaction */
-GRANT SELECT, INSERT, UPDATE ("emojis"), DELETE ON "Reaction" TO "user";
+GRANT SELECT, INSERT, UPDATE ("emojis"), DELETE ON "Reaction" TO PUBLIC;
 ALTER TABLE "Reaction" ENABLE ROW LEVEL SECURITY;
 
--- Account members can view
 CREATE POLICY account_members_view ON "Reaction" FOR SELECT
-    USING (is_account_member(
+    USING (is_user_account(
         (SELECT "accountId" FROM "Comment" WHERE "id" = "commentId")
     ));
 
--- Author can do anything
 CREATE POLICY user_all ON "Reaction" FOR ALL
     USING (is_user("userId"));
