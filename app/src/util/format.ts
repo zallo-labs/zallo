1efe642f6074a6322378c7a487dac5a410b0674f
Hayden Briese <haydenbriese@gmail.com>
2023-02-07 14:59:35 +1100

    Truncate addresses to format 0x00...0000 for symmetry rather than 0x0000...0000

--- app/src/util/format.ts
+++ app/src/util/format.ts
@@ -5,4 +5,4 @@ export const elipseTruncate = (val: string, beginLen: number, endLen: number = b
     ? `${val.slice(0, beginLen)}...${val.slice(val.length - endLen)}`
     : val;
 
-export const truncateAddr = (addr: Address) => elipseTruncate(addr, 6, 4);
+export const truncateAddr = (addr: Address) => elipseTruncate(addr, 4, 4);
