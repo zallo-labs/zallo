8940e6ac52728610c87de75b9178ba3dc63a63b2
Hayden Briese <haydenbriese@gmail.com>
2023-02-12 21:57:23 +1100

    Don't decode revert data on success

--- contracts/contracts/Factory.sol
+++ contracts/contracts/Factory.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import {DEPLOYER_SYSTEM_CONTRACT} from '@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol';
-import {SystemContractsCaller} from '@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol';
-import {IContractDeployer} from '@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IContractDeployer.sol';
+import '@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol';
+import '@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol';
 
 contract Factory {
   IContractDeployer.AccountAbstractionVersion private constant AA_VERSION =
@@ -26,7 +25,7 @@ contract Factory {
       address(DEPLOYER_SYSTEM_CONTRACT),
       0,
       abi.encodeCall(
-        IContractDeployer.create2Account,
+        DEPLOYER_SYSTEM_CONTRACT.create2Account,
         (salt, _BYTECODE_HASH, constructorArgsData, AA_VERSION)
       )
     );
@@ -37,6 +36,6 @@ contract Factory {
       revert DeployFailed(account, revertData);
     }
 
-    (account, ) = abi.decode(data, (address, bytes));
+    (account) = abi.decode(data, (address));
   }
 }
