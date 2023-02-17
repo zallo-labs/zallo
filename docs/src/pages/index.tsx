d645590b0579d76191491ca04ea0c3fddc807f34
Hayden Briese <haydenbriese@gmail.com>
2023-02-17 16:35:52 +1100

    Add app store buttons and improve margins for different screen sizes

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -7,6 +7,8 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.scss';
 import Head from '@docusaurus/Head';
 import AppImage from '@site/static/img/app.png';
+import AppstoreBadgeSvg from '@site/static/img/app-store-badge.svg';
+import GooglePlayBadgePng from '@site/static/img/google-play-badge.png';
 
 const HomepageHeader = () => {
   const { siteConfig } = useDocusaurusContext();
@@ -18,13 +20,31 @@ const HomepageHeader = () => {
           <h1 className="display--small">{siteConfig.tagline}</h1>
           <p className="title--large">Secure your zkSync account without compromises</p>
 
-          <Link className="button button--lg" to="/docs/getting-started">
-            Developers - 5 min
-          </Link>
+          <div className={styles.developersContainer}>
+            <h6 className={clsx('label--large', styles.developersLabel)}>Developers</h6>
+
+            <Link className="button button--lg" to="/docs/getting-started">
+              Get started
+            </Link>
+          </div>
+
+          <div className={styles.storeButtonsContainer}>
+            <AppstoreBadgeSvg
+              role="img"
+              className={styles.storeButton}
+              onClick={() => alert('Coming soon')}
+            />
+
+            <img
+              src={GooglePlayBadgePng}
+              className={styles.storeButton}
+              onClick={() => alert('Coming soon')}
+            />
+          </div>
         </div>
 
         <div className="col col--4">
-          <img src={AppImage} alt="App" className={styles.app} />
+          <img src={AppImage} alt="App" className={styles.appImage} />
         </div>
       </div>
     </header>
