b6c64b267405072a628a7420b922ff8752d696b3
Hayden Briese <haydenbriese@gmail.com>
2023-02-24 16:32:17 +1100

    Refactor transaction request building

--- lib/src/execute.ts
+++ lib/src/execute.ts
@@ -5,13 +5,12 @@ import * as zk from 'zksync-web3';
 import { Eip712Meta, TransactionRequest } from 'zksync-web3/build/src/types';
 import { defaultAbiCoder } from 'ethers/lib/utils';
 import { EIP712_TX_TYPE } from 'zksync-web3/build/src/utils';
-import { TransactionStruct } from './contracts/Account';
 import { Quorum } from './quorum';
 import { Tx } from './tx';
 import { EMPTY_BYTES } from './bytes';
 import { tryOrAsync } from './util/try';
 
-const FALLBACK_BASE_GAS = BigNumber.from(500_000);
+export const FALLBACK_GAS_LIMIT = BigNumber.from(1_000_000);
 const GAS_PER_SIGNER = BigNumber.from(200_000);
 
 export interface ExecuteTxOptions {
@@ -34,53 +33,30 @@ export const toTransactionRequest = async ({
   opts = {},
 }: TransactionRequestOptions): Promise<TransactionRequest> => {
   const provider = account.provider;
-  const minimalTx = {
+  const req: TransactionRequest = {
     to: tx.to,
     value: tx.value,
     data: defaultAbiCoder.encode(['bytes8', 'bytes'], [tx.salt, tx.data ?? EMPTY_BYTES]),
-  } satisfies TransactionRequest;
-
-  const opGas =
-    tx.gasLimit || (await tryOrAsync(() => provider.estimateGas(minimalTx), FALLBACK_BASE_GAS));
-  const gasLimit = opGas.add(GAS_PER_SIGNER.mul(signers.length));
-
-  return {
-    ...minimalTx,
     from: account.address,
     type: EIP712_TX_TYPE,
     nonce: await provider.getTransactionCount(account.address),
     chainId: (await provider.getNetwork()).chainId,
     gasPrice: await provider.getGasPrice(),
-    gasLimit,
     customData: {
       gasPerPubdata: zk.utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
       ...opts.customData,
       customSignature: toAccountSignature(quorum, signers),
     },
   };
+
+  const opGas =
+    tx.gasLimit || (await tryOrAsync(() => provider.estimateGas(req), FALLBACK_GAS_LIMIT));
+  req.gasLimit = opGas.add(GAS_PER_SIGNER.mul(signers.length));
+
+  return req;
 };
 
 export const executeTx = async (opts: TransactionRequestOptions) => {
   const req = await toTransactionRequest(opts);
   return opts.account.provider.sendTransaction(zk.utils.serialize(req));
 };
-
-// For external transactions
-export const toTransactionStruct = (r: TransactionRequest): TransactionStruct => ({
-  txType: r.type!,
-  from: r.from!,
-  to: r.to!,
-  value: r.value!,
-  nonce: r.nonce!,
-  data: r.data!,
-  reserved: [r.nonce!, r.value!, 0, 0],
-  signature: r.customData!.customSignature!,
-  gasLimit: r.gasLimit!,
-  gasPerPubdataByteLimit: r.gasPrice ?? 1,
-  maxFeePerGas: r.maxFeePerGas ?? 1,
-  maxPriorityFeePerGas: r.maxPriorityFeePerGas ?? 1,
-  paymaster: 0,
-  factoryDeps: [],
-  paymasterInput: '0x',
-  reservedDynamic: '0x',
-});
