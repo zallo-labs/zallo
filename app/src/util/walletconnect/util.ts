9452e7a0ddee094572a4c4317c1812e851aa5ec0
Hayden Briese <haydenbriese@gmail.com>
2023-03-10 12:28:03 +1100

    Remove unused files

--- app/src/util/walletconnect/util.ts
+++
@@ -1,6 +0,0 @@
-import { SignClient } from '@walletconnect/sign-client/dist/types/client';
-import Connector from '@walletconnect/client';
-
-export type WcClient = SignClient | Connector;
-
-export const isClientV2 = (client: WcClient): client is SignClient => client.version === 2;
