13b676938059061cf06329452136f8278a681139
Hayden Briese <haydenbriese@gmail.com>
2023-03-17 14:14:03 +1100

    Refactor

--- app/src/components/addr/useAddrEns.ts
+++ app/src/components/addr/useAddrEns.ts
@@ -1,18 +1,12 @@
 import { persistAtom } from '~/util/effect/persistAtom';
 import { refreshAtom } from '~/util/effect/refreshAtom';
 import { PROVIDER } from '~/util/network/provider';
-import { Address } from 'lib';
+import { Address, tryOrAsync } from 'lib';
 import { Duration } from 'luxon';
 import { atomFamily, useRecoilValue } from 'recoil';
 
-const fetch = async (addr: Address | null) => {
-  try {
-    return addr ? await PROVIDER.lookupAddress(addr) : null;
-  } catch (e) {
-    // Network doesn't support ENS
-    return null;
-  }
-};
+const fetch = async (addr: Address | null) =>
+  addr ? tryOrAsync(() => PROVIDER.lookupAddress(addr), null) : null;
 
 const addrEnsState = atomFamily<string | null, Address | null>({
   key: 'addrEns',
