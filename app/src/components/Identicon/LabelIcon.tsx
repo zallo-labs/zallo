e2230852712bb379bb85578cfd2c784cf6e9ffa2
Hayden Briese <haydenbriese@gmail.com>
2023-03-19 18:38:41 +1100

    Use tertiary color for LabelIcon

--- app/src/components/Identicon/LabelIcon.tsx
+++ app/src/components/Identicon/LabelIcon.tsx
@@ -1,13 +1,32 @@
-import { useTheme } from '@theme/paper';
-import { useMemo } from 'react';
+import { makeStyles } from '@theme/makeStyles';
 import { Avatar, AvatarTextProps } from 'react-native-paper';
 
 export interface LabelIconProps extends Omit<AvatarTextProps, 'theme'> {}
 
 export const LabelIcon = ({ label: l, ...props }: LabelIconProps) => {
-  const { iconSize } = useTheme();
+  const styles = useStyles();
 
-  const label = useMemo(() => l.slice(0, Math.min(l?.length, 1)).toUpperCase(), [l]);
+  const label = l.slice(0, Math.min(l?.length, 1)).toUpperCase();
 
-  return <Avatar.Text size={iconSize.medium} label={label} {...props} />;
+  return (
+    <Avatar.Text
+      size={styles.size.fontSize}
+      label={label}
+      style={styles.container}
+      labelStyle={styles.label}
+      {...props}
+    />
+  );
 };
+
+const useStyles = makeStyles(({ colors, iconSize }) => ({
+  container: {
+    backgroundColor: colors.tertiaryContainer,
+  },
+  label: {
+    color: colors.tertiary,
+  },
+  size: {
+    fontSize: iconSize.medium,
+  },
+}));
