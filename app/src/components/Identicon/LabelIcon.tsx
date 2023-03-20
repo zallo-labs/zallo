ebbbefc0e6133862487eeb68f7813b4912a107a9
Hayden Briese <haydenbriese@gmail.com>
2023-03-20 14:27:18 +1100

    Replace LabelIcon with custom implementation that supports arbitary colors

--- app/src/components/Identicon/LabelIcon.tsx
+++ app/src/components/Identicon/LabelIcon.tsx
@@ -1,32 +1,45 @@
 import { makeStyles } from '@theme/makeStyles';
-import { Avatar, AvatarTextProps } from 'react-native-paper';
+import { memo } from 'react';
+import { StyleProp, TextStyle, View, ViewProps, ViewStyle } from 'react-native';
+import { Text } from 'react-native-paper';
 
-export interface LabelIconProps extends Omit<AvatarTextProps, 'theme'> {}
+export interface LabelIconProps extends Omit<ViewProps, 'style'> {
+  label: string;
+  size?: number;
+  containerStyle?: StyleProp<ViewStyle>;
+  labelStyle?: StyleProp<TextStyle>;
+}
 
-export const LabelIcon = ({ label: l, ...props }: LabelIconProps) => {
-  const styles = useStyles();
+export const LabelIcon = memo(
+  ({ label: l, size, containerStyle, labelStyle, ...viewProps }: LabelIconProps) => {
+    const styles = useStyles(size);
 
-  const label = l.slice(0, Math.min(l?.length, 1)).toUpperCase();
+    const label = l.slice(0, Math.min(l?.length, 1)).toUpperCase();
 
-  return (
-    <Avatar.Text
-      size={styles.size.fontSize}
-      label={label}
-      style={styles.container}
-      labelStyle={styles.label}
-      {...props}
-    />
-  );
-};
+    return (
+      <View {...viewProps} style={[styles.container, containerStyle]}>
+        <Text style={[styles.label, labelStyle]} numberOfLines={1}>
+          {label}
+        </Text>
+      </View>
+    );
+  },
+);
 
-const useStyles = makeStyles(({ colors, iconSize }) => ({
+const useStyles = makeStyles(({ colors, iconSize, window }, size: number = iconSize.medium) => ({
   container: {
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: colors.tertiaryContainer,
   },
   label: {
+    fontSize: size / 2,
+    lineHeight: size / window.fontScale,
     color: colors.tertiary,
-  },
-  size: {
-    fontSize: iconSize.medium,
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
 }));
