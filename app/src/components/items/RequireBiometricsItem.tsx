2ecf00cb78cbaf5774f8aa569b778bdeb335b9d9
Hayden Briese <haydenbriese@gmail.com>
2023-03-23 16:48:32 +1100

    Show biometrics setting item as disabled when not supported rather than not at all

--- app/src/components/items/RequireBiometricsItem.tsx
+++ app/src/components/items/RequireBiometricsItem.tsx
@@ -1,22 +1,31 @@
 import { materialIcon } from '@theme/icons';
+import { makeStyles } from '@theme/makeStyles';
 import { Switch } from 'react-native-paper';
 import { useRecoilState } from 'recoil';
 import { ListItem } from '~/components/list/ListItem';
 import { authSettingsAtom, useSupportsBiometrics } from '~/provider/AuthGate';
 
+const FingerprintIcon = materialIcon('fingerprint');
+
 export const RequireBiometricsItem = () => {
   const [{ requireBiometrics }, setSettings] = useRecoilState(authSettingsAtom);
 
+  const disabled = !useSupportsBiometrics();
   const toggle = () => setSettings((s) => ({ ...s, requireBiometrics: !s.requireBiometrics }));
-
-  if (!useSupportsBiometrics()) return null;
 
   return (
     <ListItem
-      leading={materialIcon('fingerprint')}
+      leading={(props) => <FingerprintIcon {...props} size={useStyles().icon.fontSize} />}
       headline="Biometrics"
       supporting="Require biometrics when opening the app"
-      trailing={<Switch value={requireBiometrics} onValueChange={toggle} />}
+      trailing={<Switch value={requireBiometrics} onValueChange={toggle} disabled={disabled} />}
+      disabled={disabled}
     />
   );
 };
+
+const useStyles = makeStyles(({ iconSize }) => ({
+  icon: {
+    fontSize: iconSize.medium,
+  },
+}));
