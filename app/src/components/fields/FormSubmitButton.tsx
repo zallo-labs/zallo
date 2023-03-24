5477867a4508e07b17b89881f86ee05c1b4cfabd
Hayden Briese <haydenbriese@gmail.com>
2023-03-24 14:10:09 +1100

    Use async supporting Button for FormSubmitButton

--- app/src/components/fields/FormSubmitButton.tsx
+++ app/src/components/fields/FormSubmitButton.tsx
@@ -1,16 +1,26 @@
 import { FieldValues, useFormState, UseFormStateProps } from 'react-hook-form';
-import { Button, ButtonProps } from 'react-native-paper';
+import { Button, ButtonProps } from '~/components/Button';
 
 export interface FormSubmitButtonProps<TFieldValues extends FieldValues>
   extends Pick<UseFormStateProps<TFieldValues>, 'control'>,
-    ButtonProps {}
+    ButtonProps {
+  requireChanges?: boolean;
+}
 
 export const FormSubmitButton = <TFieldValues extends FieldValues = FieldValues>({
+  requireChanges,
   control,
   disabled,
   ...props
 }: FormSubmitButtonProps<TFieldValues>) => {
-  const { isValid, isSubmitting, isSubmitted } = useFormState({ control });
+  const { isValid, isSubmitting, isSubmitted, isDirty } = useFormState({ control });
 
-  return <Button {...props} disabled={disabled || (isSubmitted && !isValid) || isSubmitting} />;
+  return (
+    <Button
+      {...props}
+      disabled={
+        disabled || isSubmitting || (isSubmitted && !isValid) || (requireChanges && !isDirty)
+      }
+    />
+  );
 };
