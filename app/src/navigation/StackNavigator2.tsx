81e4a3228f30171058185e37ef0040e5c9f29155
Hayden Briese <haydenbriese@gmail.com>
2023-03-24 16:19:21 +1100

    Sort stack navigator screens

--- app/src/navigation/StackNavigator2.tsx
+++ app/src/navigation/StackNavigator2.tsx
@@ -4,49 +4,49 @@ import {
   StackScreenProps,
   TransitionPresets,
 } from '@react-navigation/stack';
+import { AccountsSheet } from '~/screens/AccountsSheet/AccountsSheet';
+import { AddressSheet, AddressSheetScreenParams } from '~/screens/AddressSheet/AddressSheet';
+import { AlertModal, AlertModalParams } from '~/screens/alert/AlertModal';
+import { ContactScreen, ContactScreenParams } from '~/screens/Contact/ContactScreen';
+import { ContactsScreen, ContactsScreenParams } from '~/screens/contacts/ContactsScreen';
 import { CreateAccountScreen } from '~/screens/CreateAccount/CreateAccountScreen';
 import { CreateUserScreen } from '~/screens/CreateUser/CreateUserScreen';
-import { AccountsSheet } from '~/screens/AccountsSheet/AccountsSheet';
 import { HomeScreen } from '~/screens/Home/HomeScreen';
 import { OnboardScreen } from '~/screens/Onboard/OnboardScreen';
 import { useShowOnboarding } from '~/screens/Onboard/useShowOnboarding';
-import { ScanScreen, ScanScreenParams } from '~/screens/Scan/ScanScreen';
 import { ProposalScreen, ProposalScreenParams } from '~/screens/Proposal/ProposalScreen2';
-import { SendScreen, SendScreenParams } from '~/screens/Send/SendScreen2';
-import { ContactsScreen, ContactsScreenParams } from '~/screens/contacts/ContactsScreen';
-import { AddressSheet, AddressSheetScreenParams } from '~/screens/AddressSheet/AddressSheet';
-import { ContactScreen, ContactScreenParams } from '~/screens/Contact/ContactScreen';
 import { QrModalScreen, QrModalScreenParams } from '~/screens/QrModal/QrModalScreen';
-import { SettingsScreen } from '~/screens/Settings/SettingsScreen';
+import { ScanScreen, ScanScreenParams } from '~/screens/Scan/ScanScreen';
+import { SendScreen, SendScreenParams } from '~/screens/Send/SendScreen2';
 import { SessionsScreen } from '~/screens/sessions/SessionsScreen';
+import { SettingsScreen } from '~/screens/Settings/SettingsScreen';
 import { TokensScreen, TokensScreenParams } from '~/screens/tokens/TokensScreen';
-import { AlertModal, AlertModalParams } from '~/screens/alert/AlertModal';
 import { UserScreen } from '~/screens/User/UserScreen';
 
 export type StackNavigatorParamList = {
-  // Onboarding
-  Onboard: undefined;
-  CreateUser: undefined;
-  // Home
   Home: undefined;
-  AccountsSheet: undefined;
+  CreateAccount: undefined;
   Scan: ScanScreenParams;
-  AddressSheet: AddressSheetScreenParams;
   Proposal: ProposalScreenParams;
   Send: SendScreenParams;
   Contacts: ContactsScreenParams;
-  ContactsModal: ContactsScreenParams;
   Contact: ContactScreenParams;
-  QrModal: QrModalScreenParams;
-  Alert: AlertModalParams;
-  // Settings
   Settings: undefined;
   User: undefined;
   Sessions: undefined;
   Tokens: TokensScreenParams;
-  TokensModal: TokensScreenParams;
+  // Onboarding
+  Onboard: undefined;
+  CreateUser: undefined;
   // Account
-  CreateAccount: undefined;
+  // Transparent modal
+  AccountsSheet: undefined;
+  AddressSheet: AddressSheetScreenParams;
+  QrModal: QrModalScreenParams;
+  Alert: AlertModalParams;
+  // Card modal
+  ContactsModal: ContactsScreenParams;
+  TokensModal: TokensScreenParams;
 };
 
 export type StackNavigatorNavigationProp = StackNavigationProp<StackNavigatorParamList>;
@@ -71,18 +71,16 @@ export const StackNavigator = () => {
       )}
 
       <Navigation.Screen name="Home" component={HomeScreen} />
+      <Navigation.Screen name="CreateAccount" component={CreateAccountScreen} />
       <Navigation.Screen name="Scan" component={ScanScreen} />
       <Navigation.Screen name="Proposal" component={ProposalScreen} />
       <Navigation.Screen name="Send" component={SendScreen} />
       <Navigation.Screen name="Contacts" component={ContactsScreen} />
       <Navigation.Screen name="Contact" component={ContactScreen} />
-
       <Navigation.Screen name="Settings" component={SettingsScreen} />
       <Navigation.Screen name="User" component={UserScreen} />
       <Navigation.Screen name="Sessions" component={SessionsScreen} />
       <Navigation.Screen name="Tokens" component={TokensScreen} />
-
-      <Navigation.Screen name="CreateAccount" component={CreateAccountScreen} />
 
       <Navigation.Group screenOptions={{ presentation: 'transparentModal' }}>
         <Navigation.Screen name="AccountsSheet" component={AccountsSheet} />
