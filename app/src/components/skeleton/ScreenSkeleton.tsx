c5b5584f1d11d06b7169642d59f823206e6c34fa
Hayden Briese <haydenbriese@gmail.com>
2023-03-27 17:57:57 +1100

    Show spinner for ScreenSkeleton if no custom layout is defined

--- app/src/components/skeleton/ScreenSkeleton.tsx
+++ app/src/components/skeleton/ScreenSkeleton.tsx
@@ -1,26 +1,37 @@
 import { ReactNode } from 'react';
-import { View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Appbar } from 'react-native-paper';
 import { AppbarBack } from '~/components/Appbar/AppbarBack';
 import { AppbarHeaderProps } from '~/components/Appbar/useAppbarHeader';
-import { MenuIcon } from '~/util/theme/icons';
+import { Screen } from '../layout/Screen';
 import { LineSkeleton } from './LineSkeleton';
 
 export interface ScreenSkeletonProps {
   children?: ReactNode;
   mode?: AppbarHeaderProps['mode'];
-  menu?: boolean;
   title?: boolean;
 }
 
-export const ScreenSkeleton = ({ children, mode, menu, title = true }: ScreenSkeletonProps) => (
-  <View>
+export const ScreenSkeleton = ({ children, mode, title = true }: ScreenSkeletonProps) => (
+  <Screen withoutTopInset>
     <Appbar.Header mode={mode}>
-      {menu ? <Appbar.Action icon={MenuIcon} /> : <AppbarBack />}
+      <AppbarBack />
 
       {title && <Appbar.Content title={<LineSkeleton width={150} />} />}
     </Appbar.Header>
 
-    {children}
-  </View>
+    {children || (
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator />
+      </View>
+    )}
+  </Screen>
 );
+
+const styles = StyleSheet.create({
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
