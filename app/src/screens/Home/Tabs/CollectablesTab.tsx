744d979dd46af4df675fcdfb7d925a47952c6515
Hayden Briese <haydenbriese@gmail.com>
2023-03-30 15:19:00 +1100

    Disable collectables tab and show coming soon message

--- app/src/screens/Home/Tabs/CollectablesTab.tsx
+++ app/src/screens/Home/Tabs/CollectablesTab.tsx
@@ -1,23 +1,10 @@
 import { StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
 import { withSuspense } from '~/components/skeleton/withSuspense';
 import { TabScreenSkeleton } from '~/components/tab/TabScreenSkeleton';
 import { TabNavigatorScreenProp } from '.';
 
 export type CollectablesTabProps = TabNavigatorScreenProp<'Collectables'>;
 
-export const CollectablesTab = withSuspense(
-  () => (
-    <Text variant="bodyLarge" style={styles.text}>
-      Coming soon!
-    </Text>
-  ),
-  TabScreenSkeleton,
-);
+export const CollectablesTab = withSuspense(() => null, TabScreenSkeleton);
 
-const styles = StyleSheet.create({
-  text: {
-    alignSelf: 'center',
-    margin: 16,
-  },
-});
+const styles = StyleSheet.create({});
