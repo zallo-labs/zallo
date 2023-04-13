3bf69f1ceffe38a7e5c194e5e010545ccd1bb766
Hayden Briese <haydenbriese@gmail.com>
2023-04-13 14:58:22 +1000

    Substitue '*' when converting targes to struct, filter out empty fallback target and extend asTargets params

--- lib/src/permissions/TargetPermission.ts
+++ lib/src/permissions/TargetPermission.ts
@@ -9,22 +9,38 @@ import { PermissionStruct } from './permissions';
 import _ from 'lodash';
 import { BytesLike } from 'ethers';
 
-export type TargetPermission = Record<Address | '*', Set<Selector | '*'>>;
+export type Target = Address | '*';
+export type TargetPermission = Record<Target, Set<Selector | '*'>>;
 
-export const asTargets = (
-  entries?: { to: Addresslike | '*'; selectors: Arraylike<BytesLike | '*'> }[],
-): TargetPermission =>
-  entries
-    ? {
-        '*': new Set([]),
-        ...Object.fromEntries(
-          entries.map(({ to, selectors }) => [
-            to === '*' ? to : asAddress(to),
-            new Set(toArray(selectors).map((s) => (s === '*' ? s : asSelector(s)))),
-          ]),
+export type Targetlike = Addresslike | '*';
+export type Targetslike =
+  | Partial<Record<Targetlike, Arraylike<BytesLike | '*'>>>
+  | { to: Targetlike; selectors: Arraylike<BytesLike | '*'> }[];
+
+export const asTargets = (targets?: Targetslike): TargetPermission => {
+  if (!targets) return DEFAULT_TARGETS;
+
+  const entries: { to: Targetlike; selectors: Arraylike<BytesLike | '*'> }[] = Array.isArray(
+    targets,
+  )
+    ? targets
+    : Object.entries(targets).map(([to, selectors]) => ({
+        to,
+        selectors: selectors ?? [],
+      }));
+
+  return {
+    '*': new Set([]),
+    ...Object.fromEntries(
+      entries.map(({ to, selectors }) => [
+        to === '*' || to === FALLBACK_ADDRESS ? '*' : asAddress(to),
+        new Set(
+          toArray(selectors).map((s) => (s === '*' || s === ANY_SELECTOR ? s : asSelector(s))),
         ),
-      }
-    : DEFAULT_TARGETS;
+      ]),
+    ),
+  };
+};
 
 const FALLBACK_ADDRESS = ZERO_ADDR;
 const ANY_SELECTOR = '0x00000000' as Selector;
@@ -40,28 +56,16 @@ export const TARGET_PERMISSION_ABI = newAbiType<
   '(address to, bytes4[] selectors)[]',
   (targets) =>
     Object.entries(targets)
-      .map(
-        ([address, selectors]): AwaitedObj<TargetStruct> => ({
-          to: asAddress(address),
-          selectors: [...selectors].sort(compareBytes),
-        }),
-      )
+      .map(([target, selectors]): AwaitedObj<TargetStruct> => {
+        return {
+          to: target === '*' ? FALLBACK_ADDRESS : asAddress(target),
+          selectors: [...selectors].map((s) => (s === '*' ? ANY_SELECTOR : s)).sort(compareBytes),
+        };
+      })
+      // Remove fallback target without selectors; it is equivalent to having no fallback
+      .filter(({ to, selectors }) => to !== FALLBACK_ADDRESS || selectors.length > 0)
       .sort((a, b) => compareAddress(a.to, b.to)),
-  (targetStructs) =>
-    (targetStructs ?? []).reduce(
-      (acc, { to, selectors }) => ({
-        ...acc,
-        [to === FALLBACK_ADDRESS ? '*' : to]: new Set(
-          selectors.map((s) => {
-            const selector = asSelector(s);
-            return selector !== ANY_SELECTOR ? selector : '*';
-          }),
-        ),
-      }),
-      {
-        '*': new Set([]),
-      },
-    ),
+  (targetStructs) => asTargets(targetStructs),
 );
 
 export const permissionAsTargets = (p: PermissionStruct | undefined) =>
