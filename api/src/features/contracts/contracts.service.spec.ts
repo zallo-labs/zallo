a6e1a3f410d2a77cc0f23df4808e49ddf7507fd6
Hayden Briese <haydenbriese@gmail.com>
2023-04-13 16:03:30 +1000

    Fix Contracts service test

--- api/src/features/contracts/contracts.service.spec.ts
+++ api/src/features/contracts/contracts.service.spec.ts
@@ -1,13 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContractsService } from './contracts.service';
+import { createMock } from '@golevelup/ts-jest';
+import { PRISMA_MOCK_PROVIDER } from '../util/prisma/prisma.service.mock';
 
 describe(ContractsService.name, () => {
   let service: ContractsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContractsService],
-    }).compile();
+      providers: [ContractsService, PRISMA_MOCK_PROVIDER],
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<ContractsService>(ContractsService);
   });
