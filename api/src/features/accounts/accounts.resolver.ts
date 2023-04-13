b7797907384af5f1f88b4e83c3ad7686b71d204a
Hayden Briese <haydenbriese@gmail.com>
2023-04-13 16:03:12 +1000

    Move accounts resolver logic to service

--- api/src/features/accounts/accounts.resolver.ts
+++ api/src/features/accounts/accounts.resolver.ts
@@ -1,5 +1,4 @@
 import { Args, Info, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { PrismaService } from '../util/prisma/prisma.service';
 import { GraphQLResolveInfo } from 'graphql';
 import { Account } from '@gen/account/account.model';
 import {
@@ -10,37 +9,22 @@ import {
   AccountSubscriptionFilters,
   AccountSubscriptionPayload,
   ACCOUNT_SUBSCRIPTION,
-  AccountEvent,
   USER_ACCOUNT_SUBSCRIPTION,
 } from './accounts.args';
 import { getSelect } from '~/util/select';
 import { AccountsService } from './accounts.service';
-import { ProviderService } from '~/features/util/provider/provider.service';
-import { asPolicyKey, calculateProxyAddress, randomDeploySalt } from 'lib';
-import { CONFIG } from '~/config';
 import { UserId } from '~/decorators/user.decorator';
-import { FaucetService } from '../faucet/faucet.service';
 import { PubsubService } from '../util/pubsub/pubsub.service';
-import { UserContext, getRequestContext, getUser } from '~/request/ctx';
-import { Prisma } from '@prisma/client';
-import { inputAsPolicy, policyAsCreateState } from '../policies/policies.util';
-import { ContractsService } from '../contracts/contracts.service';
+import { UserContext } from '~/request/ctx';
 
 @Resolver(() => Account)
 export class AccountsResolver {
-  constructor(
-    private service: AccountsService,
-    private prisma: PrismaService,
-    private provider: ProviderService,
-    private contracts: ContractsService,
-    private faucet: FaucetService,
-    private pubsub: PubsubService,
-  ) {}
+  constructor(private service: AccountsService, private pubsub: PubsubService) {}
 
   @Query(() => Account, { nullable: true })
   async account(
     @Args() { id }: AccountArgs,
-    @Info() info?: GraphQLResolveInfo,
+    @Info() info: GraphQLResolveInfo,
   ): Promise<Account | null> {
     return this.service.findUnique({
       where: { id },
@@ -49,10 +33,7 @@ export class AccountsResolver {
   }
 
   @Query(() => [Account])
-  async accounts(
-    @Args() args: AccountsArgs,
-    @Info() info?: GraphQLResolveInfo,
-  ): Promise<Account[]> {
+  async accounts(@Args() args: AccountsArgs, @Info() info: GraphQLResolveInfo): Promise<Account[]> {
     return this.service.findMany({
       ...args,
       ...getSelect(info),
@@ -89,90 +70,17 @@ export class AccountsResolver {
 
   @Mutation(() => Account)
   async createAccount(
-    @Args('args') { name, policies: policyInputs }: CreateAccountInput,
-    @Info() info?: GraphQLResolveInfo,
+    @Args('args') input: CreateAccountInput,
+    @Info() info: GraphQLResolveInfo,
   ): Promise<Account> {
-    const impl = CONFIG.accountImplAddress;
-    const deploySalt = randomDeploySalt();
-
-    const policies = policyInputs.map((p, i) => ({
-      ...inputAsPolicy(asPolicyKey(i), p),
-      name: p.name || `Policy ${i}`,
-    }));
-
-    const account = await this.provider.useProxyFactory((factory) =>
-      calculateProxyAddress({ impl, policies }, factory, deploySalt),
-    );
-
-    // Add account to user context
-    getUser().accounts.add(account);
-    getRequestContext()?.session?.accounts?.push(account);
-
-    await this.prisma.asUser.$transaction(async (prisma) => {
-      const { policyStates } = await prisma.account.create({
-        data: {
-          id: account,
-          deploySalt,
-          impl,
-          name,
-          policies: {
-            create: policies.map(
-              (policy): Prisma.PolicyCreateWithoutAccountInput => ({
-                key: policy.key,
-                name: policy.name,
-                states: {
-                  create: {
-                    ...policyAsCreateState(policy),
-                  },
-                },
-              }),
-            ),
-          },
-        },
-        select: {
-          policyStates: {
-            select: {
-              id: true,
-              policyKey: true,
-            },
-          },
-        },
-      });
-
-      // Set policy rules as draft of policies - this can't be done in the same create as the account )':
-      await Promise.all(
-        policyStates.map(({ id, policyKey }) =>
-          prisma.policy.update({
-            where: { accountId_key: { accountId: account, key: policyKey } },
-            data: { draftId: id },
-            select: null,
-          }),
-        ),
-      );
-    });
-
-    const r = await this.service.activateAccount(account, { ...getSelect(info) });
-
-    this.service.publishAccount({ account: { id: account }, event: AccountEvent.create });
-    await this.contracts.addAccountAsVerified(account);
-    this.faucet.requestTokens(account);
-
-    return r;
+    return this.service.createAccount(input, getSelect(info));
   }
 
   @Mutation(() => Account)
   async updateAccount(
-    @Args('args') { id, name }: UpdateAccountInput,
-    @Info() info?: GraphQLResolveInfo,
+    @Args('args') input: UpdateAccountInput,
+    @Info() info: GraphQLResolveInfo,
   ): Promise<Account> {
-    const r = await this.prisma.asUser.account.update({
-      where: { id },
-      data: { name },
-      ...getSelect(info),
-    });
-
-    this.service.publishAccount({ account: { id }, event: AccountEvent.update });
-
-    return r;
+    return this.service.updateAccount(input, getSelect(info));
   }
 }
--- api/src/features/accounts/accounts.service.ts
+++ api/src/features/accounts/accounts.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { asAddress, Address, deployAccountProxy, toDeploySalt } from 'lib';
+import {
+  asAddress,
+  Address,
+  deployAccountProxy,
+  toDeploySalt,
+  randomDeploySalt,
+  asPolicyKey,
+  calculateProxyAddress,
+} from 'lib';
 import { PrismaService } from '../util/prisma/prisma.service';
 import { ProviderService } from '~/features/util/provider/provider.service';
 import { PubsubService } from '../util/pubsub/pubsub.service';
@@ -9,8 +17,21 @@ import {
   AccountSubscriptionPayload,
   ACCOUNT_SUBSCRIPTION,
   USER_ACCOUNT_SUBSCRIPTION,
+  AccountEvent,
+  CreateAccountInput,
+  UpdateAccountInput,
 } from './accounts.args';
-import { POLICY_STATE_FIELDS, prismaAsPolicy } from '../policies/policies.util';
+import {
+  POLICY_STATE_FIELDS,
+  getDefaultPolicyName,
+  inputAsPolicy,
+  policyAsCreateState,
+  prismaAsPolicy,
+} from '../policies/policies.util';
+import { CONFIG } from '~/config';
+import { getUser, getRequestContext } from '~/request/ctx';
+import { ContractsService } from '../contracts/contracts.service';
+import { FaucetService } from '../faucet/faucet.service';
 
 @Injectable()
 export class AccountsService {
@@ -18,14 +39,107 @@ export class AccountsService {
     private prisma: PrismaService,
     private provider: ProviderService,
     private pubsub: PubsubService,
+    private contracts: ContractsService,
+    private faucet: FaucetService,
   ) {}
 
   findUnique = this.prisma.asUser.account.findUnique;
   findMany = this.prisma.asUser.account.findMany;
 
-  async activateAccount<T extends Pick<Prisma.AccountUpdateArgs, 'select'>>(
+  async createAccount<R extends Prisma.AccountArgs>(
+    { name, policies: policyInputs }: CreateAccountInput,
+    res?: Prisma.SelectSubset<R, Prisma.AccountArgs>,
+  ) {
+    const impl = CONFIG.accountImplAddress;
+    const deploySalt = randomDeploySalt();
+
+    const policies = policyInputs.map((p, i) => {
+      const key = asPolicyKey(i);
+
+      return {
+        ...inputAsPolicy(key, p),
+        name: p.name || getDefaultPolicyName(key),
+      };
+    });
+
+    const account = await this.provider.useProxyFactory((factory) =>
+      calculateProxyAddress({ impl, policies }, factory, deploySalt),
+    );
+
+    // Add account to user context
+    getUser().accounts.add(account);
+    getRequestContext()?.session?.accounts?.push(account);
+
+    await this.prisma.asUser.$transaction(async (prisma) => {
+      const { policyStates } = await prisma.account.create({
+        data: {
+          id: account,
+          deploySalt,
+          impl,
+          name,
+          policies: {
+            create: policies.map(
+              (policy): Prisma.PolicyCreateWithoutAccountInput => ({
+                key: policy.key,
+                name: policy.name,
+                states: {
+                  create: {
+                    ...policyAsCreateState(policy),
+                  },
+                },
+              }),
+            ),
+          },
+        },
+        select: {
+          policyStates: {
+            select: {
+              id: true,
+              policyKey: true,
+            },
+          },
+        },
+      });
+
+      // Set policy rules as draft of policies - this can't be done in the same create as the account )':
+      await Promise.all(
+        policyStates.map(({ id, policyKey }) =>
+          prisma.policy.update({
+            where: { accountId_key: { accountId: account, key: policyKey } },
+            data: { draftId: id },
+            select: null,
+          }),
+        ),
+      );
+    });
+
+    const r = await this.activateAccount(account, res);
+
+    this.publishAccount({ account: { id: account }, event: AccountEvent.create });
+    await this.contracts.addAccountAsVerified(account);
+    this.faucet.requestTokens(account);
+
+    return r;
+  }
+
+  async updateAccount<R extends Prisma.AccountArgs>(
+    { id, name }: UpdateAccountInput,
+    res?: Prisma.SelectSubset<R, Prisma.AccountArgs>,
+  ) {
+    const r = await this.prisma.asUser.account.update({
+      where: { id },
+      data: { name },
+      ...res,
+    });
+
+    this.publishAccount({ account: { id }, event: AccountEvent.update });
+
+    return r;
+  }
+
+  private async activateAccount<R extends Prisma.AccountArgs>(
     accountAddr: Address,
-    updateArgs?: T,
+    res?: Prisma.SelectSubset<R, Prisma.AccountArgs>,
   ) {
     const {
       impl,
@@ -74,11 +188,11 @@ export class AccountsService {
           })),
         },
       },
-      ...updateArgs,
-    }) as Prisma.Prisma__AccountClient<Prisma.AccountGetPayload<T>>;
+      ...res,
+    }); // as Prisma.Prisma__AccountClient<Prisma.AccountGetPayload<R>>;
   }
 
-  async publishAccount({ event, account: { id } }: AccountSubscriptionPayload) {
+  private async publishAccount({ event, account: { id } }: AccountSubscriptionPayload) {
     const payload: AccountSubscriptionPayload = { event, account: { id } }; // Reconstruct to exclude other fields
     await this.pubsub.publish<AccountSubscriptionPayload>(`${ACCOUNT_SUBSCRIPTION}.${id}`, payload);
 
