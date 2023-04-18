3202fe20008cb345509847f787be73ae0c772564
Hayden Briese <haydenbriese@gmail.com>
2023-04-18 10:09:57 +1000

    Use skeleton for account value

--- app/src/components/skeleton/LineSkeleton.tsx
+++ app/src/components/skeleton/LineSkeleton.tsx
@@ -1,17 +1,15 @@
 import { useTheme } from '@theme/paper';
 import { Rect } from 'react-content-loader/native';
-import { Skeleton } from './Skeleton';
+import { Skeleton, SkeletonProps } from './Skeleton';
+import { O } from 'ts-toolbelt';
 
-export interface LineSkeletonProps {
-  width?: number;
-  height?: number;
-}
+export interface LineSkeletonProps extends O.Optional<SkeletonProps, 'width' | 'height'> {}
 
-export const LineSkeleton = ({ width = 100, height = 8 }: LineSkeletonProps) => {
+export const LineSkeleton = ({ width = 100, height = 8, ...skeletonProps }: LineSkeletonProps) => {
   const { roundness } = useTheme();
 
   return (
-    <Skeleton width={width} height={height}>
+    <Skeleton width={width} height={height} {...skeletonProps}>
       <Rect x="0" y="0" rx={roundness} ry={roundness} width={width} height={height} />
     </Skeleton>
   );
