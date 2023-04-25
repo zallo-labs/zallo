7eec189700f84bc7fd1bc7136abd8386a292a747
Hayden Briese <haydenbriese@gmail.com>
2023-04-25 12:28:08 +1000

    Emit an event when a proposal is executed

--- app/src/components/Subscriptions.tsx
+++ app/src/components/Subscriptions.tsx
@@ -1,12 +1,14 @@
 import { useAccountIds } from '@api/account';
 import { useAccountSubscription } from '@api/account/useAccountsSubscription';
-import { useProposalSubscription } from '@api/proposal';
+import { useEmitProposalExecutionEvents, useProposalSubscription } from '@api/proposal';
 
 export const Subscriptions = () => {
   const accounts = useAccountIds();
 
   useAccountSubscription(); // Subscribes to all user accounts
-  useProposalSubscription({ accounts });
+
+  useProposalSubscription({ variables: { accounts } });
+  useEmitProposalExecutionEvents({ variables: { accounts } });
 
   return null;
 };
