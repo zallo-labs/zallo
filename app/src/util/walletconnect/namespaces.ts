b9e0e4bf09f87e42ae1bc367e0ce4f860b155321
Hayden Briese <haydenbriese@gmail.com>
2023-04-25 12:30:09 +1000

    Refactor

--- app/src/util/walletconnect/namespaces.ts
+++ app/src/util/walletconnect/namespaces.ts
@@ -1,17 +1,26 @@
 import { AccountId } from '@api/account';
-import { CHAIN_ID } from '@network/provider';
+import { SUPPORTED_CHAINS } from '@network/provider';
 import { Arraylike, toSet } from 'lib';
 import { WC_METHODS } from './methods';
 
-export const WC_NAMESPACE = 'eip155';
+export const WC_NAMESPACE_KEY = 'eip155';
 
-// https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-10.md
-const toEip155Account = (account: AccountId) => `${WC_NAMESPACE}:${CHAIN_ID()}:${account}`;
+export const WC_SUPPORTED_CHAINS = Object.values(SUPPORTED_CHAINS).map(
+  (chain) => `${WC_NAMESPACE_KEY}:${chain.id}`,
+);
+
+const WC_NAMESPACE = {
+  chains: WC_SUPPORTED_CHAINS,
+  methods: [...WC_METHODS],
+  events: ['accountsChanged', 'chainChanged'],
+};
 
 export const toNamespaces = (accounts: Arraylike<AccountId>) => ({
-  [WC_NAMESPACE]: {
-    accounts: [...toSet(accounts)].map(toEip155Account),
-    methods: [...WC_METHODS],
-    events: ['accountsChanged', 'chainChanged'],
+  [WC_NAMESPACE_KEY]: {
+    ...WC_NAMESPACE,
+    // https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-10.md
+    accounts: [...toSet(accounts)].flatMap((account) =>
+      WC_SUPPORTED_CHAINS.map((chain) => `${chain}:${account}`),
+    ),
   },
 });
