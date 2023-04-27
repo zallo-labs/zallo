6820a32a239ceb33578ee8d10ed56a291d62d69a
Hayden Briese <haydenbriese@gmail.com>
2023-04-27 15:56:27 +1000

    Remove use of BigIntJSON
    It doesn't work because we patch BigInt.toString() with another serialization method which occurs prior to the replacer

--- app/src/util/BigIntJSON.ts
+++ app/src/util/BigIntJSON.ts
@@ -3,6 +3,11 @@ import { wrapJSON } from './wrapJson';
 const PATTERN = /^BigInt::([0-9]+)$/;
 const toString = (value: bigint) => `BigInt::${value}`;
 
+/* This DOES NOT WORK
+ * .toString() is called on the bigint value before it is passed to the replacer function
+ * and we patch BigInt.toString() so the value is stringified 3n -> '3' by the time it reaches tryStringify
+ */
+
 export default wrapJSON({
   tryStringify: (_key, value) => {
     if (typeof value === 'bigint') return toString(value);
