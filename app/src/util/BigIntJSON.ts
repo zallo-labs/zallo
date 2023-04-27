2be6ffbec2a89be09c853cdb79fb199a1706d0c2
Hayden Briese <haydenbriese@gmail.com>
2023-04-27 16:29:55 +1000

    Fix BigIntJSON serialization when BigInt.toString() has been defined with a different serialization function

--- app/src/util/BigIntJSON.ts
+++ app/src/util/BigIntJSON.ts
@@ -3,16 +3,15 @@ import { wrapJSON } from './wrapJson';
 const PATTERN = /^BigInt::([0-9]+)$/;
 const toString = (value: bigint) => `BigInt::${value}`;
 
-/* This DOES NOT WORK
- * .toString() is called on the bigint value before it is passed to the replacer function
- * and we patch BigInt.toString() so the value is stringified 3n -> '3' by the time it reaches tryStringify
- */
-
 export default wrapJSON({
-  tryStringify: (_key, value) => {
+  tryStringify(key) {
+    // JSON calls toString() on the value before passing it to the replacer function
+    // This results in 3n -> '3' by the time it reaches tryStringify due to our BigInt.toString() patch
+    // this[key] always returns the original value so it is used over the value parameter
+    const value = this[key];
     if (typeof value === 'bigint') return toString(value);
   },
-  tryParse: (_key, value) => {
+  tryParse(_key, value) {
     if (typeof value === 'string') {
       const matches = value.match(PATTERN);
       if (matches) {
