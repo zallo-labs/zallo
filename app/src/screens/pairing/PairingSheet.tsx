11b291f6a76186c6f0367d6b6d5e453445ca5f60
Hayden Briese <haydenbriese@gmail.com>
2023-04-29 12:58:35 +1000

    Close sheet and cause re-render when forgetting a session

--- app/src/screens/pairing/PairingSheet.tsx
+++ app/src/screens/pairing/PairingSheet.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import { ListItem } from '~/components/list/ListItem';
 import { Sheet } from '~/components/sheet/Sheet';
 import { StackNavigatorScreenProps } from '~/navigation/StackNavigator';
-import { useWalletConnect } from '~/util/walletconnect';
+import { useUpdateWalletConnect, useWalletConnect } from '~/util/walletconnect';
 import * as Linking from 'expo-linking';
 import { CloseIcon, ExternalLinkIcon } from '@theme/icons';
 
@@ -16,15 +16,27 @@ export type PairingSheetProps = StackNavigatorScreenProps<'PairingSheet'>;
 export const PairingSheet = ({ route, navigation: { goBack } }: PairingSheetProps) => {
   const { topic } = route.params;
   const client = useWalletConnect();
+  const update = useUpdateWalletConnect();
   const pairing = client.pairing.getAll({ topic })?.[0];
   const session = client.session.getAll({ pairingTopic: topic })?.[0];
+
+  // Close sheet if the pairing doesn't exist anymore
+  if (!pairing) {
+    goBack();
+    return null;
+  }
 
   const metadata = pairing.peerMetadata ?? session.peer.metadata;
 
   const open = metadata.url && (() => Linking.openURL(metadata.url));
 
   const forget =
-    pairing && (() => client.disconnect({ topic, reason: getSdkError('USER_DISCONNECTED') }));
+    pairing &&
+    (async () => {
+      goBack();
+      await client.disconnect({ topic, reason: getSdkError('USER_DISCONNECTED') });
+      update();
+    });
 
   return (
     <Sheet onClose={goBack} contentContainerStyle={styles.container}>
