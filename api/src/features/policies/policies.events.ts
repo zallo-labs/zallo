9a0e77c85c02a3d858ada80bc3a1485ac31e6b85
Hayden Briese <haydenbriese@gmail.com>
2023-05-02 11:16:46 +1000

    Add policy event processors

---
+++ api/src/features/policies/policies.events.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../util/prisma/prisma.service';
+import { EventsProcessor, EventData } from '../events/events.processor';
+import { ACCOUNT_INTERFACE, Address, Hex, PolicyKey, asAddress, asHex, asPolicyKey } from 'lib';
+
+@Injectable()
+export class PoliciesEventsProcessor {
+  constructor(private events: EventsProcessor, private prisma: PrismaService) {
+    this.events.on(
+      ACCOUNT_INTERFACE.getEventTopic(ACCOUNT_INTERFACE.events['PolicyAdded(uint32,bytes32)']),
+      this.policyAdded,
+    );
+    this.events.on(
+      ACCOUNT_INTERFACE.getEventTopic(ACCOUNT_INTERFACE.events['PolicyRemoved(uint32)']),
+      this.policyRemoved,
+    );
+  }
+
+  private async policyAdded({ log }: EventData) {
+    const r = ACCOUNT_INTERFACE.decodeEventLog(
+      ACCOUNT_INTERFACE.events['PolicyAdded(uint32,bytes32)'],
+      log.data,
+      log.topics,
+    );
+
+    await this.markStateAsActive(
+      asAddress(log.address),
+      asPolicyKey(r[0]),
+      asHex(log.transactionHash),
+    );
+  }
+
+  private async policyRemoved({ log }: EventData) {
+    const r = ACCOUNT_INTERFACE.decodeEventLog(
+      ACCOUNT_INTERFACE.events['PolicyRemoved(uint32)'],
+      log.data,
+      log.topics,
+    );
+
+    await this.markStateAsActive(
+      asAddress(log.address),
+      asPolicyKey(r[0]),
+      asHex(log.transactionHash),
+    );
+  }
+
+  private async markStateAsActive(accountId: Address, key: PolicyKey, transactionHash: Hex) {
+    const proposalId = (
+      await this.prisma.asSystem.transaction.findUniqueOrThrow({
+        where: { hash: transactionHash },
+        select: { proposalId: true },
+      })
+    ).proposalId;
+
+    const state = await this.prisma.asSystem.policyState.findUniqueOrThrow({
+      where: {
+        accountId_policyKey_proposalId: { accountId, policyKey: key, proposalId },
+      },
+      select: {
+        id: true,
+        policy: { select: { draftId: true } },
+      },
+    });
+
+    await this.prisma.asSystem.policy.update({
+      where: { accountId_key: { accountId, key } },
+      data: {
+        activeId: state.id,
+        ...(state.policy.draftId === state.id && { draftId: null }),
+      },
+    });
+  }
+}
