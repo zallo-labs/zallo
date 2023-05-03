2135db2941eaa76654c9e8aa41ab304a946f0d0c
Hayden Briese <haydenbriese@gmail.com>
2023-05-03 21:42:05 +1000

    Add ComputedField decorator
    This allows specifying the necessary dependencies of ResolveFields
    Replacing the global select

---
+++ api/src/decorators/computed.decorator.ts
@@ -0,0 +1,14 @@
+import { Extensions, ResolveField, ResolveFieldOptions, ReturnTypeFunc } from '@nestjs/graphql';
+import { ObjectTypeSet } from '@db/edgeql-js/reflection';
+import { Shape } from '~/features/db/db.select';
+
+export const ComputedField =
+  <T extends ObjectTypeSet>(
+    typeFunc: ReturnTypeFunc,
+    select: Shape<T>,
+    options?: ResolveFieldOptions,
+  ): MethodDecorator =>
+  (target, propertyKey, descriptor) => {
+    ResolveField(typeFunc, options)(target, propertyKey, descriptor);
+    Extensions({ select })(target, propertyKey, descriptor);
+  };
