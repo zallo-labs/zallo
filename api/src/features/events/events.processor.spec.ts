d81e574ccc5d2451b3e3189d8d131b2a947f9fcd
Hayden Briese <haydenbriese@gmail.com>
2023-05-03 22:57:55 +1000

    Update and improve event queueing tests

--- api/src/features/events/events.processor.spec.ts
+++ api/src/features/events/events.processor.spec.ts
@@ -10,6 +10,7 @@ describe(EventsProcessor.name, () => {
   let processor: EventsProcessor;
   let queue: DeepMocked<Queue<EventData>>;
   let provider: DeepMocked<ProviderService>;
+  let attemptsMade = 0;
 
   let topic1Listener: jest.Mock;
   const logs = [
@@ -37,29 +38,23 @@ describe(EventsProcessor.name, () => {
     provider.getBlockNumber.mockReturnValue((async () => 1)());
 
     queue = module.get(getQueueToken(EVENTS_QUEUE.name));
+
+    attemptsMade = 0;
   });
 
+  const process = (data: EventData) =>
+    processor.process({ data, attemptsMade: attemptsMade++ } as Job<EventData>);
+
   it('send relevant event to listeners', async () => {
-    await processor.process({ data: { from: 1 } } as Job<EventData>);
+    await process({ from: 1 });
 
     expect(topic1Listener).toHaveBeenCalledTimes(2);
     expect(topic1Listener).toHaveBeenCalledWith({ log: logs[0] });
     expect(topic1Listener).toHaveBeenCalledWith({ log: logs[1] });
   });
 
-  it('queue next after processing events', async () => {
-    await processor.process({ data: { from: 1 } } as Job<EventData>);
-
-    expect(topic1Listener).toHaveBeenCalled();
-    expect(queue.add).toHaveBeenCalledTimes(1);
-    expect(queue.add.mock.invocationCallOrder[0]).toBeGreaterThan(
-      topic1Listener.mock.invocationCallOrder[0],
-    );
-  });
-
-  it('queue next immediately when behind', async () => {
-    provider.getBlockNumber.mockReturnValue((async () => 100000)());
-    await processor.process({ data: { from: 1 } } as Job<EventData>);
+  it('queue before processing', async () => {
+    await process({ from: 1 });
 
     expect(topic1Listener).toHaveBeenCalled();
     expect(queue.add).toHaveBeenCalledTimes(1);
@@ -68,16 +63,36 @@ describe(EventsProcessor.name, () => {
     );
   });
 
+  it('queue on the first attempt', async () => {
+    await process({ from: 1 });
+    expect(queue.add).toHaveBeenCalledTimes(1);
+  });
+
+  it('not queue on subsequent attempts', async () => {
+    await process({ from: 1 });
+    await process({ from: 1 });
+    expect(queue.add).toHaveBeenCalledTimes(1);
+  });
+
   it('split into 2 jobs if too many results', async () => {
+    provider.getBlockNumber.mockReturnValue((async () => 10)());
     provider.getLogs.mockImplementation(async () => {
       throw new Error(
-        'Query returned more than 10000 results. Try with this block range [0x00, 0x02]',
+        'Query returned more than 10000 results. Try with this block range [0x01, 0x03]',
       );
     });
 
-    await processor.process({ data: { from: 1 } } as Job<EventData>);
+    queue.addBulk.mockImplementation(async (jobs) => {
+      expect(jobs).toHaveLength(2);
+      expect(jobs[0].data.from).toEqual(1);
+      expect(jobs[0].data.to).toEqual(3);
+      expect(jobs[1].data.from).toEqual(4);
+      expect(jobs[1].data.to).toEqual(10);
+      return [];
+    });
 
-    expect(queue.add).not.toHaveBeenCalled();
+    await process({ from: 1, to: 10 });
+
     expect(queue.addBulk).toHaveBeenCalledTimes(1);
   });
 });
