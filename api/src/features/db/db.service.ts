0f7b580770a4bdf6fe64642ccf31a4b067a17937
Hayden Briese <haydenbriese@gmail.com>
2023-05-05 18:30:35 +1000

    Configure edge query based on user request context

--- api/src/features/db/db.service.ts
+++ api/src/features/db/db.service.ts
@@ -2,20 +2,35 @@ import { $infer } from '@db/edgeql-js';
 import { Injectable } from '@nestjs/common';
 import { Client, createClient } from 'edgedb';
 import { Expression } from '@db/edgeql-js/typesystem';
+import { getRequestContext } from '~/request/ctx';
 
 @Injectable()
 export class DbService {
-  private client: Client;
+  private __client: Client;
 
   constructor() {
-    this.client = createClient();
+    this.__client = createClient();
   }
 
   async onModuleInit() {
-    await this.client.ensureConnected();
+    await this.__client.ensureConnected();
   }
 
   async query<Expr extends Expression>(expression: Expr): Promise<$infer<Expr>> {
     return await expression.run(this.client);
+  }
+
+  get client(): Client {
+    const user = getRequestContext()?.user;
+
+    return user
+      ? this.__client.withGlobals({
+          // current_user_id: user.address,
+          current_user_address: user.address,
+          current_user_accounts_array: [...user.accounts],
+        })
+      : this.__client.withConfig({
+          apply_access_policies: false,
+        });
   }
 }
