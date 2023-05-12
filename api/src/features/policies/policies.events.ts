198907585098b97aeccb4aa98ba77211a203d3ed
Hayden Briese <haydenbriese@gmail.com>
2023-05-12 17:44:29 +1000

    Migrate policy events processor to edgedb

--- api/src/features/policies/policies.events.ts
+++ api/src/features/policies/policies.events.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../util/prisma/prisma.service';
 import { EventsProcessor, ListenerData } from '../events/events.processor';
-import { ACCOUNT_INTERFACE, Address, Hex, PolicyKey, asAddress, asHex, asPolicyKey } from 'lib';
+import { ACCOUNT_INTERFACE, PolicyKey, asAddress, asHex, asPolicyKey } from 'lib';
+import { DatabaseService } from '../database/database.service';
+import e from '~/edgeql-js';
+import { and } from '../database/database.util';
+import { selectPolicy } from './policies.service';
+import { Log } from '@ethersproject/abstract-provider';
 
 @Injectable()
 export class PoliciesEventsProcessor {
-  constructor(private events: EventsProcessor, private prisma: PrismaService) {
+  constructor(private db: DatabaseService, private events: EventsProcessor) {
     this.events.on(
       ACCOUNT_INTERFACE.getEventTopic(ACCOUNT_INTERFACE.events['PolicyAdded(uint32,bytes32)']),
       this.policyAdded,
@@ -23,11 +27,7 @@ export class PoliciesEventsProcessor {
       log.topics,
     );
 
-    await this.markStateAsActive(
-      asAddress(log.address),
-      asPolicyKey(r[0]),
-      asHex(log.transactionHash),
-    );
+    await this.markStateAsActive(log, asPolicyKey(r[0]));
   }
 
   private async policyRemoved({ log }: ListenerData) {
@@ -37,37 +37,33 @@ export class PoliciesEventsProcessor {
       log.topics,
     );
 
-    await this.markStateAsActive(
-      asAddress(log.address),
-      asPolicyKey(r[0]),
-      asHex(log.transactionHash),
-    );
+    await this.markStateAsActive(log, asPolicyKey(r[0]));
   }
 
-  private async markStateAsActive(accountId: Address, key: PolicyKey, transactionHash: Hex) {
-    const proposalId = (
-      await this.prisma.asSystem.transaction.findUniqueOrThrow({
-        where: { hash: transactionHash },
-        select: { proposalId: true },
-      })
-    ).proposalId;
+  private async markStateAsActive(log: Log, key: PolicyKey) {
+    // It's possible that two policies are activated in the same proposal; it's not prohibited by a constraint.
+    const policyState = e.assert_single(
+      e.select(e.PolicyState, (ps) => ({
+        filter: and(
+          e.op(ps.policy, '=', selectPolicy({ account: asAddress(log.address), key })),
+          e.op(
+            ps.proposal,
+            '=',
+            e.select(e.Transaction, () => ({
+              filter_single: { hash: asHex(log.transactionHash) },
+              proposal: { id: true },
+            })).proposal,
+          ),
+        ),
+      })),
+    );
 
-    const state = await this.prisma.asSystem.policyState.findUniqueOrThrow({
-      where: {
-        accountId_policyKey_proposalId: { accountId, policyKey: key, proposalId },
-      },
-      select: {
-        id: true,
-        policy: { select: { draftId: true } },
-      },
-    });
-
-    await this.prisma.asSystem.policy.update({
-      where: { accountId_key: { accountId, key } },
-      data: {
-        activeId: state.id,
-        ...(state.policy.draftId === state.id && { draftId: null }),
-      },
-    });
+    await e
+      .update(policyState, () => ({
+        set: {
+          activationBlock: BigInt(log.blockNumber),
+        },
+      }))
+      .run(this.db.client);
   }
 }
