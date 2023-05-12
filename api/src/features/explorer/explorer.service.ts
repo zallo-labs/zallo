68c521ad0e413071b74229c94fd06fe489196925
Hayden Briese <haydenbriese@gmail.com>
2023-05-12 17:37:52 +1000

    Remove transfers functionality from explorer serivce

--- api/src/features/explorer/explorer.service.ts
+++ api/src/features/explorer/explorer.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { BigNumber, BigNumberish, Contract } from 'ethers';
+import { BigNumberish, Contract } from 'ethers';
 import { Address, Addresslike, ChainName, Hex, asAddress } from 'lib';
 import { fetchJsonWithRetry } from '~/util/fetch';
-import { ExplorerTransfer } from './explorer.model';
 import { JsonFragment } from '@ethersproject/abi';
 import { AbiSource } from '../contract-functions/contract-functions.model';
 
@@ -11,32 +10,6 @@ export class ExplorerService {
   async transaction(transactionHash: Hex): Promise<TransactionData | undefined> {
     // https://zksync2-testnet-explorer.zksync.dev/transaction/0x71c873a22ca9a1d05f06cb1dbb6bb73d0ff5be0cce67ed04a5ae8f0fa87e787f
     return this.query<TransactionData>(`/transaction/${transactionHash}`);
-  }
-
-  async accountTransfers({
-    account,
-    limit,
-    direction = 'newer',
-  }: AccountTransfersArgs): Promise<ExplorerTransfer[]> {
-    if (limit < 0 || limit > 100) throw new Error('limit must be [0, 100]');
-
-    // https://zksync2-testnet-explorer.zksync.dev/transactions?limit=10&direction=older&accountAddress=0x6BfA67e65e4735DCc9Ff6036D09F680f35740A83
-    const txs = await this.query<TransactionsData>(
-      `/transactions?accountAddress=${account}&limit=${limit}&direction=${direction}`,
-    );
-    if (!txs) return [];
-
-    return txs.list.flatMap((tx) =>
-      tx.erc20Transfers.map((t, i) => ({
-        id: `${tx.transactionHash}-${i}`,
-        transferNumber: i,
-        token: asAddress(t.tokenInfo.l2Address),
-        from: asAddress(t.from),
-        to: asAddress(t.to),
-        amount: BigNumber.from(t.amount).toBigInt(),
-        timestamp: new Date(tx.receivedAt),
-      })),
-    );
   }
 
   async tokenInfo(token: Address) {
@@ -86,17 +59,6 @@ interface TransactionData {
     to: Address;
     amount: BigNumberish;
   }[];
-}
-
-interface TransactionsData {
-  list: TransactionData[];
-  total: number;
-}
-
-interface AccountTransfersArgs {
-  account: Address;
-  limit: number;
-  direction?: 'older' | 'newer';
 }
 
 interface TokenInfo {
