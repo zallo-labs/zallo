1585399a09bb2ec052f92d14c65bebef4bcdf7ca
Hayden Briese <haydenbriese@gmail.com>
2023-05-16 11:36:46 +1000

    Custom persistedAtom implementation that persists default value

--- app/src/util/persistedAtom.ts
+++ app/src/util/persistedAtom.ts
@@ -1,53 +1,84 @@
-import { WritableAtom } from 'jotai';
-import { RESET, atomWithStorage, createJSONStorage, unstable_NO_STORAGE_VALUE } from 'jotai/utils';
-import type { AsyncStorage as AsyncStorageType } from 'jotai/vanilla/utils/atomWithStorage';
+import { WritableAtom, atom } from 'jotai';
+import { RESET, createJSONStorage, unstable_NO_STORAGE_VALUE } from 'jotai/utils';
+import type { AsyncStorage as TAsyncStorage } from 'jotai/vanilla/utils/atomWithStorage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as SecureStore from 'expo-secure-store';
 
+interface StorageOptions<V, U extends AnyJson> {
+  secure?: SecureStore.SecureStoreOptions;
+  stringifiy?: (value: V) => U;
+  parse?: (value: U) => V;
+}
+
+// Based off https://github.com/pmndrs/jotai/blob/main/src/vanilla/utils/atomWithStorage.ts
+// Primary difference is that this persists the initialValue
 export const persistedAtom = <V, U extends AnyJson = AnyJson>(
   key: string,
   initialValue: V,
   options?: StorageOptions<V, U>,
-): WritableAtom<V, [SetStateActionWithReset<V>], void> => {
-  const storage = create(options);
+): WritableAtom<V, [SetStateActionWithReset<V>], V> => {
+  const storage = getStorage(options);
+  const baseAtom = atom(initialValue);
+  baseAtom.debugLabel = `${key}::base`;
 
-  const atom = atomWithStorage(key, initialValue, storage);
-  atom.debugLabel = key;
+  baseAtom.onMount = (setAtom) => {
+    storage.getItem(key).then((v) => {
+      if (v === unstable_NO_STORAGE_VALUE) {
+        storage.setItem(key, initialValue);
+      } else if (v !== initialValue) {
+        setAtom(v);
+      }
+    });
 
-  return atom;
+    if (storage.subscribe) return storage.subscribe(key, setAtom);
+  };
+
+  const pAtom = atom(
+    (get) => get(baseAtom),
+    (get, set, update: SetStateActionWithReset<V>) => {
+      const nextValue =
+        typeof update === 'function'
+          ? (update as (prev: V) => V | typeof RESET)(get(baseAtom))
+          : update;
+
+      const newValue = nextValue === RESET ? initialValue : nextValue;
+
+      set(baseAtom, newValue);
+      storage.setItem(key, newValue);
+
+      return newValue;
+    },
+  );
+  pAtom.debugLabel = key;
+
+  return pAtom;
 };
 
-type StorageOptions<V, U extends AnyJson> = {
-  secure?: SecureStore.SecureStoreOptions;
-  stringifiy?: (value: V) => U;
-  parse?: (value: U) => V;
-};
+const ASYNC_STORAGE = createJSONStorage(() => AsyncStorage);
 
-const create = <V, U extends AnyJson>({
-  secure: secure,
+const getStorage = <V, U extends AnyJson>({
+  secure,
   parse,
   stringifiy,
-}: StorageOptions<V, U> = {}): AsyncStorageType<V> => {
-  const store = createJSONStorage<U>(() =>
-    secure
-      ? {
-          getItem: (key) => SecureStore.getItemAsync(key, secure),
-          setItem: (key, newValue) => SecureStore.setItemAsync(key, newValue, secure),
-          removeItem: (key) => SecureStore.deleteItemAsync(key, secure),
-        }
-      : AsyncStorage,
-  );
+}: StorageOptions<V, U> = {}): TAsyncStorage<V> => {
+  const store = secure
+    ? createJSONStorage<U>(() => ({
+        getItem: (key) => SecureStore.getItemAsync(key, secure),
+        setItem: (key, newValue) => SecureStore.setItemAsync(key, newValue, secure),
+        removeItem: (key) => SecureStore.deleteItemAsync(key, secure),
+      }))
+    : (ASYNC_STORAGE as TAsyncStorage<U>);
 
-  const getItem: AsyncStorageType<V>['getItem'] = parse
+  const getItem: TAsyncStorage<V>['getItem'] = parse
     ? async (key) => {
         const r = await store.getItem(key);
         return r === unstable_NO_STORAGE_VALUE ? unstable_NO_STORAGE_VALUE : parse(r);
       }
-    : (store.getItem as AsyncStorageType<V>['getItem']);
+    : (store.getItem as TAsyncStorage<V>['getItem']);
 
-  const setItem: AsyncStorageType<V>['setItem'] = stringifiy
+  const setItem: TAsyncStorage<V>['setItem'] = stringifiy
     ? async (key, newValue) => store.setItem(key, stringifiy(newValue))
-    : (store.setItem as unknown as AsyncStorageType<V>['setItem']);
+    : (store.setItem as unknown as TAsyncStorage<V>['setItem']);
 
   return {
     getItem,
