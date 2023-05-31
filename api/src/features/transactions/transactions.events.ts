4f602b02e09f0b23e537163e16af57bf76d7513c
Hayden Briese <haydenbriese@gmail.com>
2023-05-31 18:22:27 +1000

    Fix failed proposal publishing account address

--- api/src/features/transactions/transactions.events.ts
+++ api/src/features/transactions/transactions.events.ts
@@ -95,7 +95,7 @@ export class TransactionsEvents implements OnModuleInit {
       throw new Error(`Proposal not found for reverted transaction: ${receipt.transactionHash}`);
 
     await this.proposals.publishProposal(
-      { account: asAddress(receipt.contractAddress), hash: proposalHash as Hex },
+      { account: asAddress(receipt.from), hash: proposalHash as Hex },
       ProposalEvent.executed,
     );
   }
