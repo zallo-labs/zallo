3fcc2588c0f21e72966af7c0a561860d66d8e1ce
Hayden Briese <haydenbriese@gmail.com>
2023-06-08 16:20:51 +1000

    Don't log unauthorized exceptions to sentry

--- api/src/features/util/sentry/sentry.interceptor.ts
+++ api/src/features/util/sentry/sentry.interceptor.ts
@@ -1,4 +1,10 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { HttpArgumentsHost, RpcArgumentsHost, WsArgumentsHost } from '@nestjs/common/interfaces';
 import type { GqlContextType, GraphQLArgumentsHost } from '@nestjs/graphql';
 import { GqlArgumentsHost } from '@nestjs/graphql';
@@ -7,27 +13,35 @@ import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+type Filter<E = any> = [type: new (...args: any[]) => E, shouldReport: (exception: E) => boolean];
+
 // Based off https://github.com/mentos1386/nest-raven
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
+  private readonly filters: Filter[] = [
+    [UnauthorizedException, () => false] satisfies Filter<UnauthorizedException>,
+  ];
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // first param would be for events, second is for errors
     return next.handle().pipe(
       tap({
         error: (exception) => {
-          withScope((scope) => {
-            switch (context.getType<GqlContextType>()) {
-              case 'graphql':
-                this.addGraphQLExceptionMetadatas(scope, GqlArgumentsHost.create(context));
-              case 'http':
-                this.addHttpExceptionMetadatas(scope, context.switchToHttp());
-              case 'ws':
-                this.addWsExceptionMetadatas(scope, context.switchToWs());
-              case 'rpc':
-                this.addRpcExceptionMetadatas(scope, context.switchToRpc());
-            }
-            return captureException(exception);
-          });
+          if (this.shouldReport(exception)) {
+            withScope((scope) => {
+              switch (context.getType<GqlContextType>()) {
+                case 'graphql':
+                  this.addGraphQLExceptionMetadatas(scope, GqlArgumentsHost.create(context));
+                case 'http':
+                  this.addHttpExceptionMetadatas(scope, context.switchToHttp());
+                case 'ws':
+                  this.addWsExceptionMetadatas(scope, context.switchToWs());
+                case 'rpc':
+                  this.addRpcExceptionMetadatas(scope, context.switchToRpc());
+              }
+              return captureException(exception);
+            });
+          }
         },
       }),
     );
@@ -61,5 +75,11 @@ export class SentryInterceptor implements NestInterceptor {
   private addWsExceptionMetadatas(scope: Scope, ws: WsArgumentsHost): void {
     scope.setExtra('ws_client', ws.getClient());
     scope.setExtra('ws_data', ws.getData());
+  }
+
+  private shouldReport(exception: unknown): boolean {
+    return this.filters.every(
+      ([type, filter]) => !(exception instanceof type && (!filter || filter(exception))),
+    );
   }
 }
