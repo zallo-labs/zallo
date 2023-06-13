4ee090f37a6ddcfbd0b486ab186726eeeca2acd2
Hayden Briese <haydenbriese@gmail.com>
2023-06-13 15:17:52 +1000

    Improve swap transfer ratio
    Supports more extreme transfers, and always shows a value - whether the user has input an amount or not

--- app/src/screens/swap/ToTokenItem.tsx
+++ app/src/screens/swap/ToTokenItem.tsx
@@ -1,13 +1,14 @@
+import { convertDecimals } from '@token/token';
 import { useToken } from '@token/useToken';
 import { Address } from 'lib';
-import { formatUnits } from 'viem';
+import { useFormattedNumber } from '~/components/format/FormattedNumber';
 import { ListItem, ListItemProps } from '~/components/list/ListItem';
 import { ListItemSkeleton } from '~/components/list/ListItemSkeleton';
 import { withSuspense } from '~/components/skeleton/withSuspense';
 import { useFormattedTokenAmount } from '~/components/token/TokenAmount';
 import { Pool, TokenAmount, useEstimatedSwap } from '~/util/swap';
 
-const RATIO_DECIMALS = 4;
+const RATIO_DECIMALS = 18;
 const RATIO_FACTOR = 10n ** BigInt(RATIO_DECIMALS);
 
 export interface ToTokenItemProps extends Partial<ListItemProps> {
@@ -17,21 +18,34 @@ export interface ToTokenItemProps extends Partial<ListItemProps> {
 }
 
 function ToTokenItem({ account, from, pool, ...itemProps }: ToTokenItemProps) {
-  const to = useEstimatedSwap({ account, from, pool });
+  const to = useEstimatedSwap({ account, pool, from });
 
   const fromToken = useToken(from.token);
   const toToken = useToken(to.token);
 
-  const ratio = from.amount !== 0n ? (to.amount * RATIO_FACTOR) / from.amount : 0n;
+  const ratioFromAmount = from.amount || 10n ** BigInt(fromToken.decimals);
+  const { amount: ratioToAmount } = useEstimatedSwap({
+    account,
+    pool,
+    from: { ...from, amount: ratioFromAmount },
+  });
+
+  const ratio =
+    (ratioToAmount * RATIO_FACTOR) /
+    convertDecimals(ratioFromAmount, fromToken.decimals, toToken.decimals);
 
   return (
     <ListItem
       leading={to.token}
       overline="To (estimated)"
       headline={useFormattedTokenAmount(to)}
-      trailing={`${formatUnits(ratio, toToken.decimals - fromToken.decimals + RATIO_DECIMALS)}/${
-        fromToken.symbol
-      }`}
+      trailing={useFormattedNumber({
+        value: ratio,
+        decimals: RATIO_DECIMALS,
+        maximumFractionDigits: 3,
+        minimumNumberFractionDigits: 4,
+        postFormat: (v) => `${v}/${fromToken.symbol}`,
+      })}
       {...itemProps}
     />
   );
