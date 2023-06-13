b440463cc57d0cc4b6c9460a7a29fe6ead46318f
Hayden Briese <haydenbriese@gmail.com>
2023-06-13 14:30:45 +1000

    Add viem compatible abis to lib

---
+++ lib/src/abi/account.ts
@@ -0,0 +1,4 @@
+import { AccountProxy__factory, Account__factory } from '../contracts';
+
+export const ACCOUNT_ABI = Account__factory.abi;
+export const ACCOUNT_PROXY_ABI = AccountProxy__factory.abi;
