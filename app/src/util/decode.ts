dac3c975642681df5c248abcd911ce44987d94e8
Hayden Briese <haydenbriese@gmail.com>
2023-06-13 15:15:10 +1000

    Use viem for decoding erc20 transfers

--- app/src/util/decode.ts
+++ app/src/util/decode.ts
@@ -1,15 +1,13 @@
-import { ERC20_INTERFACE } from '@token/token';
-import { BigNumber, BigNumberish } from 'ethers';
-import { Addresslike, Hex, asAddress, tryOrIgnore } from 'lib';
+import { ERC20_ABI, Hex, asAddress, tryOrIgnore } from 'lib';
+import { decodeFunctionData } from 'viem';
 
 export const tryDecodeTransfer = (data: Hex | undefined) =>
   tryOrIgnore(() => {
     if (!data) return undefined;
 
-    const [to, amount] = ERC20_INTERFACE.decodeFunctionData(
-      ERC20_INTERFACE.functions['transfer(address,uint256)'],
-      data,
-    ) as [Addresslike, BigNumberish];
+    const r = decodeFunctionData({ abi: ERC20_ABI, data });
 
-    return { to: asAddress(to), amount: BigNumber.from(amount).toBigInt() };
+    return r?.functionName === 'transfer'
+      ? { to: asAddress(r.args[0]), amount: r.args[1] }
+      : undefined;
   });
