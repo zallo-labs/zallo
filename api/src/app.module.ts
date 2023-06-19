0aff90fb5736d460da47ce828b635dfbf18480b4
Hayden Briese <haydenbriese@gmail.com>
2023-06-19 17:57:36 +1000

    Have Bull re-use redis connections where possible

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -15,20 +15,19 @@ import { TransactionsModule } from './features/transactions/transactions.module'
 import { FaucetModule } from './features/faucet/faucet.module';
 import { ExpoModule } from './features/util/expo/expo.module';
 import { PubsubModule } from './features/util/pubsub/pubsub.module';
-import { CONFIG } from './config';
-import { BullModule } from '@nestjs/bull';
 import { PoliciesModule } from './features/policies/policies.module';
 import { ContractsModule } from './features/contracts/contracts.module';
 import { ExplorerModule } from './features/explorer/explorer.module';
 import { TransfersModule } from './features/transfers/transfers.module';
 import { EventsModule } from './features/events/events.module';
-import { REDIS_OPTIONS, RedisModule } from './features/util/redis/redis.module';
+import { RedisModule } from './features/util/redis/redis.module';
 import { ReceiptsModule } from './features/receipts/receipts.module';
 import { PaymasterModule } from './features/paymaster/paymaster.module';
 import { SentryModule } from './features/util/sentry/sentry.module';
 import { SentryInterceptor } from './features/util/sentry/sentry.interceptor';
 import { OperationsModule } from './features/operations/operations.module';
 import { SimulationModule } from './features/simulation/simulation.module';
+import { BullModule } from './features/util/bull/bull.module';
 
 @Module({
   imports: [
@@ -36,17 +35,7 @@ import { SimulationModule } from './features/simulation/simulation.module';
     SentryModule.forRoot(),
     DatabaseModule,
     RedisModule,
-    BullModule.forRoot({
-      // Requires separate redis client - https://github.com/OptimalBits/bull/issues/1873
-      url: CONFIG.redisUrl,
-      redis: REDIS_OPTIONS,
-      defaultJobOptions: {
-        removeOnComplete: true,
-        removeOnFail: false,
-        attempts: 18, // 2^18 * 200ms = ~14.5h
-        backoff: { type: 'exponential', delay: 200 },
-      },
-    }),
+    BullModule,
     ApolloModule,
     PubsubModule,
     ProviderModule,
