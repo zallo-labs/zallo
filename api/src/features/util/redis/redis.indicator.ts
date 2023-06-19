151b62b5892377c3f18d14c90407ce461233c04f
Hayden Briese <haydenbriese@gmail.com>
2023-06-19 18:14:11 +1000

    Refactor redis health indicator

--- api/src/features/util/redis/redis.indicator.ts
+++ api/src/features/util/redis/redis.indicator.ts
@@ -1,21 +1,12 @@
-import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Injectable } from '@nestjs/common';
 import { HealthCheckError, HealthIndicator } from '@nestjs/terminus';
 import Redis from 'ioredis';
-import { InjectRedisSubscriber } from '~/decorators/redis.decorator';
 
 @Injectable()
 export class RedisHealthIndicator extends HealthIndicator {
-  constructor(
-    @InjectRedis() private readonly redis: Redis,
-    @InjectRedisSubscriber() private readonly redisSub: Redis,
-  ) {
-    super();
-  }
-
-  async check(key: string) {
+  async check(key: string, client: Redis) {
     try {
-      await Promise.all([this.redis.ping(), this.redisSub.ping()]);
+      await client.ping();
       return this.getStatus(key, true, { status: 'ok' });
     } catch (e) {
       throw new HealthCheckError('Redis check failed', e);
