237e859df5ca3b96b00ba0aace9e484e6a62f258
Hayden Briese <haydenbriese@gmail.com>
2023-06-27 13:14:19 +1000

    Workaround for FAB shadow clipping navbar

--- app/src/components/buttons/Fab.tsx
+++ app/src/components/buttons/Fab.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Keyboard } from 'react-native';
 import { FAB as PaperFAB } from 'react-native-paper';
 import { IconSource } from 'react-native-paper/lib/typescript/src/components/Icon';
@@ -13,29 +13,26 @@ export type FabProps = Omit<BaseProps, 'icon'> &
 
 export const Fab = ({ appbar, onPress, style, ...props }: FabProps) => {
   return (
-    <View style={[styles.container, style]}>
-      <PaperFAB
-        icon={undefined as unknown as IconSource} // https://github.com/callstack/react-native-paper/issues/3594
-        size={appbar ? 'small' : 'medium'}
-        mode={appbar ? 'flat' : 'elevated'}
-        {...props}
-        {...(onPress && {
-          onPress: (e) => {
-            Keyboard.dismiss();
-            onPress(e);
-          },
-        })}
-      />
-    </View>
+    <PaperFAB
+      icon={undefined as unknown as IconSource} // https://github.com/callstack/react-native-paper/issues/3594
+      size={appbar ? 'small' : 'medium'}
+      mode={appbar ? 'flat' : 'elevated'}
+      style={[styles.fab, style]}
+      {...props}
+      {...(onPress && {
+        onPress: (e) => {
+          Keyboard.dismiss();
+          onPress(e);
+        },
+      })}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  fab: {
     position: 'absolute',
-    bottom: 0,
-    right: 0,
-    alignItems: 'flex-end',
-    margin: 16,
+    bottom: 22, // Should be 16, but that currently causes the shadow to be clipped by the navbar - https://github.com/callstack/react-native-paper/issues/3957
+    right: 16,
   },
 });
