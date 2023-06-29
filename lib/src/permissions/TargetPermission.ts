085bda62ea25da0c08fe5f286c2d9e2fa7f270f4
Hayden Briese <haydenbriese@gmail.com>
2023-06-29 15:27:51 +1000

    Optimize targets config when converting to a struct

--- lib/src/permissions/TargetPermission.ts
+++ lib/src/permissions/TargetPermission.ts
@@ -9,6 +9,8 @@ import _ from 'lodash';
 import { Operation } from '../operation';
 import { SatisfiabilityResult } from '../satisfiability';
 import assert from 'assert';
+import { getFunctionSelector, getAbiItem } from 'viem';
+import { ERC20_ABI } from '../abi';
 
 export interface TargetsConfig {
   contracts: Record<Address, Target>;
@@ -29,31 +31,35 @@ const TARGET_ABI = '((bytes4 selector, bool allow)[] functions, bool defaultAllo
 
 export const TARGETS_CONFIG_ABI = newAbiType<TargetsConfig, AwaitedObj<TargetsConfigStruct>>(
   `((address addr, ${TARGET_ABI} target)[] contracts, ${TARGET_ABI} defaultTarget)`,
-  (c) => ({
-    contracts: Object.entries(c.contracts)
-      .map(([address, target]) => ({
-        addr: address,
-        target: {
-          functions: Object.entries(target.functions)
-            .map(([selector, allow]) => ({
-              selector,
-              allow,
-            }))
-            .sort((a, b) => compareBytes(a.selector, b.selector)),
-          defaultAllow: target.defaultAllow,
-        },
-      }))
-      .sort((a, b) => compareAddress(a.addr, b.addr)),
-    defaultTarget: {
-      functions: Object.entries(c.default.functions)
-        .map(([selector, allow]) => ({
-          selector,
-          allow,
+  (unoptimizedConfig) => {
+    const c = optimize(unoptimizedConfig);
+
+    return {
+      contracts: Object.entries(c.contracts)
+        .map(([address, target]) => ({
+          addr: address,
+          target: {
+            functions: Object.entries(target.functions)
+              .map(([selector, allow]) => ({
+                selector,
+                allow,
+              }))
+              .sort((a, b) => compareBytes(a.selector, b.selector)),
+            defaultAllow: target.defaultAllow,
+          },
         }))
-        .sort((a, b) => compareBytes(a.selector, b.selector)),
-      defaultAllow: c.default.defaultAllow,
-    },
-  }),
+        .sort((a, b) => compareAddress(a.addr, b.addr)),
+      defaultTarget: {
+        functions: Object.entries(c.default.functions)
+          .map(([selector, allow]) => ({
+            selector,
+            allow,
+          }))
+          .sort((a, b) => compareBytes(a.selector, b.selector)),
+        defaultAllow: c.default.defaultAllow,
+      },
+    };
+  },
   (s) =>
     s
       ? {
@@ -117,23 +123,38 @@ export const setTargetAllowed = (
   selector: Selector,
   allow: boolean,
 ) => {
-  // Naive
+  if (!targets.contracts[to])
+    targets.contracts[to] = { functions: {}, defaultAllow: targets.default.defaultAllow };
+
   targets.contracts[to].functions[selector] = allow;
 
-  // Optimization - remove selector if matches the target default
-  if (targets.contracts[to].defaultAllow === allow) {
-    delete targets.contracts[to].functions[selector];
-  }
-
-  // Optimization - remove target if it matches the the default
-  if (
-    Object.keys(targets.contracts[to].functions).length === 0 &&
-    targets.default.defaultAllow === allow &&
-    (targets.default.functions[selector] === undefined ||
-      targets.default.functions[selector] === allow)
-  ) {
-    delete targets.contracts[to];
-  }
+  targets = optimize(targets);
 
   assert(isTargetAllowed(targets, to, selector) == allow);
 };
+
+const optimize = (targets: TargetsConfig): TargetsConfig => {
+  // Remove selectors that match the contract default
+  targets.contracts = _.mapValues(targets.contracts, (target) => ({
+    ...target,
+    functions: _.pickBy(target.functions, (allow) => allow !== target.defaultAllow),
+  }));
+
+  // Remove contracts without selectors that match the default
+  targets.contracts = _.pickBy(
+    targets.contracts,
+    (target) =>
+      Object.keys(target.functions).length > 0 ||
+      target.defaultAllow !== targets.default.defaultAllow,
+  );
+
+  return targets;
+};
+
+export const SPENDING_TRANSFER_FUNCTIONS = new Set<Selector>([
+  getFunctionSelector(getAbiItem({ abi: ERC20_ABI, name: 'transfer' })) as Selector,
+  getFunctionSelector(getAbiItem({ abi: ERC20_ABI, name: 'approve' })) as Selector,
+  getFunctionSelector(getAbiItem({ abi: ERC20_ABI, name: 'increaseAllowance' })) as Selector,
+  // decreaseAllowance is not currently factored into spending limits by hook contract
+  getFunctionSelector(getAbiItem({ abi: ERC20_ABI, name: 'decreaseAllowance' })) as Selector,
+]);
