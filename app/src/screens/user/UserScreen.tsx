2baff46d9c1f7d0fee29c7f8b73d07ba0cdec839
Hayden Briese <haydenbriese@gmail.com>
2023-07-06 19:49:13 +1000

    Combine User screens

--- app/src/screens/user/UserScreen.tsx
+++ app/src/screens/user/UserScreen.tsx
@@ -1,29 +1,44 @@
-import { useUpdateUser, useUser } from '@api/user';
-import { ShareIcon } from '@theme/icons';
 import { useForm } from 'react-hook-form';
 import { StyleSheet } from 'react-native';
 import { Appbar } from '~/components/Appbar/Appbar';
-import { Button } from '~/components/Button';
-import { FormSubmitButton } from '~/components/fields/FormSubmitButton';
+import { FormSubmitButton, useFormSubmitDisabled } from '~/components/fields/FormSubmitButton';
 import { FormTextField } from '~/components/fields/FormTextField';
-import { RequireBiometricsItem } from '~/components/items/RequireBiometricsItem';
 import { Actions } from '~/components/layout/Actions';
 import { Screen } from '~/components/layout/Screen';
 import { StackNavigatorScreenProps } from '~/navigation/StackNavigator';
 import { FormResetIcon } from '~/components/fields/ResetFormIcon';
+import { gql, useSuspenseQuery } from '@apollo/client';
+import { UserDocument, UserQuery, UserQueryVariables, useUserUpdateMutation } from '@api/generated';
+
+gql`
+  query User {
+    user {
+      id
+      name
+    }
+  }
+
+  mutation UserUpdate($name: String!) {
+    updateUser(input: { name: $name }) {
+      id
+      name
+    }
+  }
+`;
 
 interface Inputs {
   name: string;
 }
 
-export type UserScreenProps = StackNavigatorScreenProps<'User'>;
+export type UserScreenProps = StackNavigatorScreenProps<'User' | 'CreateUser'>;
 
-export const UserScreen = ({ navigation: { navigate } }: UserScreenProps) => {
-  const user = useUser();
-  const updateUser = useUpdateUser();
+export const UserScreen = ({ route, navigation: { navigate } }: UserScreenProps) => {
+  const isOnboarding = route.name === 'CreateUser';
+  const [update] = useUserUpdateMutation();
+  const { user } = useSuspenseQuery<UserQuery, UserQueryVariables>(UserDocument).data;
 
   const { control, handleSubmit, reset } = useForm<Inputs>({
-    defaultValues: { name: user.name },
+    defaultValues: { name: user.name ?? '' },
   });
 
   return (
@@ -39,34 +54,30 @@ export const UserScreen = ({ navigation: { navigate } }: UserScreenProps) => {
         label="Name"
         supporting="Only visible by account members"
         name="name"
+        placeholder="Alisha"
+        autoFocus
         control={control}
         rules={{ required: true }}
         containerStyle={styles.nameContainer}
+        onEndEditing={handleSubmit(async ({ name }) => {
+          await update({ variables: { name } });
+        })}
       />
 
-      <RequireBiometricsItem />
-
       <Actions>
-        <Button
-          mode="contained-tonal"
-          icon={ShareIcon}
-          style={styles.actionButton}
-          onPress={() => navigate('QrModal', { address: user.address })}
-        >
-          Share
-        </Button>
-
-        <FormSubmitButton
-          mode="contained"
-          requireChanges
-          control={control}
-          style={styles.actionButton}
-          onPress={handleSubmit(async ({ name }) => {
-            await updateUser({ name });
-          })}
-        >
-          Update
-        </FormSubmitButton>
+        {isOnboarding && (
+          <FormSubmitButton
+            mode="contained"
+            requireChanges
+            control={control}
+            style={styles.actionButton}
+            onPress={() => {
+              navigate('Approver');
+            }}
+          >
+            Continue
+          </FormSubmitButton>
+        )}
       </Actions>
     </Screen>
   );
