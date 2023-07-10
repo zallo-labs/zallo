c248bb23c8d20bcb3f4c28cba8da10b6c0d2ccdd
Hayden Briese <haydenbriese@gmail.com>
2023-07-10 11:38:50 +1000

    Redesign settings screen

--- app/src/screens/settings/SettingsScreen.tsx
+++ app/src/screens/settings/SettingsScreen.tsx
@@ -2,6 +2,7 @@ import { useApproverAddress } from '@network/useApprover';
 import {
   ContactsIcon,
   GithubIcon,
+  NavigateNextIcon,
   NotificationsIcon,
   TwitterIcon,
   WalletConnectIcon,
@@ -9,9 +10,9 @@ import {
 import { makeStyles } from '@theme/makeStyles';
 import { ETH } from '@token/tokens';
 import { Image } from 'expo-image';
-import { ScrollView } from 'react-native';
-import { Divider } from 'react-native-paper';
-import { useAddressLabel } from '~/components/address/AddressLabel';
+import { Pressable, ScrollView, TouchableOpacity, View } from 'react-native';
+import { Divider, Text } from 'react-native-paper';
+import { AddressLabel, useAddressLabel } from '~/components/address/AddressLabel';
 import { Appbar } from '~/components/Appbar/Appbar';
 import { Screen } from '~/components/layout/Screen';
 import { ListItem } from '~/components/list/ListItem';
@@ -20,6 +21,21 @@ import * as Linking from 'expo-linking';
 import { CONFIG } from '~/util/config';
 import { Address } from 'lib';
 import { FingerprintIcon } from '../biometrics/BiometricsScreen';
+import { AddressIcon } from '~/components/Identicon/AddressIcon';
+import { gql } from '@api/gen';
+import { useSuspenseQuery } from '@apollo/client';
+import { SettingsDocument, SettingsQuery, SettingsQueryVariables } from '@api/generated';
+import { Chevron } from '~/components/Chevron';
+import { ICON_SIZE } from '@theme/paper';
+
+gql(/* GraphQL */ `
+  query Settings {
+    user {
+      id
+      name
+    }
+  }
+`);
 
 export interface SettingsScreenParams {
   account: Address;
@@ -32,74 +48,79 @@ export const SettingsScreen = ({ route, navigation: { navigate } }: SettingsScre
   const styles = useStyles();
   const approver = useApproverAddress();
 
+  const { user } = useSuspenseQuery<SettingsQuery, SettingsQueryVariables>(SettingsDocument).data;
+
   return (
     <Screen>
-      <Appbar mode="large" leading="back" headline="Settings" />
+      <Appbar mode="small" leading="back" headline="Settings" />
 
       <ScrollView style={styles.container}>
-        <ListItem
-          leading={account}
-          headline="Account"
-          supporting={`Manage ${useAddressLabel(account)}`}
-          onPress={() => navigate('Account', { account })}
-        />
+        <TouchableOpacity style={styles.userContainer} onPress={() => {}}>
+          <AddressIcon address={approver} size={ICON_SIZE.large} />
+
+          <Text variant="titleLarge" style={styles.userName}>
+            {user.name}
+          </Text>
+
+          <View style={styles.approverContainer}>
+            <View style={styles.approverChevron} />
+
+            <Text variant="bodyLarge" style={styles.approverItem}>
+              <AddressLabel address={approver} />
+            </Text>
+
+            <Chevron style={[styles.approverChevron, styles.approverItem]} />
+          </View>
+        </TouchableOpacity>
 
         <ListItem
-          leading={approver}
-          headline="User"
-          supporting="Manage the user for this device"
-          onPress={() => navigate('User')}
-        />
-
-        <ListItem
-          leading={FingerprintIcon}
-          leadingSize="medium"
-          headline="Biometrics"
-          supporting="Configure when biometrics is required"
-          onPress={() => navigate('Biometrics', {})}
-        />
-
-        <ListItem
-          leading={(props) => <WalletConnectIcon {...props} size={styles.icon.width} />}
+          leading={WalletConnectIcon}
           headline="Sessions"
-          supporting="Manage WalletConnect sessions"
+          trailing={NavigateNextIcon}
           onPress={() => navigate('Sessions', { account })}
         />
 
         <ListItem
           leading={() => <Image source={ETH.iconUri} style={styles.icon} />}
           headline="Tokens"
-          supporting="Manage supported tokens"
+          trailing={NavigateNextIcon}
           onPress={() => navigate('Tokens', { account })}
         />
 
         <ListItem
-          leading={(props) => <ContactsIcon {...props} size={styles.icon.width} />}
+          leading={ContactsIcon}
           headline="Contacts"
-          supporting="Manage contacts"
+          trailing={NavigateNextIcon}
           onPress={() => navigate('Contacts', {})}
         />
 
         <ListItem
-          leading={(props) => <NotificationsIcon {...props} size={styles.icon.width} />}
+          leading={FingerprintIcon}
+          headline="Biometrics"
+          trailing={NavigateNextIcon}
+          onPress={() => navigate('Biometrics', {})}
+        />
+
+        <ListItem
+          leading={NotificationsIcon}
           headline="Notifications"
-          supporting="Choose which notifications to receive"
+          trailing={NavigateNextIcon}
           onPress={() => navigate('NotificationSettings', {})}
         />
 
-        <Divider horizontalInset />
-
         <ListItem
-          leading={(props) => <TwitterIcon {...props} size={styles.icon.width} />}
+          leading={TwitterIcon}
           headline="Contact us"
-          supporting="Tweet at us, or slide into our DMs"
+          // supporting="Tweet at us, or slide into our DMs for support"
+          trailing={NavigateNextIcon}
           onPress={() => Linking.openURL(CONFIG.metadata.twitter)}
         />
 
         <ListItem
-          leading={(props) => <GithubIcon {...props} size={styles.icon.width} />}
+          leading={GithubIcon}
           headline="GitHub"
-          supporting="Contribute to Zallo under the AGPL-3.0 license"
+          // supporting="Contribute to Zallo under the AGPL-3.0 license"
+          trailing={NavigateNextIcon}
           onPress={() => Linking.openURL(CONFIG.metadata.github)}
         />
       </ScrollView>
@@ -107,12 +128,31 @@ export const SettingsScreen = ({ route, navigation: { navigate } }: SettingsScre
   );
 };
 
-const useStyles = makeStyles(({ iconSize }) => ({
+const useStyles = makeStyles(({ colors, iconSize }) => ({
   container: {
     flex: 1,
   },
   icon: {
-    width: iconSize.medium,
-    height: iconSize.medium,
+    width: iconSize.small,
+    height: iconSize.small,
+  },
+  userContainer: {
+    alignItems: 'center',
+    marginHorizontal: 16,
+    marginBottom: 16,
+  },
+  userName: {
+    marginTop: 8,
+  },
+  approverContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  approverItem: {
+    color: colors.tertiary,
+  },
+  approverChevron: {
+    width: ICON_SIZE.small,
   },
 }));
