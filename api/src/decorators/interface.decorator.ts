b1ff988755c6a5d2ebd54d4532e42412a5b08654
Hayden Briese <haydenbriese@gmail.com>
2023-07-12 11:41:32 +1000

    Refactor

--- api/src/decorators/interface.decorator.ts
+++ api/src/decorators/interface.decorator.ts
@@ -1,19 +1,14 @@
 import { Field, ID, InterfaceType, ObjectType, ObjectTypeOptions } from '@nestjs/graphql';
 import { uuid } from 'edgedb/dist/codecs/ifaces';
 
-@InterfaceType()
-export class Node {
-  @Field(() => ID)
-  id: uuid;
-}
-
-export const NodeType =
+const createObjectWithInterfaceDecorator =
+  (iface: Function | (() => Function)) =>
   (options?: ObjectTypeOptions): ClassDecorator =>
   (target) =>
     ObjectType({
       ...options,
       implements: [
-        Node,
+        iface,
         ...(options?.implements
           ? Array.isArray(options.implements)
             ? options.implements
@@ -21,3 +16,19 @@ export const NodeType =
           : []),
       ],
     })(target);
+
+@InterfaceType()
+export class Node {
+  @Field(() => ID)
+  id: uuid;
+}
+
+export const NodeType = createObjectWithInterfaceDecorator(Node);
+
+@InterfaceType()
+export class Err {
+  @Field(() => String)
+  message: string;
+}
+
+export const ErrType = createObjectWithInterfaceDecorator(Err);
