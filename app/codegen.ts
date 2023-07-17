7ed7b85ab071462b946526f8567891fe3a5dfe60
Hayden Briese <haydenbriese@gmail.com>
2023-07-17 17:42:58 +1000

    Configure relay compiler

--- app/codegen.ts
+++ app/codegen.ts
@@ -1,6 +1,11 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-const plugins = ['typescript', 'typescript-operations', 'typescript-react-apollo'];
+const plugins = [
+  '@n1ru4l/graphql-codegen-relay-optimizer-plugin',
+  'typescript',
+  'typescript-operations',
+  'typescript-react-apollo',
+];
 
 export default {
   overwrite: true,
@@ -9,11 +14,6 @@ export default {
       schema: '../api/schema.graphql',
       documents: ['src/**/*', '!src/gql/uniswap/**/*'],
       plugins,
-      //
-      // preset: 'client-preset', // Enables useFragments - https://the-guild.dev/graphql/codegen/plugins/presets/preset-client
-      // presetConfig: {
-      //   gqlTagName: 'gql',
-      // },
       config: {
         // https://the-guild.dev/graphql/codegen/plugins/typescript/typescript
         // defaultScalarType: 'unknown',
@@ -29,15 +29,22 @@ export default {
           Selector: 'lib#Selector',
           Uint256: 'lib#BigIntlike',
         },
+        withHOC: false,
+        withComponent: false,
+        withHooks: true,
+        // https://the-guild.dev/graphql/codegen/plugins/typescript/relay-operation-optimizer
+        skipDocumentsValidation: true,
+        flattenGeneratedTypes: true,
       },
     },
     'src/gql/api/gen/': {
       schema: '../api/schema.graphql',
       documents: ['src/**/*', '!src/gql/uniswap/**/*'],
-      preset: 'client-preset', // Enables useFragments - https://the-guild.dev/graphql/codegen/plugins/presets/preset-client
+      preset: 'client', // Enables useFragments - https://the-guild.dev/graphql/codegen/plugins/presets/preset-client
       presetConfig: {
         gqlTagName: 'gql',
       },
+      // plugins: ['@n1ru4l/graphql-codegen-relay-optimizer-plugin'], // Breaks )':
       config: {
         // https://the-guild.dev/graphql/codegen/plugins/typescript/typescript
         // defaultScalarType: 'unknown',
