1015a50a6efe1201582976a6fd65ed6bf0064e1b
Hayden Briese <haydenbriese@gmail.com>
2023-07-19 14:43:10 +1000

    Add account balances cache to provider

--- api/src/features/util/provider/provider.service.ts
+++ api/src/features/util/provider/provider.service.ts
@@ -16,18 +16,35 @@ import {
   getProxyAddress,
   deployAccountProxy,
   getEthersConnectionParams,
+  ERC20_ABI,
 } from 'lib';
 import { Mutex } from 'async-mutex';
 import { PublicClient, WebSocketTransport, createPublicClient, webSocket } from 'viem';
+import { LRUCache } from 'lru-cache';
+import { ETH_ADDRESS } from 'zksync-web3/build/src/utils';
+
+interface BalanceKey {
+  account: Address;
+  token: Address;
+}
 
 @Injectable()
 export class ProviderService extends zk.Provider {
   public chain: Chain;
   public client: PublicClient<WebSocketTransport, typeof CONFIG.chain>;
-
   private wallet: zk.Wallet;
   private walletMutex = new Mutex(); // TODO: replace with distributed mutex - https://linear.app/zallo/issue/ZAL-91
   private proxyFactory: Factory;
+  private balancesCache = new LRUCache<string, bigint>({
+    fetchMethod: (key) => this.fetchBalance(key),
+    max: 100_000,
+    ttl: 15_000, // 15s
+    // Return stale value, then fetch in background
+    allowStale: true,
+    // Re-use stale value if fetch fails
+    allowStaleOnFetchRejection: true,
+    noDeleteOnFetchRejection: true,
+  });
 
   constructor() {
     super(...getEthersConnectionParams(CONFIG.chain, 'http'));
@@ -36,6 +53,7 @@ export class ProviderService extends zk.Provider {
     this.client = createPublicClient({
       transport: webSocket(undefined, { retryCount: 10 }),
       chain: CONFIG.chain,
+      batch: { multicall: true },
     });
 
     this.wallet = (
@@ -86,5 +104,22 @@ export class ProviderService extends zk.Provider {
 
   async deployProxy(args: Omit<DeployArgs, 'factory'>) {
     return this.useProxyFactory((factory) => deployAccountProxy({ ...args, factory }));
+  }
+
+  async balance(key: BalanceKey): Promise<bigint> {
+    return (await this.balancesCache.fetch(`${key.account}-${key.token}`)) ?? 0n;
+  }
+
+  private async fetchBalance(key: string) {
+    const [account, token] = key.split('-') as [Address, Address];
+
+    if (token === ETH_ADDRESS) return await this.client.getBalance({ address: account });
+
+    return await this.client.readContract({
+      abi: ERC20_ABI,
+      address: token,
+      functionName: 'balanceOf',
+      args: [account],
+    });
   }
 }
