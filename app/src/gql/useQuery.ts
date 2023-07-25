9afe864fbaf7d66c3f98224a705e112dc50a321a
Hayden Briese <haydenbriese@gmail.com>
2023-07-25 12:41:01 +1000

    Replace query documents with matching one generated by relay-compiler if possible

--- app/src/gql/useQuery.ts
+++ app/src/gql/useQuery.ts
@@ -4,7 +4,11 @@ import {
   AnyVariables,
   UseQueryState,
   DocumentInput,
+  TypedDocumentNode,
+  UseQueryExecute,
 } from 'urql';
+import { DocumentNode, Kind } from 'graphql';
+import * as gen from '~/gql/api/generated';
 
 type Options<Data, Variables extends AnyVariables> = Omit<
   UseQueryArgs<Variables, Data>,
@@ -12,6 +16,11 @@ type Options<Data, Variables extends AnyVariables> = Omit<
 >;
 
 type OptionalVariables = undefined | Record<string, never>;
+
+type QueryResponse<Data, Variables extends AnyVariables> = Omit<
+  UseQueryState<Data, Variables>,
+  'data'
+> & { data: Data; reexecute: UseQueryExecute };
 
 export function useQuery<Data, Variables extends AnyVariables>(
   // Check whether the variables are optional
@@ -27,10 +36,39 @@ export function useQuery<Data, Variables extends AnyVariables>(
         options?: Options<Data, Variables>,
       ]
 ) {
-  return useBaseQuery({ ...options, query, variables: variables! })[0] as Omit<
-    UseQueryState<Data, Variables>,
-    'data'
-  > & {
-    data: Data;
-  };
+  const [response, reexecute] = useBaseQuery({
+    ...options,
+    query: getReplacementDocument(query) ?? query,
+    variables: variables!,
+  });
+
+  const r = response as QueryResponse<Data, Variables>;
+  r.reexecute = reexecute;
+
+  return r;
+}
+
+const CACHED_REPLACEMENTS = new Map<DocumentInput<unknown, unknown>, DocumentNode | null>();
+
+function getReplacementDocument<Data, Variables>(
+  doc: DocumentInput<Data, Variables>,
+): TypedDocumentNode<Data, Variables> | null {
+  // Replace documents ('client-preset' generated) with ones from api/generated - which are optimized by the Relay compiler
+  const cachedMatch = CACHED_REPLACEMENTS.get(doc);
+  if (cachedMatch !== undefined) return cachedMatch;
+
+  if (typeof doc === 'object' && doc.definitions.length >= 1) {
+    const def = doc.definitions[0];
+
+    if (def.kind === Kind.OPERATION_DEFINITION && def.name) {
+      const matchingDocument = gen[`${def.name.value}Document` as keyof typeof gen];
+      if (typeof matchingDocument === 'object') {
+        CACHED_REPLACEMENTS.set(doc, matchingDocument);
+        return matchingDocument;
+      }
+    }
+  }
+
+  CACHED_REPLACEMENTS.set(doc, null);
+  return null;
 }
