07fa908442cdc1aad3b6e580286b7041453cc1f7
Hayden Briese <haydenbriese@gmail.com>
2023-07-26 17:50:40 +1000

    Use tokens from database for faucet service

--- api/src/features/faucet/faucet.service.ts
+++ api/src/features/faucet/faucet.service.ts
@@ -1,62 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { BigNumber } from 'ethers';
-import { parseEther, parseUnits } from 'ethers/lib/utils';
-import { asAddress, Address, filterAsync } from 'lib';
-import * as zk from 'zksync-web3';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Address, filterAsync } from 'lib';
 import { ProviderService } from '~/features/util/provider/provider.service';
 import { selectAccount } from '../accounts/accounts.util';
 import { DatabaseService } from '../database/database.service';
-
-interface TokenFaucet {
-  addr: Address;
-  amount: BigNumber;
-}
-
-const ETH: TokenFaucet = {
-  addr: asAddress(zk.utils.ETH_ADDRESS),
-  amount: parseEther('0.01'),
-};
-
-const DAI: TokenFaucet = {
-  addr: asAddress('0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b'),
-  amount: parseUnits('3', 18),
-};
-const USDC: TokenFaucet = {
-  addr: asAddress('0x0faF6df7054946141266420b43783387A78d82A9'),
-  amount: parseUnits('1', 6),
-};
-const LINK: TokenFaucet = {
-  addr: asAddress('0x40609141Db628BeEE3BfAB8034Fc2D8278D0Cc78'),
-  amount: parseUnits('1', 18),
-};
+import e from '~/edgeql-js';
+import { parseUnits } from 'viem';
+import { ETH_ADDRESS } from 'zksync-web3/build/src/utils';
 
 @Injectable()
-export class FaucetService {
+export class FaucetService implements OnModuleInit {
+  private tokens: { address: Address; amount: bigint }[] = [];
+
   constructor(private provider: ProviderService, private db: DatabaseService) {}
 
+  async onModuleInit() {
+    const tokens = await this.db.query(
+      e.select(e.Token, (t) => ({
+        filter: e.op('not', e.op('exists', t.user)),
+        address: true,
+        decimals: true,
+      })),
+    );
+
+    this.tokens = tokens.map((t) => ({
+      address: t.address as Address,
+      amount: parseUnits(t.address === ETH_ADDRESS ? '0.1' : '1', t.decimals),
+    }));
+  }
+
   async requestableTokens(account: Address): Promise<Address[]> {
-    return (await this.getTokensToSend(account)).map((token) => token.addr);
+    return (await this.getTokensToSend(account)).map((token) => token.address);
   }
 
   async requestTokens(account: Address): Promise<Address[]> {
     const tokensToSend = await this.getTokensToSend(account);
 
     return (
-      await filterAsync(tokensToSend, async (token) => !!(await this.transfer(account, token)))
-    ).map((token) => token.addr);
+      await filterAsync(
+        tokensToSend,
+        async (token) => !!(await this.transfer(account, token.address, token.amount)),
+      )
+    ).map((token) => token.address);
   }
 
   private async getTokensToSend(account: Address) {
     if (!(await this.db.query(selectAccount(account)))) return [];
 
-    return filterAsync([ETH, DAI, USDC, LINK], async (token) => {
-      const recipientBalance = await this.provider.getBalance(account, undefined, token.addr);
+    return filterAsync(this.tokens, async (token) => {
+      const recipientBalance = await this.provider.getBalance(account, undefined, token.address);
       if (recipientBalance.gte(token.amount)) return false;
 
       const walletBalance = await this.provider.getBalance(
         this.provider.walletAddress,
         undefined,
-        token.addr,
+        token.address,
       );
       if (walletBalance.lt(token.amount)) return false;
 
@@ -64,13 +61,9 @@ export class FaucetService {
     });
   }
 
-  private async transfer(recipient: Address, token: TokenFaucet) {
+  private async transfer(to: Address, token: Address, amount: bigint) {
     return this.provider.useWallet(async (wallet) => {
-      const tx = await wallet.transfer({
-        to: recipient,
-        token: token.addr,
-        amount: token.amount,
-      });
+      const tx = await wallet.transfer({ to, token, amount });
 
       return tx.wait();
     });
