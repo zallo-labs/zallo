03dd4209cd1f8d507015f23f2e28b9737c37a893
Hayden Briese <haydenbriese@gmail.com>
2023-08-03 19:31:23 +1000

    Remove unused eas submission mode

--- app/src/screens/pair-ledger/BleDeviceItem.tsx
+++
@@ -1,9 +0,0 @@
-import { LedgerBleDevice } from './useLedgerBleDevices';
-
-export interface BleDeviceItemProps {
-  device: LedgerBleDevice;
-}
-
-export function BleDeviceItem({ device: d }: BleDeviceItemProps) {
-  return null;
-}
