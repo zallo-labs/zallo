e1f5dd9a8959c87a5a55eb0720d00b4340c738fe
Hayden Briese <haydenbriese@gmail.com>
2023-08-22 09:52:52 +1000

    Remove support for eth_sign and refactor wallet connect signing utils

--- app/src/util/walletconnect/methods/signing.ts
+++ app/src/util/walletconnect/methods/signing.ts
@@ -1,12 +1,11 @@
-import { ethers } from 'ethers';
 import { match } from 'ts-pattern';
-import { Addresslike, asAddress, asHex } from 'lib';
-import { hexToString, isHex } from 'viem';
+import { Addresslike, asAddress, isHex } from 'lib';
+import { TypedDataDefinition, hexToString } from 'viem';
 
-export type SigningRequest = EthSignRequest | PersonalSignRequest | SignTypedDataRequest;
+export type SigningRequest = PersonalSignRequest | SignTypedDataRequest;
 
 const WC_SIGNING_METHODS_ARRAY = [
-  // eth_sign is insecure and not supported
+  // eth_sign is insecure and intentionally *not* supported
   'personal_sign',
   'eth_signTypedData', // v1
   'eth_signTypedData_v3',
@@ -38,87 +37,26 @@ export interface SignTypedDataRequest {
   params: [account: Addresslike, typedDataJson: string];
 }
 
-export interface Eip712TypedDomainData<M = Record<string, unknown>, Type extends string = string> {
-  domain: ethers.TypedDataDomain;
-  types: Record<Type, ethers.TypedDataField[]>;
-  primaryType: Type;
-  message: M;
-}
-
 export const normalizeSigningRequest = (r: SigningRequest) =>
   match(r)
-    .with({ method: 'personal_sign' }, ({ method, params: [message, account] }) => ({
-      method,
-      account: asAddress(account),
-      message: isHex(message) ? hexToString(message) : message,
-    }))
-    .with({ method: 'eth_sign' }, ({ method, params: [account, message] }) => ({
-      method,
-      account: asAddress(account),
-      message: asHex(message),
-    }))
+    .with(
+      { method: 'personal_sign' },
+      ({ params: [message, account] }) =>
+        ({
+          method: 'personal-sign',
+          account: asAddress(account),
+          message: isHex(message) ? hexToString(message) : message,
+        }) as const,
+    )
     .with(
       { method: 'eth_signTypedData' },
       { method: 'eth_signTypedData_v3' },
       { method: 'eth_signTypedData_v4' },
-      ({ method, params: [account, typedDataJson] }) => ({
-        method,
-        account: asAddress(account),
-        message: toTypedData(typedDataJson),
-      }),
+      ({ params: [account, typedDataJson] }) =>
+        ({
+          method: 'typed-data',
+          account: asAddress(account),
+          typedData: JSON.parse(typedDataJson) as TypedDataDefinition,
+        }) as const,
     )
     .exhaustive();
-
-const toTypedData = (typedDataJson: string): Eip712TypedDomainData => {
-  const typedData = JSON.parse(typedDataJson) as Eip712TypedDomainData;
-
-  // EIP712Domain is sent according to the RPC spec, but must not be passed into ethers
-  // https://github.com/ethers-io/ethers.js/issues/687#issuecomment-714069471
-  delete typedData.types['EIP712Domain'];
-
-  return typedData;
-};
-
-export const isTypedData = (v: unknown): v is Eip712TypedDomainData =>
-  v !== null &&
-  typeof v === 'object' &&
-  'domain' in v &&
-  'types' in v &&
-  'primaryType' in v &&
-  'message' in v;
-
-export interface Eip712DataNode {
-  type?: string;
-  name?: string;
-  children: Eip712DataNode[] | string;
-}
-
-export const isTypedDataNode = (v: unknown): v is Eip712DataNode =>
-  v !== null && typeof v === 'object' && 'children' in v;
-
-export const asTypedDataNode = (d: Eip712TypedDomainData): Eip712DataNode => {
-  const getNode = (
-    v: Record<string, unknown> | unknown,
-    name: string | undefined,
-    type: string,
-  ): Eip712DataNode => {
-    if (typeof v !== 'object' || v === null)
-      return {
-        name,
-        type,
-        children: typeof v === 'string' ? v : JSON.stringify(v),
-      };
-
-    const childrenTypes = d.types[type];
-
-    return {
-      name,
-      type,
-      children: Object.entries(v).map(([key, value]) =>
-        getNode(value, key, childrenTypes.find((t) => t.name === key)!.type),
-      ),
-    };
-  };
-
-  return getNode(d.message, undefined, d.primaryType);
-};
