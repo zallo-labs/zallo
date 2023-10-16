8adaac5b00f4d417fa83c7fb9a3b03ac363bde5f
Hayden Briese <haydenbriese@gmail.com>
2023-10-16 23:47:37 +1100

    Execute onPress when FormSubmitButton is disabled by an onBlur event

--- app/src/components/fields/FormSubmitButton.tsx
+++ app/src/components/fields/FormSubmitButton.tsx
@@ -1,31 +1,42 @@
+import { useEffect, useRef } from 'react';
 import { FieldValues, useFormState, UseFormStateProps } from 'react-hook-form';
+import { GestureResponderEvent } from 'react-native';
 import { Button, ButtonProps } from '~/components/Button';
 
-export interface FormSubmitDisabledOptions<TFieldValues extends FieldValues> {
+export interface FormSubmitButtonProps<TFieldValues extends FieldValues>
+  extends Omit<ButtonProps, 'onPress'> {
   control?: UseFormStateProps<TFieldValues>['control'];
   requireChanges?: boolean;
+  onPress?: (e?: GestureResponderEvent) => void;
 }
-
-export function useFormSubmitDisabled<TFieldValues extends FieldValues>({
-  control,
-  requireChanges,
-}: FormSubmitDisabledOptions<TFieldValues>) {
-  const { isValid, isSubmitting, isSubmitted, isDirty } = useFormState({ control });
-
-  return isSubmitting || (isSubmitted && !isValid) || (requireChanges && !isDirty);
-}
-
-export interface FormSubmitButtonProps<TFieldValues extends FieldValues>
-  extends ButtonProps,
-    FormSubmitDisabledOptions<TFieldValues> {}
 
 export function FormSubmitButton<TFieldValues extends FieldValues>({
   requireChanges,
   control,
-  disabled,
   ...props
 }: FormSubmitButtonProps<TFieldValues>) {
+  const { isValid, isSubmitting, isSubmitted, isDirty } = useFormState({ control });
+
+  const disabled =
+    isSubmitting || (isSubmitted && !isValid) || (requireChanges && !isDirty) || props.disabled;
+
+  const expectOnPress = useRef(false);
+  useEffect(() => {
+    if (isSubmitted && expectOnPress.current) props.onPress?.();
+  }, [isSubmitted]);
+
   return (
-    <Button {...props} disabled={useFormSubmitDisabled({ control, requireChanges }) || disabled} />
+    <Button
+      {...props}
+      disabled={disabled}
+      loading={isSubmitting}
+      {...(props.onPress && {
+        onPointerDown: () => (expectOnPress.current = true),
+        onPress: (e) => {
+          expectOnPress.current = false;
+          props.onPress?.(e);
+        },
+      })}
+    />
   );
 }
