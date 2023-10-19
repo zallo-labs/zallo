1d3ffcf2990092b1be12bb22620df69ce2dcdce1
Hayden Briese <haydenbriese@gmail.com>
2023-10-19 21:39:31 +1100

    Make sentry DSN optional to easily disable in dev

--- api/src/config.ts
+++ api/src/config.ts
@@ -31,7 +31,7 @@ export const CONFIG = {
   accountImplAddress: tryAsAddress(required`ACCOUNT_IMPL_${chain.key.toUpperCase()}`),
   proxyFactoryAddress: tryAsAddress(required`PROXY_FACTORY_${chain.key.toUpperCase()}`),
   uniswapGqlUrl: required`UNISWAP_GQL_URL`,
-  sentryDsn: required`API_SENTRY_DSN`,
+  sentryDsn: optional`API_SENTRY_DSN`,
   serverId: optional`FLY_ALLOC_ID` || os.hostname(),
   bullBoardUser: optional`BULL_BOARD_USER`,
   bullBoardPassword: optional`BULL_BOARD_PASSWORD`,
@@ -39,7 +39,7 @@ export const CONFIG = {
     [
       optional`GOOGLE_OAUTH_WEB_CLIENT`,
       optional`GOOGLE_OAUTH_IOS_CLIENT`,
-      ...JSON.parse(optional`APPLE_OAUTH_CLIENTS` || '[]'),
+      ...(JSON.parse(optional`APPLE_OAUTH_CLIENTS` || '[]') as string[]),
     ].filter(isPresent),
   ),
 } as const;
