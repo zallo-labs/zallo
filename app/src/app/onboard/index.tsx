8b4b103ed1dcfdad50f29151b879717f76c777fd
Hayden Briese <haydenbriese@gmail.com>
2023-10-23 23:47:53 +1100

    Improve onboarding page

--- app/src/app/onboard/index.tsx
+++ app/src/app/onboard/index.tsx
@@ -1,6 +1,5 @@
-import { Image } from 'expo-image';
-import { SearchParams, Stack, useRouter } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { SearchParams, useRouter } from 'expo-router';
+import { Platform, View, ScrollView } from 'react-native';
 import { Text } from 'react-native-paper';
 import { gql } from 'urql';
 import { Button } from '~/components/Button';
@@ -10,6 +9,17 @@ import { LinkGoogleButton } from '~/components/link/LinkGoogleButton';
 import { LinkingButton } from '~/components/link/LinkingButton';
 import { LinkLedgerButton } from '~/components/link/ledger/LinkLedgerButton';
 import { useUrqlApiClient } from '@api/client';
+import { AppbarOptions } from '~/components/Appbar/AppbarOptions';
+import {
+  AppScreenshots,
+  AppStoreBadge,
+  GithubIcon,
+  GooglePlayBadge,
+  LogoIcon,
+  TwitterIcon,
+} from '@theme/icons';
+import { CONFIG } from '~/util/config';
+import { makeStyles } from '@theme/makeStyles';
 
 const Query = gql(/* GraphQL */ `
   query OnboardScreen {
@@ -24,6 +34,7 @@ export type OnboardScreenRoute = `/onboard/`;
 export type OnboardScreenParams = SearchParams<OnboardScreenRoute>;
 
 export default function OnboardScreen() {
+  const styles = useStyles();
   const { push } = useRouter();
   const api = useUrqlApiClient();
 
@@ -38,24 +49,44 @@ export default function OnboardScreen() {
   };
 
   return (
-    <View style={styles.root}>
-      <Stack.Screen options={{ headerShown: false }} />
+    <ScrollView contentContainerStyle={styles.root}>
+      <AppbarOptions
+        leading={() => null}
+        trailing={[
+          (props) => <TwitterIcon {...props} onPress={() => push(CONFIG.metadata.twitter)} />,
+          (props) => <GithubIcon {...props} onPress={() => push(CONFIG.metadata.github)} />,
+        ]}
+      />
 
-      <View style={styles.header}>
-        <Image
-          source={require('assets/logo-color.svg')}
-          onError={(e) => {
-            console.error(e);
-          }}
-          style={{ width: 325, height: 104 }}
-        />
-        <Text variant="headlineSmall" style={styles.description}>
-          Permission-based{'\n'}self-custodial smart wallet
-        </Text>
+      <View style={styles.content}>
+        <View style={styles.spacer} />
+
+        <View style={styles.mainContent}>
+          <LogoIcon style={styles.logo} contentFit="contain" />
+
+          <Text variant="headlineSmall" style={styles.text}>
+            Self-custody without compromise
+          </Text>
+
+          <View style={styles.companionContainer}>
+            <Text variant="titleMedium" style={styles.text}>
+              Get the companion app
+            </Text>
+
+            <View style={styles.appStores}>
+              <AppStoreBadge style={styles.appStoreBadge} />
+              <GooglePlayBadge style={styles.playStoreBadge} />
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.screenshotsContainer}>
+          <AppScreenshots style={styles.screenshots} contentFit="contain" />
+        </View>
       </View>
 
-      <Actions>
-        <Text variant="titleMedium" style={styles.continueText}>
+      <Actions flex={false} style={styles.actionsContainer}>
+        <Text variant="titleMedium" style={styles.text}>
           Continue with
         </Text>
 
@@ -70,26 +101,71 @@ export default function OnboardScreen() {
           Continue
         </Button>
       </Actions>
-    </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const useStyles = makeStyles(({ layout, window }) => ({
   root: {
+    flexGrow: 1,
+  },
+  content: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 16,
+    marginVertical: 32,
+    gap: 8,
+    flexWrap: 'wrap',
+  },
+  spacer: {
+    ...(!(Platform.OS === 'web' && layout === 'expanded') && { display: 'none' }),
     flex: 1,
   },
-  header: {
+  mainContent: {
     flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 16,
+    gap: 16,
   },
-  description: {
-    marginTop: 32,
+  logo: {
+    minWidth: 325,
+    minHeight: 104,
+  },
+  companionContainer: {
+    ...(Platform.OS !== 'web' && { display: 'none' }),
+    gap: 16,
+    marginVertical: 16,
+  },
+  appStores: {
+    flexDirection: 'row',
+    gap: 16,
+  },
+  text: {
     textAlign: 'center',
   },
-  continueText: {
-    textAlign: 'center',
+  appStoreBadge: {
+    width: 143,
+    height: 48,
+  },
+  playStoreBadge: {
+    width: 161,
+    height: 48,
+  },
+  screenshotsContainer: {
+    ...(!(Platform.OS === 'web' && layout === 'expanded') && { display: 'none' }),
+    flex: 1,
+    alignItems: 'center',
+    minWidth: 'auto',
+  },
+  screenshots: {
+    width: 413,
+    height: 453,
+  },
+  actionsContainer: {
+    alignSelf: 'center',
+    maxWidth: 1000,
+    width: '100%',
   },
   methodsContainer: {
     flexDirection: 'row',
@@ -98,4 +174,4 @@ const styles = StyleSheet.create({
     gap: 16,
     marginBottom: 16,
   },
-});
+}));
