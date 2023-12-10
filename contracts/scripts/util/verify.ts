61709b52cc0067b0072b5c0cfa61d4d07009e7ff
Hayden Briese <haydenbriese@gmail.com>
2023-12-11 10:04:21 +1100

    fix(contracts): pretty success message when contract is already verified
    Previously an exception was raised

--- contracts/scripts/util/verify.ts
+++ contracts/scripts/util/verify.ts
@@ -1,6 +1,7 @@
 import hre from 'hardhat';
 import { Address, Hex } from 'lib';
 import { CONFIG } from '../../config';
+import chalk from 'chalk';
 
 export interface VerifyOptions {
   address: Address;
@@ -10,5 +11,13 @@ export interface VerifyOptions {
 
 // https://era.zksync.io/docs/dev/building-on-zksync/contracts/contract-verification.html#verify-smart-contract-programmatically
 export const verify = async (args: VerifyOptions) => {
-  if (CONFIG.chain.verifyUrl) await hre.run('verify:verify', args);
+  try {
+    if (CONFIG.chain.verifyUrl) await hre.run('verify:verify', args);
+  } catch (e) {
+    if (e instanceof Error && e.message.includes('already verified')) {
+      console.log(chalk.green('Already verified'));
+    } else {
+      throw e;
+    }
+  }
 };
