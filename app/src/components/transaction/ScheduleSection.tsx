de4d417bcc3e6005f867eca56f5e4d4e4cf3088b
Hayden Briese <haydenbriese@gmail.com>
2024-02-22 14:33:11 +1100

    feat(app): scheduled transaction cancellation

--- app/src/components/transaction/ScheduleSection.tsx
+++ app/src/components/transaction/ScheduleSection.tsx
@@ -2,11 +2,16 @@ import { Timestamp } from '#/format/Timestamp';
 import { ListHeader } from '#/list/ListHeader';
 import { ListItem } from '#/list/ListItem';
 import { DELAY_ENTRIES } from '#/policy/DelaySettings';
+import { showError } from '#/provider/SnackbarProvider';
 import { FragmentType, gql, useFragment } from '@api';
-import { materialCommunityIcon } from '@theme/icons';
+import { TransactionProposalStatus } from '@api/documents.generated';
+import { CancelIcon, materialCommunityIcon } from '@theme/icons';
 import { createStyles } from '@theme/styles';
+import { useRouter } from 'expo-router';
 import { ReactNode } from 'react';
+import { IconButton } from 'react-native-paper';
 import { P, match } from 'ts-pattern';
+import { useMutation } from 'urql';
 
 const DelayIcon = materialCommunityIcon('timer-outline');
 const ScheduledIcon = materialCommunityIcon('calendar-month');
@@ -18,6 +23,7 @@ const TransactionProposal = gql(/* GraphQL */ `
     status
     account {
       id
+      address
       policies {
         id
         state {
@@ -37,6 +43,21 @@ const TransactionProposal = gql(/* GraphQL */ `
   }
 `);
 
+const CancelScheduledTx = gql(/* GraphQL */ `
+  mutation ScheduleSection_CancelScheduledTx($input: ProposeCancelScheduledTransactionInput!) {
+    proposeCancelScheduledTransaction(input: $input) {
+      id
+    }
+  }
+`);
+
+const DISPLAY_STATUSES: TransactionProposalStatus[] = [
+  'Pending',
+  'Scheduled',
+  'Executing',
+  'Cancelled',
+];
+
 export interface ScheduleSectionProps {
   children?: ReactNode;
   proposal: FragmentType<typeof TransactionProposal>;
@@ -44,11 +65,13 @@ export interface ScheduleSectionProps {
 
 export function ScheduleSection({ children, ...props }: ScheduleSectionProps) {
   const p = useFragment(TransactionProposal, props.proposal);
+  const router = useRouter();
+  const proposeCancelScheduledTx = useMutation(CancelScheduledTx)[1];
 
   const delay = p.account.policies.find((policy) => !p.policy || policy.id === p.policy.id)?.state
     ?.delay;
 
-  if (!delay) return;
+  if (!delay || !DISPLAY_STATUSES.includes(p.status)) return;
 
   return (
     <>
@@ -70,6 +93,23 @@ export function ScheduleSection({ children, ...props }: ScheduleSectionProps) {
               leading={ScheduledIcon}
               leadingSize="medium"
               headline={<Timestamp timestamp={t.scheduledFor!} time />}
+              trailing={(props) => (
+                <IconButton
+                  {...props}
+                  mode="contained"
+                  icon={CancelIcon}
+                  onPress={async () => {
+                    const id = (
+                      await proposeCancelScheduledTx({
+                        input: { account: p.account.address, proposal: p.id },
+                      })
+                    ).data?.proposeCancelScheduledTransaction.id;
+                    if (!id) return showError('Failed to propose to cancel scheduled transaction');
+
+                    router.push({ pathname: '/(drawer)/transaction/[id]', params: { id } });
+                  }}
+                />
+              )}
             />
           </>
         ))
