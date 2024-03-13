d83df577752887a71ba05fc6f4ddfba0b60fe23f
Hayden Briese <haydenbriese@gmail.com>
2024-03-14 09:58:29 +1100

    feat(api): add zksync-sepolia syncswap contract to global labels

--- api/dbschema/labels.list.ts
+++ api/dbschema/labels.list.ts
@@ -1,10 +1,14 @@
 import { CHAINS, Chain } from 'chains';
 import { Address, UAddress, asUAddress } from 'lib';
+import { SYNCSWAP } from 'lib/dapps';
 
 export const GLOBAL_LABELS: Record<UAddress, string> = {
   ...every('0x70fa585aFdbe80Ad4619bcCec17B86d31f017a23', 'Zallo'),
-  'zksync:0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295': 'SyncSwap',
-  'zksync-goerli:0xB3b7fCbb8Db37bC6f572634299A58f51622A847e': 'SyncSwap',
+  ...Object.fromEntries(
+    Object.entries(SYNCSWAP.router.address).map(
+      ([chain, address]) => [asUAddress(address, chain as Chain), 'SyncSwap'] as const,
+    ),
+  ),
 };
 
 function every(address: Address, label: string) {
