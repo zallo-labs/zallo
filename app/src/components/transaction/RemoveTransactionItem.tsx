f0fa1b996cd797e615c0bf096b17884f0a60a46d
Hayden Briese <haydenbriese@gmail.com>
2024-03-15 15:10:21 +1100

    fix(app): always redirect to activity screen when removing transaction

--- app/src/components/transaction/RemoveTransactionItem.tsx
+++ app/src/components/transaction/RemoveTransactionItem.tsx
@@ -4,6 +4,7 @@ import { UUID } from 'lib';
 import { Menu } from 'react-native-paper';
 import { useMutation } from 'urql';
 import { useConfirmRemoval } from '~/hooks/useConfirm';
+import { useSelectedAccount } from '~/hooks/useSelectedAccount';
 
 const Remove = gql(/* GraphQL */ `
   mutation RemoveTransactionItem_Remove($proposal: UUID!) {
@@ -18,6 +19,7 @@ export interface RemoveTransactionItemProps {
 
 export function RemoveTransactionItem({ proposal, close }: RemoveTransactionItemProps) {
   const router = useRouter();
+  const account = useSelectedAccount()!;
   const remove = useMutation(Remove)[1];
   const confirmRemoval = useConfirmRemoval({
     message: 'Are you sure you want to remove this proposal?',
@@ -30,7 +32,7 @@ export function RemoveTransactionItem({ proposal, close }: RemoveTransactionItem
         close();
         if (await confirmRemoval()) {
           await remove({ proposal });
-          router.back();
+          router.push({ pathname: '/(drawer)/[account]/(home)/activity', params: { account } });
         }
       }}
     />
