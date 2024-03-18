bb519fd65f60c8f6536733e1d88aaf6c4018c12a
Hayden Briese <haydenbriese@gmail.com>
2024-03-19 10:27:32 +1100

    feat(app): redirect to transfer transaction rather than push
    This now works due to the drawer layout changes

--- app/src/app/(drawer)/[account]/transfer.tsx
+++ app/src/app/(drawer)/[account]/transfer.tsx
@@ -116,7 +116,7 @@ function TransferScreen() {
               });
               if (!proposal) return;
 
-              router.push({
+              router.replace({
                 pathname: `/(drawer)/transaction/[id]`,
                 params: { id: proposal },
               });
