12da0b1a7326c4d3e0098db84b43b08a1ea2ab1a
Hayden Briese <haydenbriese@gmail.com>
2024-04-22 14:52:01 +1000

    feat(app): improve landing page text

--- app/src/components/landing/PrimarySection.tsx
+++ app/src/components/landing/PrimarySection.tsx
@@ -3,16 +3,17 @@ import { createStyles, useStyles } from '@theme/styles';
 import { Text } from 'react-native-paper';
 import { Button } from '#/Button';
 import { Link } from 'expo-router';
-import { TypewriterText } from './TypewriterText';
+import { TypewriterText, TypewriterValue } from './TypewriterText';
 import { UAddress } from 'lib';
+import { CONFIG } from 'app.config';
 
 const SELLING_POINTS = [
   { text: 'self-custody' },
   { text: 'security that scales' },
-  { text: 'fast & low-cost actions' },
+  { text: 'low fees' },
   { text: 'onchain banking' },
   { text: 'Zallo.', completeDelay: 5000 },
-];
+] satisfies TypewriterValue[];
 
 export interface PrimarySectionProps {
   account?: UAddress;
@@ -38,77 +39,87 @@ export function PrimarySection({ account }: PrimarySectionProps) {
       </Text>
 
       <Text variant="headlineSmall" style={styles.text}>
-        Secure your digital assets with self-custodial banking on zkSync Era. Access your account
-        seamlessly across devices, and enjoy practical self-custody with security that scales
+        Secure your digital assets with self-custodial banking on zkSync Era.{'\n'}
+        Access your account seamlessly and confidently with security that scales.
       </Text>
 
       <View style={styles.actionsContainer}>
         <View style={styles.actions}>
           {account ? (
             <Link href={{ pathname: `/(drawer)/[account]/(home)/`, params: { account } }} asChild>
-              <Button mode="contained" style={styles.action}>
+              <Button mode="contained" style={styles.primaryAction}>
                 View account
               </Button>
             </Link>
           ) : (
             <Link href="/onboard/account" asChild>
-              <Button mode="contained" style={styles.action}>
+              <Button mode="contained" style={styles.primaryAction}>
                 Get started
               </Button>
             </Link>
           )}
 
-          <Button mode="text">Learn more</Button>
+          <Link href={CONFIG.metadata.twitter} asChild>
+            <Button mode="text" style={styles.action}>
+              Learn more
+            </Button>
+          </Link>
         </View>
       </View>
     </View>
   );
 }
 
-const stylesheet = createStyles(({ colors, fonts }) => ({
-  section: {
-    flex: 1,
-    justifyContent: 'center',
-    gap: 16,
-    marginVertical: 16,
-  },
-  text: {
-    color: colors.onPrimaryContainer,
-  },
-  animatedText: {
-    ...fonts.displayLarge,
-    color: colors.tertiary,
-  },
-  actionsContainer: {
+const stylesheet = createStyles(({ colors, fonts }) => {
+  const action = {
     flexGrow: {
       compact: 1,
       medium: undefined,
     },
-    justifyContent: 'flex-end',
-  },
-  actions: {
-    flexDirection: {
-      compact: 'column',
-      medium: 'row',
-    },
-    marginVertical: 8,
-    columnGap: 16,
-    rowGap: 8,
-  },
-  action: {
-    minWidth: {
-      compact: 200,
-      medium: 200,
-    },
-    flexGrow: {
-      compact: 1,
+    flexBasis: {
+      compact: '100%',
       medium: undefined,
     },
-  },
-  spacer: {
-    flex: {
-      compact: 1,
-      medium: undefined,
+  } as const;
+
+  return {
+    section: {
+      flex: 1,
+      justifyContent: 'center',
+      gap: 16,
+      marginVertical: 16,
     },
-  },
-}));
+    text: {
+      color: colors.onPrimaryContainer,
+    },
+    animatedText: {
+      ...fonts.displayLarge,
+      color: colors.tertiary,
+    },
+    actionsContainer: {
+      flexGrow: {
+        compact: 1,
+        medium: undefined,
+      },
+      justifyContent: 'flex-end',
+    },
+    actions: {
+      flexDirection: 'row',
+      flexWrap: 'wrap-reverse',
+      marginVertical: 8,
+      columnGap: 16,
+      rowGap: 8,
+    },
+    primaryAction: {
+      minWidth: 200,
+      ...action,
+    },
+    action,
+    spacer: {
+      flex: {
+        compact: 1,
+        medium: undefined,
+      },
+    },
+  };
+});
