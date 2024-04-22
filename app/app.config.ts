69902367fb2799f0321bbf749f1b50fb1911cf45
Hayden Briese <haydenbriese@gmail.com>
2024-04-22 15:45:54 +1000

    fix(app): incorrect import path

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,7 +3,6 @@ import { ConfigPlugin } from 'expo/config-plugins';
 import { PluginConfigType as BuildPropertiesConfig } from 'expo-build-properties/build/pluginConfig';
 import expoRouterPlugin from 'expo-router/plugin';
 import path from 'path';
-import '@sentry/react-native/expo';
 
 // Absolute path resolution is required for EAS builds (during gradlew autolinking), but not available for dev client
 require('dotenv').config({ path: path.resolve(process.env.EAS_BUILD ? __dirname : '', '../.env') });
