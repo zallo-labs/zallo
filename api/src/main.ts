eb715cfccc039b52e0d67857e8a6b237f1a1ff67
Hayden Briese <haydenbriese@gmail.com>
2024-05-05 22:17:15 +1000

    feat(api): allow caching of CORS preflight request

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,11 +8,15 @@ import { GQL_ENDPOINT } from './apollo/apollo.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: true, credentials: true },
     logger: [...Object.values(LogLevel).slice(Object.values(LogLevel).indexOf(CONFIG.logLevel))],
   });
 
   app.enableShutdownHooks(); // Enable shutdown signals
+  app.enableCors({
+    origin: true, // * is not allowed when credentials=true
+    credentials: true,
+    maxAge: 7200 /* 2 hours */,
+  });
 
   await app.listen(CONFIG.apiPort);
   Logger.debug(`${await app.getUrl()}${GQL_ENDPOINT}`);
