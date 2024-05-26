95533a48668a08eddb09b93b923effe3764bcc69
Hayden Briese <haydenbriese@gmail.com>
2024-05-26 22:49:21 +1000

    fix(api): tsconfig for running jest

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -4,7 +4,6 @@ const { compilerOptions } = require('./tsconfig.json');
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['./dist', './dbschema', './generated', './node_modules'],
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
@@ -12,13 +11,11 @@ const config: JestConfigWithTsJest = {
   testTimeout: 30_000,
   globalSetup: './src/setup.jest.ts',
 
-  moduleNameMapper: {
-    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 
-    // Breaks edgedb import
-    //   uuid: require.resolve('uuid'), // https://github.com/uuidjs/uuid/issues/451#issuecomment-1112328417
-    //   msgpackr: require.resolve('msgpackr'),
-  },
+  // roots: ['<rootDir>'],
+  // modulePaths: [compilerOptions.baseUrl],
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
 };
 
 export default config;
