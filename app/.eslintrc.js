1d82addf6ed309c717a1d9cf250a5f93cbe302ea
Hayden Briese <haydenbriese@gmail.com>
2024-05-26 23:32:20 +1000

    fix(app): linting

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,21 +1,30 @@
 module.exports = {
-  plugins: ['react', 'react-hooks', 'react-native'],
-  extends: ['expo', 'eslint:recommended', 'plugin:@tanstack/eslint-plugin-query/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  plugins: ['react-native'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
     'react/display-name': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react/prop-types': ['error', { skipUndeclared: true }],
     'react-hooks/rules-of-hooks': 'error',
     // 'react-hooks/exhaustive-deps': 'error',
     'react-hooks/exhaustive-deps': [
       'error',
-      {
-        additionalHooks: '(useDeepMemo|useMyOtherCustomHook)',
-      },
+      { additionalHooks: '(useDeepMemo|useMyOtherCustomHook)' },
     ],
     'react-native/split-platform-components': 'error',
     'react-native/no-inline-styles': 'error',
     'react-native/no-single-element-style-arrays': 'error',
   },
   ignorePatterns: ['src/lib/ampli'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
