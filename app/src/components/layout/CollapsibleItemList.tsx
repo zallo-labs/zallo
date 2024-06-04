c788a879beaa77c05de729f87365b9ecd213dd47
Hayden Briese <haydenbriese@gmail.com>
2024-06-04 10:05:10 +1000

    feat(app): animate collapsible item list header item

--- app/src/components/layout/CollapsibleItemList.tsx
+++ app/src/components/layout/CollapsibleItemList.tsx
@@ -1,26 +1,41 @@
-import { CORNER } from '@theme/paper';
-import { createStyles, useStyles } from '@theme/styles';
 import React, { ReactNode } from 'react';
 import { View } from 'react-native';
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import { CORNER } from '@theme/paper';
+import { createStyles, useStyles } from '@theme/styles';
 import Collapsible from 'react-native-collapsible';
 
 export interface CollapsableItemListProps {
   expanded: boolean;
   children: ReactNode;
+  duration?: number;
 }
 
-export function CollapsibleItemList({ expanded, children }: CollapsableItemListProps) {
+export function CollapsibleItemList({
+  children,
+  expanded,
+  duration = 300,
+}: CollapsableItemListProps) {
   const { styles } = useStyles(stylesheet);
 
   const items = React.Children.toArray(children);
+
+  const headerStyle = useAnimatedStyle(() => ({
+    borderBottomLeftRadius: withTiming(expanded ? 0 : CORNER.l, { duration }),
+    borderBottomRightRadius: withTiming(expanded ? 0 : CORNER.l, { duration }),
+  }));
 
   if (!items.length) return null;
 
   return (
     <View style={styles.container}>
-      <View style={expanded ? styles.expandedHeader : styles.collapsedHeader}>{items[0]}</View>
+      <Animated.View style={[styles.header, headerStyle]}>{items[0]}</Animated.View>
 
-      <Collapsible collapsed={!expanded} style={[styles.container, styles.expandedContainer]}>
+      <Collapsible
+        collapsed={!expanded}
+        duration={duration}
+        style={[styles.container, styles.expandedContainer]}
+      >
         {items.slice(1)}
       </Collapsible>
     </View>
@@ -32,11 +47,7 @@ const stylesheet = createStyles((_theme) => ({
     overflow: 'hidden',
     gap: 4,
   },
-  collapsedHeader: {
-    overflow: 'hidden',
-    borderRadius: CORNER.l,
-  },
-  expandedHeader: {
+  header: {
     overflow: 'hidden',
     borderTopLeftRadius: CORNER.l,
     borderTopRightRadius: CORNER.l,
