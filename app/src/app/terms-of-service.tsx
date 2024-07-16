237c9b20912dc9ed9bf88498812b944ade26ba5c
Hayden Briese <haydenbriese@gmail.com>
2024-07-16 13:52:40 +1000

    feat(app): add redirects for /privacy and /terms-of-service

---
+++ app/src/app/terms-of-service.tsx
@@ -0,0 +1,8 @@
+import { Redirect } from 'expo-router';
+import { CONFIG } from '~/util/config';
+
+export const TERMS_OF_SERVICE_HREF = CONFIG.docsUrl + '/terms-of-service';
+
+export default function TermsOfServiceScreen() {
+  return <Redirect href={TERMS_OF_SERVICE_HREF} />;
+}
