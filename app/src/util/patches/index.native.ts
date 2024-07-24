412a95639dd4917ed17c4d46fc6f61963b817f40
Hayden Briese <haydenbriese@gmail.com>
2024-07-24 10:48:30 +1000

    fix(app): swap screen - native

--- app/src/util/patches/index.native.ts
+++ app/src/util/patches/index.native.ts
@@ -2,11 +2,6 @@
 // Among those, which are overridden below: Buffer, atob, btoa
 // import 'node-libs-react-native/globals'; // This is needed for atob for some reason, even though react-native-quick-base64 should override it
 
-global.process = require('process');
-
-// @ts-expect-error Some modules expect userAgent to be a string
-global.navigator.userAgent ??= 'React Native';
-
 import './shared';
 
 import '@walletconnect/react-native-compat';
@@ -15,7 +10,10 @@ import '@walletconnect/react-native-compat';
 import './intl';
 
 // Core JS
-import 'core-js/features/symbol/async-iterator'; // Required by urql
-import 'core-js/features/promise';  // Promise.allSettled required by viem client.multicall
+import 'core-js/actual/promise'; // Promise.allSettled required by viem client.multicall; full promise polyfill required for some reason
+
+global.process = require('process');
+// @ts-expect-error Cannot assign to userAgent because it is a read-only property.
+global.navigator.userAgent ??= 'React Native';
 
 export default {};
