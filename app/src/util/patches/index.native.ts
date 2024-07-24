9caab94023a9b3ea5cdac483f48455235622dd6c
Hayden Briese <haydenbriese@gmail.com>
2024-07-24 11:35:04 +1000

    fix(app,native): swap screen attempt 2

--- app/src/util/patches/index.native.ts
+++ app/src/util/patches/index.native.ts
@@ -9,9 +9,6 @@ import '@walletconnect/react-native-compat';
 // Intl pollyfills - required for iOS
 import './intl';
 
-// Core JS
-import 'core-js/actual/promise'; // Promise.allSettled required by viem client.multicall; full promise polyfill required for some reason
-
 global.process = require('process');
 // @ts-expect-error Cannot assign to userAgent because it is a read-only property.
 global.navigator.userAgent ??= 'React Native';
