1835757379bd65582871b5b0db85ba38f5d38f47
Hayden Briese <haydenbriese@gmail.com>
2024-07-25 15:59:50 +1000

    feat(app): improved ChainSelector with zksync era placeholder

--- app/src/components/fields/ChainSelector.tsx
+++ app/src/components/fields/ChainSelector.tsx
@@ -1,71 +1,55 @@
-import { Card } from '#/layout/Card';
 import { CHAINS, Chain } from 'chains';
 import { ListItem } from '../list/ListItem';
 import { ChainIcon } from '../Identicon/ChainIcon';
-import { useState } from 'react';
-import { Chevron } from '../Chevron';
-import Collapsible from 'react-native-collapsible';
 import { SUPPORTED_CHAIN_KEYS } from '@network/chains';
 import { ICON_SIZE } from '@theme/paper';
 import { createStyles, useStyles } from '@theme/styles';
+import { ItemList, ItemListProps } from '#/layout/ItemList';
 
 const descriptions: Record<Chain, string> = {
   zksync: 'Fast & low-cost layer 2 secured by Ethereum',
-  'zksync-sepolia': 'For testing purposes only',
-  'zksync-local': 'For testing purposes only',
+  'zksync-sepolia': 'Try out Zallo on a testnet - assets have no value',
+  'zksync-local': 'Local development',
 };
 
-export interface ChainSelectorProps {
+export interface ChainSelectorProps extends Partial<ItemListProps> {
   value: Chain;
   onChange: (chain: Chain) => void;
-  chains?: Chain[];
 }
 
-export function ChainSelector({
-  value: selected,
-  onChange,
-  chains = SUPPORTED_CHAIN_KEYS,
-}: ChainSelectorProps) {
+export function ChainSelector({ value: selected, onChange, ...props }: ChainSelectorProps) {
   const { styles } = useStyles(stylesheet);
 
-  const [expanded, setExpanded] = useState(false);
-
   return (
-    <Card type="outlined" style={styles.card}>
+    <ItemList {...props}>
       <ListItem
-        leading={<ChainIcon chain={selected} size={ICON_SIZE.medium} />}
-        overline="Network"
-        headline={CHAINS[selected].name}
-        supporting={descriptions[selected]}
-        trailing={<Chevron expanded={expanded} />}
-        selected={expanded}
-        onPress={() => setExpanded((e) => !e)}
+        leading={<ChainIcon chain="zksync" size={ICON_SIZE.medium} />}
+        headline={CHAINS['zksync'].name + ' - coming soon'}
+        supporting={descriptions['zksync']}
+        containerStyle={styles.item}
+        disabled
       />
 
-      <Collapsible collapsed={!expanded}>
-        {Object.values(chains)
-          .filter((c) => c !== selected)
-          .map((c) => (
-            <ListItem
-              key={c}
-              leading={<ChainIcon chain={c} size={ICON_SIZE.medium} />}
-              headline={CHAINS[c].name}
-              supporting={descriptions[c]}
-              trailing={<Chevron expanded={expanded} />}
-              onPress={() => {
-                setExpanded(false);
-                onChange(c);
-              }}
-            />
-          ))}
-      </Collapsible>
-    </Card>
+      {SUPPORTED_CHAIN_KEYS.map((c) => (
+        <ListItem
+          key={c}
+          leading={<ChainIcon chain={c} size={ICON_SIZE.medium} />}
+          headline={CHAINS[c].name}
+          supporting={descriptions[c]}
+          selected={selected === c}
+          containerStyle={styles.item}
+          onPress={() => {
+            onChange(c);
+          }}
+          disabled={selected !== c}
+        />
+      ))}
+    </ItemList>
   );
 }
 
 const stylesheet = createStyles(({ colors }) => ({
-  card: {
-    backgroundColor: 'transparent',
-    borderColor: colors.outline,
+  item: {
+    backgroundColor: colors.surfaceContainer.mid,
   },
 }));
