c56ada72d90f667ee3b5b75a31d33154956892b1
Hayden Briese <haydenbriese@gmail.com>
2024-08-16 15:32:29 +1000

    feat(api): replay most recent subscription event

--- api/src/core/pubsub/pubsub.service.ts
+++ api/src/core/pubsub/pubsub.service.ts
@@ -5,7 +5,8 @@ import Redis from 'ioredis';
 import { InjectRedisSubscriber } from '~/common/decorators/redis.decorator';
 import { afterRequest } from '../context';
 
-const WINDOW_S = 1;
+const DEDUP_WINDOW = 1;
+const REPLAY_WINDOW = 60;
 
 export interface EventPayload<E = unknown> {
   event: E;
@@ -26,16 +27,41 @@ export class PubsubService extends RedisPubSub {
     variant: (payload: T) => string = eventVariant,
   ) {
     // Dedublicate events within a window of time
-    const id = `published:${trigger}:${variant(payload)}`;
-    const r = await this.redis.call('set', id, '1', 'NX', 'EX', WINDOW_S); // set if not exists, doesn't reset expiry
+    const id = `subscription:published:${trigger}:${variant(payload)}`;
+    const r = await this.redis.call('set', id, '1', 'NX', 'EX', DEDUP_WINDOW); // set if not exists, doesn't reset expiry
     if (r !== 'OK') return; // Event already published
 
     afterRequest(() => {
       this.publish<T>(trigger, payload);
+
+      // Store the last event for replay
+      this.redis.set(replayKey(trigger), JSON.stringify(payload), 'EX', REPLAY_WINDOW);
+    });
+  }
+
+  asyncIterator<T>(triggers: string | string[], options?: unknown): AsyncIterator<T> {
+    const iterator = super.asyncIterator<T>(triggers, options);
+
+    this.replay(Array.isArray(triggers) ? triggers : [triggers]);
+
+    return iterator;
+  }
+
+  private async replay<T>(triggers: string[]) {
+    triggers.map(async (trigger) => {
+      const lastEventJson = await this.redis.get(replayKey(trigger));
+      if (lastEventJson) {
+        const payload = JSON.parse(lastEventJson) as T;
+        this.publish<T>(trigger, payload);
+      }
     });
   }
 }
 
 function eventVariant(payload: EventPayload) {
   return `${payload.event}`;
+}
+
+function replayKey(trigger: string) {
+  return `subscription:replay:${trigger}`;
 }
