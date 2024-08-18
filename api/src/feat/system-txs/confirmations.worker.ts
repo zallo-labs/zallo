d059d6512c554814bfa6e129fea294e46235516c
Hayden Briese <haydenbriese@gmail.com>
2024-08-18 11:33:05 +1000

    feat(api): emit executed event after processing events

--- api/src/feat/system-txs/confirmations.worker.ts
+++ api/src/feat/system-txs/confirmations.worker.ts
@@ -2,13 +2,24 @@ import { Processor } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
 import { ConfirmationQueue } from './confirmations.queue';
 import { NetworksService } from '~/core/networks/networks.service';
-import { Chain, ChainConfig } from 'chains';
+import { Chain, ChainConfig, Network } from 'chains';
 import { FormattedTransactionReceipt } from 'viem';
 import { AbiEvent } from 'abitype';
-import { TypedJob } from '~/core/bull/bull.util';
+import { QueueData, RUNNING_JOB_STATUSES, TypedJob } from '~/core/bull/bull.util';
 import { Worker } from '~/core/bull/Worker';
-import { isHex } from 'lib';
+import { asChain, asDecimal, asHex, asUAddress, isHex } from 'lib';
 import { EventsService, Log } from '../events/events.service';
+import { InjectRedis } from '@songkeys/nestjs-redis';
+import Redis from 'ioredis';
+import { and, DatabaseService } from '~/core/database';
+import { AccountsCacheService } from '../auth/accounts.cache.service';
+import { ProposalsService } from '../proposals/proposals.service';
+import { ProposalEvent } from '../proposals/proposals.input';
+import e from '~/edgeql-js';
+import { ETH } from 'lib/dapps';
+import { ampli } from '~/util/ampli';
+import { runOnce } from '~/util/mutex';
+import { selectSysTx } from './system-tx.util';
 
 export type Receipt = FormattedTransactionReceipt<ChainConfig>;
 export interface ConfirmationData {
@@ -16,26 +27,18 @@ export interface ConfirmationData {
   receipt: Receipt;
 }
 
-export type ConfirmationListener = (data: ConfirmationData) => Promise<unknown>;
-
-export interface ConfirmationEventData<TAbiEvent extends AbiEvent> extends ConfirmationData {
-  log: Log<TAbiEvent>;
-}
-
 @Injectable()
 @Processor(ConfirmationQueue.name, { autorun: false })
 export class ConfirmationsWorker extends Worker<ConfirmationQueue> {
-  private listeners: ConfirmationListener[] = [];
-
   constructor(
+    private db: DatabaseService,
     private networks: NetworksService,
     private events: EventsService,
+    @InjectRedis() private redis: Redis,
+    private proposals: ProposalsService,
+    private accountsCache: AccountsCacheService,
   ) {
     super();
-  }
-
-  on(listener: ConfirmationListener) {
-    this.listeners.push(listener);
   }
 
   async process(job: TypedJob<ConfirmationQueue>) {
@@ -59,13 +62,143 @@ export class ConfirmationsWorker extends Worker<ConfirmationQueue> {
       pollingInterval: 500,
     });
 
-    // Execute listeners prior to events to ensure result is available
-    await Promise.all(this.listeners.map((listener) => listener({ chain, receipt })));
+    return receipt.status === 'success'
+      ? await this.executed({ chain, receipt })
+      : await this.reverted({ chain, receipt });
+  }
+
+  private async executed({ chain, receipt }: ConfirmationData) {
+    if (!(await this.accountsCache.isAccount(asUAddress(receipt.from, chain)))) return;
+
+    const network = this.networks.get(chain);
+    const response = await this.getResponse(network, receipt);
+
+    const systx = selectSysTx(receipt.transactionHash);
+    const insertResult = e
+      .insert(e.ConfirmedSuccess, {
+        transaction: systx.proposal,
+        systx,
+        timestamp: new Date(), // block.timestamp not used as timestamp is used for ordering and time drift issues
+        block: BigInt(receipt.blockNumber),
+        gasUsed: receipt.gasUsed,
+        ethFeePerGas: asDecimal(receipt.effectiveGasPrice, ETH).toString(),
+        response: response.data,
+      })
+      .unlessConflict();
+
+    const proposal = await this.db.query(
+      e.select(insertResult.transaction, () => ({
+        id: true,
+        account: { address: true, approvers: { user: true } },
+      })),
+    );
+    if (!proposal) return `Transaction already processed: ${receipt.transactionHash}`;
+
+    this.log.debug(`Proposal executed: ${proposal.id}`);
 
     await this.events.processConfirmed({
       chain,
       logs: receipt.logs as unknown as Log<AbiEvent, true>[],
       receipt,
     });
+    this.proposals.event(proposal, ProposalEvent.executed);
+
+    proposal.account.approvers.forEach(({ user }) => {
+      ampli.transactionExecuted(user.id, { success: true });
+    });
+  }
+
+  private async reverted({ chain, receipt }: ConfirmationData) {
+    if (!(await this.accountsCache.isAccount(asUAddress(receipt.from, chain)))) return;
+
+    const network = this.networks.get(chain);
+    const response = await this.getResponse(network, receipt);
+
+    const systx = selectSysTx(receipt.transactionHash);
+    const insertResult = e
+      .insert(e.ConfirmedFailure, {
+        transaction: systx.proposal,
+        systx,
+        timestamp: new Date(), // block.timestamp not used as timestamp is used for ordering and time drift issues
+        block: BigInt(receipt.blockNumber),
+        gasUsed: receipt.gasUsed,
+        ethFeePerGas: asDecimal(receipt.effectiveGasPrice, ETH).toString(),
+        response: response.data,
+      })
+      .unlessConflict();
+
+    const proposal = await this.db.query(
+      e.select(insertResult.transaction, () => ({
+        id: true,
+        account: { address: true, approvers: { user: true } },
+      })),
+    );
+    if (!proposal) return `Transaction already processed: ${receipt.transactionHash}`;
+
+    this.log.debug(`Proposal reverted: ${proposal.id}`);
+    this.proposals.event(proposal, ProposalEvent.executed);
+
+    proposal.account.approvers.forEach(({ user }) => {
+      ampli.transactionExecuted(user.id, { success: false });
+    });
+  }
+
+  private async getResponse(network: Network, receipt: Receipt) {
+    const tx = await network.getTransaction({ hash: receipt.transactionHash });
+    const r = /* may throw */ await network.call({
+      blockNumber: receipt.blockNumber - 1n,
+      account: receipt.from,
+      gas: tx.gas,
+      maxFeePerGas: tx.maxFeePerGas,
+      maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
+      nonce: tx.nonce,
+      to: tx.to,
+      value: tx.value,
+      data: tx.input,
+    });
+
+    return {
+      ...r,
+      data: r.data ?? '0x',
+    };
+  }
+
+  async bootstrap() {
+    await runOnce(
+      async () => {
+        const jobs = (await this.queue.getJobs(RUNNING_JOB_STATUSES))
+          .map((j) => j.data.transaction)
+          .filter(isHex);
+
+        const orphanedTransactions = await this.db.query(
+          e.select(e.SystemTx, (t) => ({
+            filter: and(
+              e.op('not', e.op('exists', t.result)),
+              jobs.length ? e.op(t.hash, 'not in', e.set(...jobs)) : undefined,
+            ),
+            hash: true,
+            proposal: {
+              account: { address: true },
+            },
+          })),
+        );
+
+        if (orphanedTransactions.length) {
+          await this.queue.addBulk(
+            orphanedTransactions.map((t) => ({
+              name: ConfirmationQueue.name,
+              data: {
+                chain: asChain(asUAddress(t.proposal.account.address)),
+                transaction: asHex(t.hash),
+              } satisfies QueueData<ConfirmationQueue>,
+            })),
+          );
+        }
+      },
+      {
+        redis: this.redis,
+        key: 'transactions-missing-jobs',
+      },
+    );
   }
 }
